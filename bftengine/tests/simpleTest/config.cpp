e95fb1e38234ea18ef308c2e1c33b6f59b9a38e6
Bryan Fink <bfink@vmware.com>
2018-09-10 15:47:17 -0700

    example code cleanup: cpplint whitespace adherence
    This PR is whitespace only in code, to adhere to the rules of cpplint,
    plus a note in the CONTRIBUTING.md file to note that cpplint should be
    used.

--- bftengine/tests/simpleTest/config.cpp
+++ bftengine/tests/simpleTest/config.cpp
@@ -1,7 +1,7 @@
 #include <utility>
 #include <string>
 
-#include "CommFactory.hpp" 
+#include "CommFactory.hpp"
 #include "ReplicaConfig.hpp"
 #include <threshsign/ThresholdSignaturesSchemes.h>
 
@@ -17,7 +17,12 @@ using namespace bftEngine;
 #define MAX_ITEM_LENGTH_STR "4096"
 
 const char* nameOfPublicFile = "public_replicas_data";
-const char* namesOfPrivateFiles[] = { "private_replica_0", "private_replica_1", "private_replica_2", "private_replica_3" };
+const char* namesOfPrivateFiles[] = {
+  "private_replica_0",
+  "private_replica_1",
+  "private_replica_2",
+  "private_replica_3"
+};
 
 const int numOfReplicas = 4;
 const int fVal = 1;
@@ -29,193 +34,210 @@ const uint16_t basePort = 3710;
 
 
 static void readOneLine(FILE *f, int capacity, char * buf) {
-	fgets(buf, capacity - 1, f);
-	buf[strlen(buf) - 1] = 0; // strip newline
+  fgets(buf, capacity - 1, f);
+  buf[strlen(buf) - 1] = 0;  // strip newline
 }
 
 static void ignoreOneLine(FILE *f) {
-	char buf[8192];
-	readOneLine(f, 8191, buf);
+  char buf[8192];
+  readOneLine(f, 8191, buf);
 }
 
 
-static IThresholdSigner * createThresholdSigner(FILE * f, int id, IThresholdFactory* factory) 
-{
-	if (id < 1) {
-		throw std::runtime_error("Expected replica's ID to be strictly greater than zero!");
-	}
+static IThresholdSigner * createThresholdSigner(FILE * f,
+                                                int id,
+                                                IThresholdFactory* factory) {
+  if (id < 1) {
+    throw std::runtime_error(
+        "Expected replica's ID to be strictly greater than zero!");
+  }
 
-	char secretKey[MAX_ITEM_LENGTH];
-	ignoreOneLine(f);
+  char secretKey[MAX_ITEM_LENGTH];
+  ignoreOneLine(f);
 
-	readOneLine(f, MAX_ITEM_LENGTH, secretKey);
-	return factory->newSigner(id, secretKey);
+  readOneLine(f, MAX_ITEM_LENGTH, secretKey);
+  return factory->newSigner(id, secretKey);
 }
 
 
-static IThresholdFactory* createThresholdFactory(FILE* f, int n) 
-{
-	char buf[MAX_ITEM_LENGTH];
-	char * curveType = buf;
-	char cryptosys[MAX_ITEM_LENGTH];
+static IThresholdFactory* createThresholdFactory(FILE* f, int n) {
+  char buf[MAX_ITEM_LENGTH];
+  char * curveType = buf;
+  char cryptosys[MAX_ITEM_LENGTH];
 
-	ignoreOneLine(f);
-	readOneLine(f, MAX_ITEM_LENGTH, cryptosys);
-	readOneLine(f, MAX_ITEM_LENGTH, buf);
+  ignoreOneLine(f);
+  readOneLine(f, MAX_ITEM_LENGTH, cryptosys);
+  readOneLine(f, MAX_ITEM_LENGTH, buf);
 
-	if (strcmp(cryptosys, MULTISIG_BLS_SCHEME) == 0) {
-		return new BlsThresholdFactory(BLS::Relic::PublicParametersFactory::getByCurveType(curveType));
-	}
-	else if (strcmp(cryptosys, THRESHOLD_BLS_SCHEME) == 0) {
-		return new BlsThresholdFactory(BLS::Relic::PublicParametersFactory::getByCurveType(curveType));
-	}
-	else {		
-		printf("ERROR: Unsupported cryptosystem: %s\n", cryptosys);
-		throw std::runtime_error("ERROR: Unsupported cryptosystem");
-	}
+  if (strcmp(cryptosys, MULTISIG_BLS_SCHEME) == 0) {
+    return new BlsThresholdFactory(
+        BLS::Relic::PublicParametersFactory::getByCurveType(curveType));
+  } else if (strcmp(cryptosys, THRESHOLD_BLS_SCHEME) == 0) {
+    return new BlsThresholdFactory(
+        BLS::Relic::PublicParametersFactory::getByCurveType(curveType));
+  } else {
+    printf("ERROR: Unsupported cryptosystem: %s\n", cryptosys);
+    throw std::runtime_error("ERROR: Unsupported cryptosystem");
+  }
 }
 
-static IThresholdVerifier * createThresholdVerifier(FILE * f, int k, int n, IThresholdFactory* factory) {
-	char publicKey[MAX_ITEM_LENGTH];
-	char verifKey[MAX_ITEM_LENGTH];
+static IThresholdVerifier * createThresholdVerifier(
+    FILE * f, int k, int n, IThresholdFactory* factory) {
+  char publicKey[MAX_ITEM_LENGTH];
+  char verifKey[MAX_ITEM_LENGTH];
 
-	readOneLine(f, MAX_ITEM_LENGTH, publicKey);
+  readOneLine(f, MAX_ITEM_LENGTH, publicKey);
 
-	ignoreOneLine(f);
-	std::vector<std::string> verifKeys;
-	verifKeys.push_back("");    // signer 0 does not exist
-	for (int i = 0; i < n; i++)
-	{
-		readOneLine(f, MAX_ITEM_LENGTH, verifKey);
-		verifKeys.push_back(std::string(verifKey));
-	}
+  ignoreOneLine(f);
+  std::vector<std::string> verifKeys;
+  verifKeys.push_back("");  // signer 0 does not exist
+  for (int i = 0; i < n; i++) {
+    readOneLine(f, MAX_ITEM_LENGTH, verifKey);
+    verifKeys.push_back(std::string(verifKey));
+  }
 
-	// Ignore last comment/empty line
-	ignoreOneLine(f);
+  // Ignore last comment/empty line
+  ignoreOneLine(f);
 
-	return factory->newVerifier(k, n, publicKey, verifKeys);
+  return factory->newVerifier(k, n, publicKey, verifKeys);
 }
 
 
 static void ignoreThresholdSigner(FILE * f) {
-	ignoreOneLine(f);
-	ignoreOneLine(f);
+  ignoreOneLine(f);
+  ignoreOneLine(f);
 }
 
 
-static bool parseReplicaConfig(uint16_t replicaId, FILE* publicKeysFile, FILE* privateKeysFile, ReplicaConfig& outConfig)
-{
+static bool parseReplicaConfig(uint16_t replicaId,
+                               FILE* publicKeysFile,
+                               FILE* privateKeysFile,
+                               ReplicaConfig& outConfig) {
+  int tempInt = 0;
+  char tempString[MAX_ITEM_LENGTH];
 
-	int tempInt = 0;
-	char tempString[MAX_ITEM_LENGTH];
+  std::set <pair<uint16_t, string>> publicKeysOfReplicas;
 
-	std::set <pair<uint16_t, string>> publicKeysOfReplicas;
+  // read from publicKeysFile
 
-	// read from publicKeysFile
+  for (int i = 0; i < numOfReplicas; i++) {
+    fscanf(publicKeysFile, "%" MAX_ITEM_LENGTH_STR "s\n", tempString);
 
-	for (int i = 0; i < numOfReplicas; i++)
-	{
-		fscanf(publicKeysFile, "%" MAX_ITEM_LENGTH_STR "s\n", tempString);
+    string name(tempString);
 
-		string name(tempString);
+    string expectedName = string("replica") + std::to_string(i);
 
-		string expectedName = string("replica") + std::to_string(i);
+    if (expectedName != name) return false;
 
-		if (expectedName != name) return false;
+    fscanf(publicKeysFile, "%" MAX_ITEM_LENGTH_STR "s\n", tempString);
 
-		fscanf(publicKeysFile, "%" MAX_ITEM_LENGTH_STR "s\n", tempString);
+    string publicKeyString(tempString);
 
-		string publicKeyString(tempString);
+    publicKeysOfReplicas.insert({i, publicKeyString});
+  }
 
-		publicKeysOfReplicas.insert({i, publicKeyString});
-	}
+  // Read threshold signatures
+  IThresholdFactory* slowCommitFactory = nullptr;
+  IThresholdSigner* thresholdSignerForSlowPathCommit = nullptr;
+  IThresholdVerifier* thresholdVerifierForSlowPathCommit = nullptr;
 
-	// Read threshold signatures
-	IThresholdFactory* slowCommitFactory = nullptr;
-	IThresholdSigner* thresholdSignerForSlowPathCommit = nullptr;
-	IThresholdVerifier* thresholdVerifierForSlowPathCommit = nullptr;
+  IThresholdFactory* optimisticCommitFactory = nullptr;
+  IThresholdSigner* thresholdSignerForOptimisticCommit = nullptr;
+  IThresholdVerifier* thresholdVerifierForOptimisticCommit = nullptr;
 
-	IThresholdFactory* optimisticCommitFactory = nullptr;
-	IThresholdSigner* thresholdSignerForOptimisticCommit = nullptr;
-	IThresholdVerifier* thresholdVerifierForOptimisticCommit = nullptr;
+  slowCommitFactory = createThresholdFactory(publicKeysFile, numOfReplicas);
+  thresholdVerifierForSlowPathCommit =
+      createThresholdVerifier(publicKeysFile,
+                              numOfReplicas - fVal,
+                              numOfReplicas,
+                              slowCommitFactory);
 
-	slowCommitFactory = createThresholdFactory(publicKeysFile, numOfReplicas);
-	thresholdVerifierForSlowPathCommit = createThresholdVerifier(publicKeysFile, numOfReplicas - fVal, numOfReplicas, slowCommitFactory);
+  optimisticCommitFactory = createThresholdFactory(publicKeysFile,
+                                                   numOfReplicas);
+  thresholdVerifierForOptimisticCommit =
+      createThresholdVerifier(publicKeysFile,
+                              numOfReplicas,
+                              numOfReplicas,
+                              optimisticCommitFactory);
 
-	optimisticCommitFactory = createThresholdFactory(publicKeysFile, numOfReplicas);
-	thresholdVerifierForOptimisticCommit = createThresholdVerifier(publicKeysFile, numOfReplicas, numOfReplicas, optimisticCommitFactory);
+  // read from privateKeysFile
 
-	// read from privateKeysFile
+  fscanf(privateKeysFile, "%d\n", &tempInt);
+  if (tempInt != replicaId) return false;
 
-	fscanf(privateKeysFile, "%d\n", &tempInt);
-	if (tempInt != replicaId) return false;
+  fscanf(privateKeysFile, "%" MAX_ITEM_LENGTH_STR "s\n", tempString);
 
-	fscanf(privateKeysFile, "%" MAX_ITEM_LENGTH_STR "s\n", tempString);
+  string sigPrivateKey(tempString);
 
-	string sigPrivateKey(tempString);
+  thresholdSignerForSlowPathCommit =
+      createThresholdSigner(privateKeysFile, replicaId+1, slowCommitFactory);
 
-	thresholdSignerForSlowPathCommit = createThresholdSigner(privateKeysFile, replicaId+1, slowCommitFactory);
-	
-	thresholdSignerForOptimisticCommit = createThresholdSigner(privateKeysFile, replicaId+1, optimisticCommitFactory);
+  thresholdSignerForOptimisticCommit =
+      createThresholdSigner(privateKeysFile,
+                            replicaId+1,
+                            optimisticCommitFactory);
 
-	delete slowCommitFactory;
-	delete optimisticCommitFactory;
+  delete slowCommitFactory;
+  delete optimisticCommitFactory;
 
-	// set configuration
+  // set configuration
 
-	outConfig.replicaId = replicaId;
-	outConfig.fVal = 1;
-	outConfig.cVal = 0;
-	outConfig.numOfClientProxies = 1;
-	outConfig.statusReportTimerMillisec = 2000;
-	outConfig.concurrencyLevel = 1;
-	outConfig.autoViewChangeEnabled = false;
-	outConfig.viewChangeTimerMillisec = 60000;
+  outConfig.replicaId = replicaId;
+  outConfig.fVal = 1;
+  outConfig.cVal = 0;
+  outConfig.numOfClientProxies = 1;
+  outConfig.statusReportTimerMillisec = 2000;
+  outConfig.concurrencyLevel = 1;
+  outConfig.autoViewChangeEnabled = false;
+  outConfig.viewChangeTimerMillisec = 60000;
 
-	outConfig.publicKeysOfReplicas = publicKeysOfReplicas;
-	outConfig.replicaPrivateKey = sigPrivateKey;
+  outConfig.publicKeysOfReplicas = publicKeysOfReplicas;
+  outConfig.replicaPrivateKey = sigPrivateKey;
 
-	outConfig.thresholdSignerForExecution = nullptr;
-	outConfig.thresholdVerifierForExecution = nullptr;
-	
-	outConfig.thresholdSignerForSlowPathCommit = thresholdSignerForSlowPathCommit;
-	outConfig.thresholdVerifierForSlowPathCommit = thresholdVerifierForSlowPathCommit;
-	
-	outConfig.thresholdSignerForCommit = nullptr;
-	outConfig.thresholdVerifierForCommit = nullptr;
-	
-	outConfig.thresholdSignerForOptimisticCommit = thresholdSignerForOptimisticCommit;
-	outConfig.thresholdVerifierForOptimisticCommit = thresholdVerifierForOptimisticCommit;
+  outConfig.thresholdSignerForExecution = nullptr;
+  outConfig.thresholdVerifierForExecution = nullptr;
 
-	return true;
+  outConfig.thresholdSignerForSlowPathCommit =
+      thresholdSignerForSlowPathCommit;
+  outConfig.thresholdVerifierForSlowPathCommit =
+      thresholdVerifierForSlowPathCommit;
+
+  outConfig.thresholdSignerForCommit = nullptr;
+  outConfig.thresholdVerifierForCommit = nullptr;
+
+  outConfig.thresholdSignerForOptimisticCommit =
+      thresholdSignerForOptimisticCommit;
+  outConfig.thresholdVerifierForOptimisticCommit =
+      thresholdVerifierForOptimisticCommit;
+
+  return true;
 }
 
-void getReplicaConfig(uint16_t replicaId, ReplicaConfig& outConfig)
-{
-	FILE* publicKeysFile = fopen(nameOfPublicFile, "r");
-	if (!publicKeysFile) 
-		throw std::runtime_error("Unable to read public keys");
+void getReplicaConfig(uint16_t replicaId, ReplicaConfig& outConfig) {
+  FILE* publicKeysFile = fopen(nameOfPublicFile, "r");
+  if (!publicKeysFile)
+    throw std::runtime_error("Unable to read public keys");
 
-	FILE* privateKeysFile = fopen(namesOfPrivateFiles[replicaId], "r");
-	if (!privateKeysFile) 
-		throw std::runtime_error("Unable to read private keys");
+  FILE* privateKeysFile = fopen(namesOfPrivateFiles[replicaId], "r");
+  if (!privateKeysFile)
+    throw std::runtime_error("Unable to read private keys");
 
-	bool succ = parseReplicaConfig(replicaId, publicKeysFile, privateKeysFile, outConfig);
-	if (!succ)
-		throw std::runtime_error("Unable to parse configuration");
+  bool succ = parseReplicaConfig(
+      replicaId, publicKeysFile, privateKeysFile, outConfig);
+  if (!succ)
+    throw std::runtime_error("Unable to parse configuration");
 }
 
-PlainUdpConfig getUDPConfig(uint16_t id)
-{
-	std::string ip = "127.0.0.1";
-	uint16_t port = basePort + id*2;
-	uint32_t bufLength = 64000;
+PlainUdpConfig getUDPConfig(uint16_t id) {
+  std::string ip = "127.0.0.1";
+  uint16_t port = basePort + id*2;
+  uint32_t bufLength = 64000;
 
-	std::unordered_map <NodeNum, std::tuple<std::string, uint16_t>> nodes;
-	for (int i = 0; i < (numOfReplicas+1); i++)
-		nodes.insert({ i, std::make_tuple(ip, basePort + i*2) });
-	
-	PlainUdpConfig retVal(ip, port, bufLength, nodes);
-	
-	return retVal;
+  std::unordered_map <NodeNum, std::tuple<std::string, uint16_t>> nodes;
+  for (int i = 0; i < (numOfReplicas+1); i++)
+    nodes.insert({ i, std::make_tuple(ip, basePort + i*2) });
+
+  PlainUdpConfig retVal(ip, port, bufLength, nodes);
+
+  return retVal;
 }
--- bftengine/tests/simpleTest/replica.cpp
+++ bftengine/tests/simpleTest/replica.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <thread>
 
-#include "CommFactory.hpp" 
+#include "CommFactory.hpp"
 #include "ReplicaConfig.hpp"
 
 #include "commonDefs.h"
@@ -16,69 +16,73 @@ using namespace bftEngine;
 void getReplicaConfig(uint16_t replicaId, bftEngine::ReplicaConfig& outConfig);
 PlainUdpConfig getUDPConfig(uint16_t id);
 
-class SimpleAppState : public RequestsHandler
-{
-public:
-	virtual int execute(uint16_t clientId, bool readOnly, uint32_t requestSize, const char* request, uint32_t maxReplySize, char* outReply, uint32_t& outActualReplySize) override
-	{
-		if (readOnly) // read-only request
-		{
-			assert(requestSize == sizeof(uint64_t));
-			const uint64_t reqId = *((uint64_t*)request);
-			assert(reqId == READ_VAL_REQ);
+class SimpleAppState : public RequestsHandler {
+ public:
+  virtual int execute(uint16_t clientId,
+                      bool readOnly,
+                      uint32_t requestSize,
+                      const char* request,
+                      uint32_t maxReplySize,
+                      char* outReply,
+                      uint32_t& outActualReplySize) override {
+    if (readOnly) {
+      // read-only request
+      assert(requestSize == sizeof(uint64_t));
+      const uint64_t reqId = *((uint64_t*)request);
+      assert(reqId == READ_VAL_REQ);
 
-			assert(maxReplySize >= sizeof(uint64_t));
-			uint64_t* pRet = (uint64_t*)outReply;
-			*pRet = lastValue;
-			outActualReplySize = sizeof(uint64_t);
-		}
-		else // read-write request
-		{
-			assert(requestSize == 2 * sizeof(uint64_t));
-			const uint64_t* pReqId = (uint64_t*)request;
-			assert(*pReqId == SET_VAL_REQ);
-			const uint64_t* pReqVal = (pReqId + 1);
+      assert(maxReplySize >= sizeof(uint64_t));
+      uint64_t* pRet = (uint64_t*)outReply;
+      *pRet = lastValue;
+      outActualReplySize = sizeof(uint64_t);
+    } else {
+      assert(requestSize == 2 * sizeof(uint64_t));
+      const uint64_t* pReqId = (uint64_t*)request;
+      assert(*pReqId == SET_VAL_REQ);
+      const uint64_t* pReqVal = (pReqId + 1);
 
-			stateNum++;
-			lastValue = *pReqVal;
+      stateNum++;
+      lastValue = *pReqVal;
 
-			assert(maxReplySize >= sizeof(uint64_t));
-			uint64_t* pRet = (uint64_t*)outReply;
-			*pRet = stateNum;
-			outActualReplySize = sizeof(uint64_t);
-		}
+      assert(maxReplySize >= sizeof(uint64_t));
+      uint64_t* pRet = (uint64_t*)outReply;
+      *pRet = stateNum;
+      outActualReplySize = sizeof(uint64_t);
+    }
 
-		return 0;
-	}
+    return 0;
+  }
 
-protected:
-	// state
-	uint64_t stateNum = 0;
-	uint64_t lastValue = 0;
+ protected:
+  // state
+  uint64_t stateNum = 0;
+  uint64_t lastValue = 0;
 };
 
-int main(int argc, char **argv) 
-{
-	if (argc < 2) throw std::runtime_error("Unable to read replica id");
-	uint16_t id = (argv[1][0] - '0');
-	if (id >= 4) throw std::runtime_error("Illegal replica id");
+int main(int argc, char **argv) {
+  if (argc < 2) throw std::runtime_error("Unable to read replica id");
+  uint16_t id = (argv[1][0] - '0');
+  if (id >= 4) throw std::runtime_error("Illegal replica id");
 
-	ReplicaConfig replicaConfig;
-	getReplicaConfig(id, replicaConfig);
+  ReplicaConfig replicaConfig;
+  getReplicaConfig(id, replicaConfig);
 
-	PlainUdpConfig udpConf = getUDPConfig(id);
-	
+  PlainUdpConfig udpConf = getUDPConfig(id);
 
-	ICommunication* comm = PlainUDPCommunication::create(udpConf);
+  ICommunication* comm = PlainUDPCommunication::create(udpConf);
 
-	SimpleAppState simpleAppState;
+  SimpleAppState simpleAppState;
 
-	Replica* replica = Replica::createNewReplica(&replicaConfig, &simpleAppState, nullptr, comm, nullptr);
+  Replica* replica = Replica::createNewReplica(&replicaConfig,
+                                               &simpleAppState,
+                                               nullptr,
+                                               comm,
+                                               nullptr);
 
-	replica->start();
+  replica->start();
 
-	// wait forever...
-	while (true) std::this_thread::sleep_for(std::chrono::seconds(1)); 
-	
-	return 0;
+  // wait forever...
+  while (true) std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  return 0;
 }
