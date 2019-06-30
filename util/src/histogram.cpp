ac9b78196b179e0da89f18b1c542d300885576b5
Igor Golikov <igolikov@vmware.com>
2019-06-30 12:14:55 +0300

    license updated

--- util/src/histogram.cpp
+++ util/src/histogram.cpp
@@ -25,6 +25,35 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+// Concord
+//
+// Copyright (c) 2018 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0
+// License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+
+// This program implements a client that sends requests to the simple register
+// state machine defined in replica.cpp. It sends a preset number of operations
+// to the replicas, and occasionally checks that the responses match
+// expectations.
+//
+// Operations alternate:
+//
+//  1. `readMod-1` write operations, each with a unique value
+//    a. Every second write checks that the returned sequence number is as
+//       expected.
+//  2. Every `readMod`-th operation is a read, which checks that the value
+//     returned is the same as the last value written.
+//
+// The program expects no arguments. See the `scripts/` directory for
+// information about how to run the client.
+
 #include <math.h>
 #include <stdio.h>
 #include "histogram.hpp"
