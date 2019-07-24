f0b7f512d9a7fc5b9833bb8447f7cd0e3db1e6ab
Michael Wei <mwei@cs.ucsd.edu>
2019-07-23 17:41:38 -0700

    Fix formatting errors which result in clang warnings
    Signed-off-by: Michael Wei <mwei@cs.ucsd.edu>

--- bftengine/src/bftengine/ReplicaImp.cpp
+++ bftengine/src/bftengine/ReplicaImp.cpp
@@ -611,7 +611,7 @@ namespace bftEngine
 				if (absDifference(currTime, timeOfPartProof) < controller->timeToStartSlowPathMilli() * 1000)
 					break; // don't try the next seq numbers
 
-				LOG_INFO_F(GL, "Primary initiates slow path for seqNum=%" PRId64 " (currTime=%ld timeOfPartProof=%ld",
+				LOG_INFO_F(GL, "Primary initiates slow path for seqNum=%" PRId64 " (currTime=%llu timeOfPartProof=%llu",
 				           i, currTime, timeOfPartProof);
 
 				controller->onStartingSlowCommit(i);
@@ -2015,7 +2015,7 @@ namespace bftEngine
 			}
 
 			LOG_INFO_F(GL, "**************** In onNewView curView=%" PRId64 " (num of PPs=%ld, first safe seq=%" PRId64 ","
-                       " last safe seq=%" PRId64 ", lastStableSeqNum=%ld, lastExecutedSeqNum=%" PRId64 ","
+                       " last safe seq=%" PRId64 ", lastStableSeqNum=%lld, lastExecutedSeqNum=%" PRId64 ","
                        "stableLowerBoundWhenEnteredToView= %" PRId64 ")",
 				       curView, prePreparesForNewView.size(), firstPPSeq, lastPPSeq,
 				       lastStableSeqNum, lastExecutedSeqNum, viewsManager->stableLowerBoundWhenEnteredToView());
