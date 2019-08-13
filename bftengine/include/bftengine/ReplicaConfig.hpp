3195f6792b4d705c06bc34a8885e08300bae1bfc
yuliasherman <ysherman@vmware.com>
2019-08-13 03:31:41 -0700

    Make configuration parameters more readable.

--- bftengine/include/bftengine/ReplicaConfig.hpp
+++ bftengine/include/bftengine/ReplicaConfig.hpp
@@ -74,12 +74,12 @@ struct ReplicaConfig {
   bool debugPersistentStorageEnabled = false;
 
   // Messages
-  uint32_t maxExternalMessageSize = 1 << 16;
-  uint32_t maxReplyMessageSize = 1 << 13;
+  uint32_t maxExternalMessageSize = 65536;
+  uint32_t maxReplyMessageSize = 8192;
 
   // StateTransfer
-  uint32_t maxNumOfReservedPages = 1 << 11;
-  uint32_t sizeOfReservedPage = 1 << 12;
+  uint32_t maxNumOfReservedPages = 2048;
+  uint32_t sizeOfReservedPage = 4096;
 };
 
 }
