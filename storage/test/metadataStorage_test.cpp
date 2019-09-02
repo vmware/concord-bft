a0863d89be1810ca97d290c07524c883e3100c58
Yulia Sherman <47499870+yuliasherman@users.noreply.github.com>
2019-09-02 11:29:54 +0300

    Fix broken metadataStorageTest (#202)
    * Fix metadataStorageTest broken
    
    Fix issue #200 : initialize MetadataStorage before usage.
    
    * Review fix.

--- storage/test/metadataStorage_test.cpp
+++ storage/test/metadataStorage_test.cpp
@@ -13,6 +13,7 @@
 #include "storage/db_metadata_storage.h"
 #include "blockchain/db_types.h"
 #include "blockchain/db_adapter.h"
+#include "bftengine/MetadataStorage.hpp"
 
 using namespace std;
 
@@ -25,8 +26,9 @@ using concord::storage::DBMetadataStorage;
 namespace {
 
 DBMetadataStorage *metadataStorage = nullptr;
-const ObjectId initialObjectId = 1;
+const ObjectId initialObjectId = 2;
 const uint32_t initialObjDataSize = 80;
+const uint32_t maxObjDataSize = 200;
 const uint16_t objectsNum = 100;
 
 uint8_t *createAndFillBuf(size_t length) {
@@ -63,8 +65,7 @@ TEST(metadataStorage_test, single_read) {
   auto *inBuf = writeRandomData(initialObjectId, initialObjDataSize);
   auto *outBuf = new uint8_t[initialObjDataSize];
   uint32_t realSize = 0;
-  metadataStorage->read(initialObjectId, initialObjDataSize, (char *)outBuf,
-                        realSize);
+  metadataStorage->read(initialObjectId, initialObjDataSize, (char *)outBuf, realSize);
   ASSERT_TRUE(initialObjDataSize == realSize);
   ASSERT_TRUE(is_match(inBuf, outBuf, realSize));
   delete[] inBuf;
@@ -75,17 +76,16 @@ TEST(metadataStorage_test, multi_write) {
   metadataStorage->beginAtomicWriteOnlyTransaction();
   uint8_t *inBuf[objectsNum];
   uint8_t *outBuf[objectsNum];
-  uint32_t objectsDataSize[objectsNum] = {initialObjDataSize};
-  for (auto i = 0; i < objectsNum; i++) {
-    objectsDataSize[i] += i;
-    inBuf[i] = writeInTransaction(initialObjectId + i, objectsDataSize[i]);
+  uint32_t objectsDataSize[objectsNum];
+  for (ObjectId i = initialObjectId; i < objectsNum; i++) {
+    objectsDataSize[i] = initialObjDataSize + i;
+    inBuf[i] = writeInTransaction(i, objectsDataSize[i]);
     outBuf[i] = new uint8_t[objectsDataSize[i]];
   }
   metadataStorage->commitAtomicWriteOnlyTransaction();
   uint32_t realSize = 0;
-  for (ObjectId i = 0; i < objectsNum; i++) {
-    metadataStorage->read(initialObjectId + i, objectsDataSize[i],
-                          (char *)outBuf[i], realSize);
+  for (ObjectId i = initialObjectId; i < objectsNum; i++) {
+    metadataStorage->read(i, objectsDataSize[i], (char *)outBuf[i], realSize);
     ASSERT_TRUE(objectsDataSize[i] == realSize);
     ASSERT_TRUE(is_match(inBuf[i], outBuf[i], realSize));
     delete[] inBuf[i];
@@ -99,9 +99,16 @@ int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   KeyComparator* kk = new KeyComparator(new KeyManipulator);
   const string dbPath = "./metadataStorage_test_db";
+  remove(dbPath.c_str());
   Client *dbClient = new Client(dbPath, kk);
   dbClient->init();
   metadataStorage = new DBMetadataStorage(dbClient, KeyManipulator::generateMetadataKey);
+  bftEngine::MetadataStorage::ObjectDesc objectDesc[objectsNum];
+  for (uint32_t id = initialObjectId; id < objectsNum; ++id) {
+    objectDesc[id].id = id;
+    objectDesc[id].maxSize = maxObjDataSize;
+  }
+  metadataStorage->initMaxSizeOfObjects(objectDesc, objectsNum);
   int res = RUN_ALL_TESTS();
   return res;
 }
