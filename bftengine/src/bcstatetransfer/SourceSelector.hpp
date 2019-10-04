985e5d9da7f3925375217a37bbd5a1a7840fe2b8
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2019-10-04 15:17:58 -0700

    Split source selection from BC ST into own class
    This commit is part of a refactoring of BCStateTran to make the logic
    easier to understand. Ideally we separate out the low level
    implementation details from the high level control in the `processData()`
    loop.
    
    The goal was not to change behavior and all tests currently pass.
    
    This commit also properly loads relevant metrics on initialization of
    BCStateTran.
    
    Also fix an overflow bug similar to the one
    [here](https://github.com/vmware/concord-bft/pull/78).
    
    Additionally, Separate the recording of time of last sent fetch message
    from the time of source selection (including received item message).
    This allows detecting timeouts multiple times for retransmission before
    changing a source. This appears to be the intention of the original code
    since there are two separate timeouts:
     * config.fetchRetransmissionTimeoutMilli,
     * config.sourceReplicaReplacementTimeoutMilli
    
    A change was introduced in
    commit 1db691abe6c166cb5b8d534787a5bba4ce487c34 that mistakenly reset
    the source selection time when a retransmission timeout occurred, but
    this would mistakenly mean that the source would not ever be replaced
    from timeouts alone during fetching. This change instead resets the
    send time when a retransmission occurs, so you get multiple attempts at
    retransmission for the same source, and then it will be replaced when
    the source needs to be replaced because
    config.sourceReplicaReplacementTimeoutMilli expired.

---
+++ bftengine/src/bcstatetransfer/SourceSelector.hpp
@@ -0,0 +1,107 @@
+// Concord
+//
+// Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0
+// License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+#pragma once
+
+#include <random>
+#include <set>
+#include <stdint.h>
+#include <sstream>
+
+#include "assertUtils.hpp"
+
+namespace bftEngine {
+namespace SimpleBlockchainStateTransfer {
+namespace impl {
+
+static const uint16_t NO_REPLICA = UINT16_MAX;
+
+// Information about which current source is selected and which replicas are
+// preferred, as well as data that helps to select a current source replica.
+class SourceSelector {
+
+ public:
+  SourceSelector(std::set<uint16_t> allOtherReplicas,
+                 uint32_t retransmissionTimeoutMilli,
+                 uint32_t sourceReplicaReplacementTimeoutMilli)
+      : allOtherReplicas_(allOtherReplicas), randomGen_(std::random_device()()),
+        retransmissionTimeoutMilli_(retransmissionTimeoutMilli),
+        sourceReplacementTimeoutMilli_(sourceReplicaReplacementTimeoutMilli){}
+
+  bool hasSource() const;
+  void removeCurrentReplica();
+  void setAllReplicasAsPreferred();
+  void reset();
+  bool isReset() const;
+  bool retransmissionTimeoutExpired(uint64_t currTimeMilli) const;
+
+
+  // Return true if the source should be replaced, false otherwise.
+  bool shouldReplaceSource(uint64_t currTimeMilli, bool badDataFromCurrentSource) const;
+
+  // Replace the source.
+  void updateSource(uint64_t currTimeMilli);
+
+  // Reset the source selection time without actually changing the source
+  void setSourceSelectionTime(uint64_t currTimeMilli);
+
+  // Reset the time since last transmission
+  void setSendTime(uint64_t currTimeMilli);
+
+  // Create a list of ids of the form "0, 1, 4"
+  std::string preferredReplicasToString() const;
+
+  bool hasPreferredReplicas() const {
+    return !preferredReplicas_.empty();
+  }
+
+  bool noPreferredReplicas() const {
+    return preferredReplicas_.empty();
+  }
+
+  void addPreferredReplica(uint16_t replicaId) {
+    preferredReplicas_.insert(replicaId);
+  }
+
+  uint16_t numberOfPreferredReplicas() const {
+    return static_cast<uint16_t>(preferredReplicas_.size());
+  }
+
+  bool isPreferred(uint16_t replicaId) const {
+    return preferredReplicas_.count(replicaId) != 0;
+  }
+
+  uint16_t currentReplica() const {
+    return currentReplica_;
+  }
+
+
+
+ private:
+  uint64_t timeSinceSourceSelectedMilli(uint64_t currTimeMilli) const;
+  uint64_t timeSinceSendMilli(uint64_t currTimeMilli) const;
+  void selectSource(uint64_t currTimeMilli);
+
+  std::set<uint16_t> preferredReplicas_;
+  uint16_t currentReplica_ = NO_REPLICA;
+  uint64_t sourceSelectionTimeMilli_ = 0;
+  uint64_t sendTimeMilli_ = 0;
+  std::set<uint16_t> allOtherReplicas_;
+  std::mt19937 randomGen_;
+  uint32_t retransmissionTimeoutMilli_;
+  uint32_t sourceReplacementTimeoutMilli_;
+
+
+};
+}  // namespace impl
+}  // namespace SimpleBlockchainStateTransfer
+}  // namespace bftEngine
