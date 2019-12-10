59ba98eb12662196dedbf02af4c5a247a7ce024a
Toly Kournik <tkournik@tkournik-a01.vmware.com>
2019-12-10 19:38:55 +0200

    make valgrind happy (uninitialized memory)

--- kvbc/src/ClientImp.cpp
+++ kvbc/src/ClientImp.cpp
@@ -24,7 +24,7 @@ IClient* createClient(const ClientConfig& conf, bftEngine::ICommunication* comm)
   ClientImp* c = new ClientImp();
 
   c->config_ = conf;
-  c->replyBuf_ = (char*)std::malloc(conf.maxReplySize);
+  c->replyBuf_ = (char*)std::calloc(conf.maxReplySize, sizeof(char));
   c->seqGen_ = bftEngine::SeqNumberGeneratorForClientRequests::createSeqNumberGeneratorForClientRequests();
   c->comm_ = comm;
   c->bftClient_ = nullptr;
