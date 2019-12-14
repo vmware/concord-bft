c49e3f4d5a1e86dce6d727d45f308cd779aaa527
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2019-12-13 22:31:43 -0800

    Enhance SHA_256 wrapper to allow multiple updates
    Don't just allow a hash against a single buffer. Instead allow adding
    new data by calling update and then generating the hash. This is useful
    for lots of things, and required for efficient parent hashes in merkle trees.

--- util/test/sha3_256_tests.cpp
+++ util/test/sha3_256_tests.cpp
@@ -11,6 +11,10 @@
 // terms and conditions of the subcomponent's license, as noted in the
 // LICENSE file.
 
+#include <cstdio>
+#include <fstream>
+using namespace std;
+
 #include "gtest/gtest.h"
 #include "sha3_256.h"
 
@@ -19,10 +23,10 @@ using namespace concord::util;
 namespace {
 
 // Convert a lowercase string hash to an array
-std::array<uint8_t, SHA3_256::SIZE_IN_BYTES> string_to_array(const char* s) {
-  std::array<uint8_t, SHA3_256::SIZE_IN_BYTES> hash = {0};
+SHA3_256::Digest string_to_array(const char* s) {
+  SHA3_256::Digest hash = {0};
 
-  for (size_t i = 0; i < 64; i++) {
+  for (size_t i = 0; i < SHA3_256::SIZE_IN_BYTES * 2; i++) {
     auto c = s[i];
     Assert((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f'));
     uint8_t nibble = 0;
@@ -64,6 +68,39 @@ TEST(sha3_256_test, basic) {
   expected = string_to_array("c817e3a5067a8ff4641fe6c7001717302cd07265ced4f7735719bcafbf55315c");
   hash = sha3.digest("REM", 3);
   ASSERT_EQ(expected, hash);
+}
+
+TEST(sha3_256, update) {
+  auto sha3 = SHA3_256();
+  auto left = sha3.digest("artist", 6);
+  auto right = sha3.digest("REM", 3);
+
+  // Write the left and write hashes concatenated, so we can run a commandline
+  // program on them.
+  std::string filename = std::tmpnam(nullptr);
+  std::fstream fs;
+  fs.open(filename, std::fstream::out | std::fstream::binary);
+  fs.write((char*)left.data(), left.size());
+  fs.write((char*)right.data(), right.size());
+  fs.close();
+
+  // Run an openssl command to generate the result hash
+  std::string command = "openssl sha3-256 -r " + filename;
+  char output[SHA3_256::SIZE_IN_BYTES * 2];
+  FILE* cmdfile = popen(command.c_str(), "r");
+  auto result = fread(output, 1, SHA3_256::SIZE_IN_BYTES * 2, cmdfile);
+  ASSERT_EQ(result, SHA3_256::SIZE_IN_BYTES * 2);
+  pclose(cmdfile);
+  auto expected = string_to_array(output);
+
+  // Delete the tmpfile
+  ASSERT_EQ(0, remove(filename.c_str()));
+
+  sha3.init();
+  sha3.update(left.data(), left.size());
+  sha3.update(right.data(), right.size());
+
+  ASSERT_EQ(expected, sha3.finish());
 }
 
 }  // namespace
