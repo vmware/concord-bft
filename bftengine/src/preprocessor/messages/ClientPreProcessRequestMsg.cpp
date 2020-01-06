f57cf5340b3efd51c682367dd3556703cfa42382
yuliasherman <ysherman@vmware.com>
2020-01-06 04:31:59 -0800

    Introduce ClientPreProcessRequestMsg.

---
+++ bftengine/src/preprocessor/messages/ClientPreProcessRequestMsg.cpp
@@ -0,0 +1,36 @@
+// Concord
+//
+// Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License"). You may not use this product except in
+// compliance with the Apache 2.0 License.
+//
+// This product may include a number of subcomponents with separate copyright notices and license terms. Your use of
+// these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+
+#include "ClientPreProcessRequestMsg.hpp"
+#include "assertUtils.hpp"
+
+namespace preprocessor {
+
+using namespace std;
+using namespace bftEngine;
+
+bool ClientPreProcessRequestMsg::ToActualMsgType(MessageBase* inMsg, ClientPreProcessRequestMsg*& outMsg) {
+  Assert(inMsg->type() == MsgCode::ClientPreProcessRequest);
+  if (inMsg->size() < sizeof(ClientRequestMsgHeader)) return false;
+
+  auto* msg = (ClientPreProcessRequestMsg*)inMsg;
+  if (msg->size() < (sizeof(ClientRequestMsgHeader) + msg->msgBody()->requestLength)) return false;
+
+  outMsg = msg;
+  return true;
+}
+
+unique_ptr<MessageBase> ClientPreProcessRequestMsg::convertToClientRequestMsg() {
+  msgBody_->msgType = MsgCode::ClientRequest;
+  return unique_ptr<MessageBase>((MessageBase*)this);
+}
+
+}  // namespace preprocessor
---
+++ bftengine/src/preprocessor/messages/ClientPreProcessRequestMsg.hpp
@@ -0,0 +1,32 @@
+// Concord
+//
+// Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License"). You may not use this product except in
+// compliance with the Apache 2.0 License.
+//
+// This product may include a number of subcomponents with separate copyright notices and license terms. Your use of
+// these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+
+#pragma once
+
+#include "messages/ClientRequestMsg.hpp"
+#include <memory>
+
+namespace preprocessor {
+
+class ClientPreProcessRequestMsg : public ClientRequestMsg {
+ public:
+  ClientPreProcessRequestMsg(
+      NodeIdType sender, bool isReadOnly, uint64_t reqSeqNum, uint32_t requestLength, const char* request)
+      : ClientRequestMsg(sender, isReadOnly, reqSeqNum, reqSeqNum, request) {
+    msgBody_->msgType = MsgCode::ClientPreProcessRequest;
+  }
+
+  std::unique_ptr<MessageBase> convertToClientRequestMsg();
+
+  static bool ToActualMsgType(MessageBase* inMsg, ClientPreProcessRequestMsg*& outMsg);
+};
+
+}  // namespace preprocessor
