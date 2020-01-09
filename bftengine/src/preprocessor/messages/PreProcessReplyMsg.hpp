2a8c60224c672d5053d381528b522775eb13c5b0
yuliasherman <ysherman@vmware.com>
2020-01-09 03:40:43 -0800

    Introduce PreProcessRequest and PreProcessReply messages.

---
+++ bftengine/src/preprocessor/messages/PreProcessReplyMsg.hpp
@@ -0,0 +1,41 @@
+// Concord
+//
+// Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License"). You may not use this product except in
+// compliance with the Apache 2.0 License.
+//
+// This product may include a number of subcomponents with separate copyright notices and license terms. Your use of
+// these subcomponents is subject to the terms and conditions of the sub-component's license, as noted in the LICENSE
+// file.
+
+#pragma once
+
+#include "messages/MessageBase.hpp"
+
+namespace preprocessor {
+
+#pragma pack(push, 1)
+struct PreProcessReplyMsgHeader {
+  MessageBase::Header header;
+  ViewNum viewNum;
+  SeqNum reqSeqNum;
+  NodeIdType senderId;
+  uint32_t requestLength;
+};
+// The hash of pre-execution result is a part of the message body
+#pragma pack(pop)
+
+class PreProcessReplyMsg : public MessageBase {
+ public:
+  PreProcessReplyMsg(NodeIdType senderId, uint64_t reqSeqNum, ViewNum viewNum, uint32_t replyLength, const char* reply);
+
+  void setParams(NodeIdType senderId, ReqId reqSeqNum, ViewNum view, uint32_t replyLength);
+
+  static bool ToActualMsgType(MessageBase* inMsg, PreProcessReplyMsg*& outMsg);
+
+ private:
+  PreProcessReplyMsgHeader* msgBody() const { return ((PreProcessReplyMsgHeader*)msgBody_); }
+};
+
+}  // namespace preprocessor
