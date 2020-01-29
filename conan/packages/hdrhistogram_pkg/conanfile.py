7011b6efca4036d0cacd0f6640379eb9c0629097
Yehonatan Buchnik <yontyon@gmail.com>
2020-01-29 14:06:26 +0200

    Add basic support in conan local package creation
    Add basic support in conan local package creation

---
+++ conan/packages/hdrhistogram_pkg/conanfile.py
@@ -0,0 +1,48 @@
+from conans import ConanFile, CMake, tools
+
+
+class HdrhistogramConan(ConanFile):
+    scm = {
+        "type": "git",
+        "subfolder": ".",
+        "url": "https://github.com/HdrHistogram/HdrHistogram_c.git",
+        "revision": "0.9.12"
+    }
+    name = "hdr_histogram"
+    version = "0.9.12"
+    license = "BSD 2-Clause"
+    url = "http://github.com/HdrHistogram/HdrHistogram_c"
+    description = "A HDR histogram library"
+    topics = ("HDR histogram")
+    generators = "cmake"
+
+    def requirements(self):
+        self.requires("zlib/1.2.11@conan/stable")
+
+    def configure(self):
+        self.options["zlib"].shared = True
+
+    def source(self):
+        tools.replace_in_file("CMakeLists.txt", 'project("hdr_histogram")',
+                              '''project("hdr_histogram")
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()''')
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        # build_args = {"HDR_HISTOGRAM_BUILD_PROGRAMS": "ON"}
+        # cmake.configure()
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="src")
+        self.copy("*hdr_histogram.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so*", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["hdr_histogram"]
+
