b8c43b48820665eac0722a1aed6d9b5305ab9e10
Yehonatan Buchnik <yontyon@gmail.com>
2020-01-30 14:57:35 +0200

    Fix conan scrips

--- .conan/relic_pkg/conanfile.py
+++ .conan/relic_pkg/conanfile.py
@@ -46,7 +46,7 @@ conan_basic_setup()
         self.copy("*.h", dst="relic", src="include")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.so*", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
--- .conan/rocksdb_pkg/conanfile.py
+++ .conan/rocksdb_pkg/conanfile.py
@@ -48,7 +48,7 @@ class RocksdbConan(ConanFile):
         self.copy("*.h", dst="", src="include")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.so*", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
