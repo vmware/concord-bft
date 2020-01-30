128782ab49a71a34aecd87ad287a1e893b14eca7
Yehonatan Buchnik <yontyon@gmail.com>
2020-01-30 14:21:57 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env sh
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-# Nothing needed for macos
-echo "macos homebrew packages handled in .travis.yml"
-else
-# If on Linux, install necessary packages using apt
 sudo apt-get update
-sudo apt-get install -y ccache cmake clang-format \
-python3-pip python3-setuptools
-fi
-
-sudo apt-get update
-sudo apt-get install python3 python3-pip g++ cmake clang clang-format
+sudo apt-get install -y ccache cmake clang-format python3-pip python3-setuptools
 
 python3 -m pip install --upgrade wheel
 python3 -m pip install --upgrade conan
