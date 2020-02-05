c9ebb34b44f426bc04597219b9af05105006aac4
yuliasherman <ysherman@vmware.com>
2020-02-05 07:18:18 -0800

    Compile preprocessor directory as a library

---
+++ bftengine/src/preprocessor/tests/preprocessor_test.cpp
@@ -0,0 +1,24 @@
+// Copyright 2019 VMware, all rights reserved
+/**
+ * Test public functions for RocksDBMetadataStorage class.
+ */
+
+#include "Logger.hpp"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+namespace {
+
+TEST(preprocessor_test, first) {
+
+}
+
+}  // end namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+
+  int res = RUN_ALL_TESTS();
+  return res;
+}
