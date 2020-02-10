c0a9452d7bec2896f92637436dc5937746d6a25b
Lior Gal <eliorone@gmail.com>
2020-02-10 09:25:25 +0200

    Use log4cp always on travis (#387)
    Always use log4cpp for Travis CI runs

--- .travis/preinstall.sh
+++ .travis/preinstall.sh
@@ -18,5 +18,28 @@ source ~/.profile
 conan profile new default --detect
 conan profile update settings.compiler.libcxx=libstdc++11 default
 
+
+# build and install RocksDB and its dependencies
+if [ -n "$USE_ROCKSDB" ]; then
+    # Install RocksDB dependencies
+    sudo apt-get install -y libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev
+
+    # Install RocksDB
+    cd /tmp
+    wget --https-only https://github.com/facebook/rocksdb/archive/v5.7.3.tar.gz
+    tar -xzf v5.7.3.tar.gz
+    cd rocksdb-5.7.3
+    make shared_lib
+    sudo make install-shared
+fi
+
+if [ -n "$USE_LOG4CPP" ]; then
+    cd
+    cd log4cplus/
+    ./configure CXXFLAGS="--std=c++11"
+    make
+    sudo make install
+fi
+
 # trio is need for tests
 python3 -m pip install --upgrade trio
