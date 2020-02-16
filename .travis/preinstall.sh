86c0e9ec4d87ffbe4dafde5f0a6483f8171d839e
Igor Golikov <salieri11@gmail.com>
2020-02-16 21:02:24 +0200

    temp fix for libs3

--- .travis/preinstall.sh
+++ .travis/preinstall.sh
@@ -7,7 +7,7 @@ echo "macos homebrew packages handled in .travis.yml"
 else
 # If on Linux, install necessary packages using apt
 sudo apt-get update
-sudo apt-get install -y ccache cmake clang-format libgmp3-dev \
+sudo apt-get install -y ccache cmake clang-format libgmp3-dev s3-dev \
 python3-pip python3-setuptools
 fi
 
