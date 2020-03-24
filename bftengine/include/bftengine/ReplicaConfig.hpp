884ec58301e3dc9d37285c9cbccbf5c9bbf81ce1
yuliasherman <ysherman@vmware.com>
2020-03-24 03:47:17 -0700

    Fix review comments

--- bftengine/include/bftengine/ReplicaConfig.hpp
+++ bftengine/include/bftengine/ReplicaConfig.hpp
@@ -66,8 +66,8 @@ struct ReplicaConfig {
   // a time interval in milliseconds, represents the timeout for automatically replacing the primary
   uint16_t autoPrimaryRotationTimerMillisec = 0;
 
-  // preExecutionFeatureEnabled=0/1; preExecutionFeatureEnabled=1 enables the pre-execution feature
-  uint8_t preExecutionFeatureEnabled = 0;
+  // preExecutionFeatureEnabled=true enables the pre-execution feature
+  bool preExecutionFeatureEnabled = false;
 
   // a time interval in milliseconds represents the timeout for the detection of timed out pre-execution requests
   uint64_t preExecReqStatusCheckTimerMillisec = 5000;
@@ -144,7 +144,7 @@ class ReplicaConfigSingleton {
   std::set<std::pair<uint16_t, const std::string>> GetPublicKeysOfReplicas() const {
     return config_->publicKeysOfReplicas;
   }
-  uint8_t GetPreExecutionFeatureEnabled() const { return config_->preExecutionFeatureEnabled; }
+  bool GetPreExecutionFeatureEnabled() const { return config_->preExecutionFeatureEnabled; }
   uint64_t GetPreExecReqStatusCheckTimerMillisec() const { return config_->preExecReqStatusCheckTimerMillisec; }
   std::string GetReplicaPrivateKey() const { return config_->replicaPrivateKey; }
 
