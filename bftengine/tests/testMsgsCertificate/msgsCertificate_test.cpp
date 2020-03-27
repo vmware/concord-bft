9510c9d61e01ba9c08035ef4529853913f9d0511
oded282 <oded282@gmail.com>
2020-03-27 17:01:49 +0300

    Infrastructure for testing 'MgsCertificate'

---
+++ bftengine/tests/testMsgsCertificate/msgsCertificate_test.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(msgsCertificate_test, initial_test) {}
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
