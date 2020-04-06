95496ac88667016c420d2e44fba4a859f370508f
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2020-04-06 17:30:45 +0000

    Fix license notice

--- util/include/errnoString.hpp
+++ util/include/errnoString.hpp
@@ -6,7 +6,8 @@
 // compliance with the Apache 2.0 License.
 //
 // This product may include a number of subcomponents with separate copyright notices and license terms. Your use of
-// these subcompo
+// these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
 
 #pragma once
 
