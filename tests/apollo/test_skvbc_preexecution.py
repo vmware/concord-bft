b0bc022a2f8bf74b0d64754dafaff776135e00be
Yehonatan <yontyon@gmail.com>
2020-07-01 11:56:48 +0300

    address reviews

--- tests/apollo/test_skvbc_preexecution.py
+++ tests/apollo/test_skvbc_preexecution.py
@@ -367,7 +367,7 @@ class SkvbcPreExecutionTest(unittest.TestCase):
 
     async def issue_tracked_ops_to_the_system(self, tracker):
         try:
-            with trio.move_on_after(seconds=40):
-                await tracker.run_concurrent_ops(100, write_weight=1)
+            with trio.move_on_after(seconds=30):
+                await tracker.run_concurrent_ops(50, write_weight=1)
         except trio.TooSlowError:
             pass
