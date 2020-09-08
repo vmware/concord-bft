7a83fbdd58ec0907cba71c88a26ff0b6a9f09fca
Yehoantan <yontyon@gmail.com>
2020-09-08 14:25:07 +0300

    Add recovery tests for metadata db

--- bftengine/tests/metadataStorage/metadataStorage_test.cpp
+++ bftengine/tests/metadataStorage/metadataStorage_test.cpp
@@ -39,9 +39,12 @@ uint8_t *createAndFillBuf(size_t length) {
   return buffer;
 }
 
-uint8_t *writeRandomData(const ObjectId &objectId, const uint32_t &dataLen) {
+uint8_t *writeRandomData(const ObjectId &objectId, const uint32_t &dataLen, DBMetadataStorage *db = nullptr) {
+  if (db == nullptr) {
+    db = metadataStorage;
+  }
   uint8_t *data = createAndFillBuf(dataLen);
-  metadataStorage->atomicWrite(objectId, (char *)data, dataLen);
+  db->atomicWrite(objectId, (char *)data, dataLen);
   return data;
 }
 
@@ -49,6 +52,19 @@ uint8_t *writeInTransaction(const ObjectId &objectId, const uint32_t &dataLen) {
   uint8_t *data = createAndFillBuf(dataLen);
   metadataStorage->writeInBatch(objectId, (char *)data, dataLen);
   return data;
+}
+
+DBMetadataStorage *initiateMetadataStorage(Client *dbClient, const std::string &dbPath, bool remove_old_file) {
+  if (remove_old_file) remove(dbPath.c_str());
+  DBMetadataStorage *metadataStorage_ =
+      new DBMetadataStorage(dbClient, std::make_unique<concord::storage::v1DirectKeyValue::MetadataKeyManipulator>());
+  bftEngine::MetadataStorage::ObjectDesc objectDesc[objectsNum];
+  for (uint32_t id = initialObjectId; id < objectsNum; ++id) {
+    objectDesc[id].id = id;
+    objectDesc[id].maxSize = maxObjDataSize;
+  }
+  metadataStorage_->initMaxSizeOfObjects(objectDesc, objectsNum);
+  return metadataStorage_;
 }
 
 bool is_match(const uint8_t *exp, const uint8_t *actual, const size_t len) {
@@ -92,22 +108,64 @@ TEST(metadataStorage_test, multi_write) {
   }
 }
 
+uint8_t *createUpdateAndCloseDB(Client *client, bool clear_db = false) {
+  auto db = initiateMetadataStorage(client, "./metadataStorage_test_db_recover", true);
+  auto *inBuf = writeRandomData(initialObjectId, initialObjDataSize, db);
+  if (clear_db) {
+    db->setDontLoadStorageOnStartupFlag();
+  }
+  delete db;
+  return inBuf;
+}
+
+TEST(metadataStorage_test, recovering_test) {
+  Client *dbClient_ = new Client("./metadataStorage_test_db_recover", make_unique<KeyComparator>(new DBKeyComparator));
+  dbClient_->init();
+  // We create a db in a different path and close it after writing a single object.
+  auto inBuf = createUpdateAndCloseDB(dbClient_);
+
+  // We will now verify that the written object is available if we load the DB from the file.
+  auto db = initiateMetadataStorage(dbClient_, "./metadataStorage_test_db_recover", false);
+
+  auto *outBuf = new uint8_t[initialObjDataSize];
+  uint32_t realSize = 0;
+  db->read(initialObjectId, initialObjDataSize, (char *)outBuf, realSize);
+  ASSERT_TRUE(initialObjDataSize == realSize);
+  ASSERT_TRUE(is_match(inBuf, outBuf, realSize));
+
+  delete[] outBuf;
+  delete[] inBuf;
+  delete db;
+  delete dbClient_;
+}
+
+TEST(metadataStorage_test, clear_db_test) {
+  Client *dbClient_ = new Client("./metadataStorage_test_db_recover", make_unique<KeyComparator>(new DBKeyComparator));
+  dbClient_->init();
+  // We create a db in a different path and close it after writing a single object. Also, we tell the DB to delete its
+  // metadata before closing.
+  auto inBuf = createUpdateAndCloseDB(dbClient_, true);
+
+  // We will now verify that the written object is not available if we load the DB from the file.
+  auto db = initiateMetadataStorage(dbClient_, "./metadataStorage_test_db_recover", false);
+
+  auto *outBuf = new uint8_t[initialObjDataSize];
+  uint32_t realSize = 0;
+  db->read(initialObjectId, initialObjDataSize, (char *)outBuf, realSize);
+  ASSERT_TRUE(realSize == 0);
+
+  delete[] outBuf;
+  delete[] inBuf;
+  delete db;
+  delete dbClient_;
+}
 }  // end namespace
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  const string dbPath = "./metadataStorage_test_db";
-  remove(dbPath.c_str());
-  Client *dbClient = new Client(dbPath, make_unique<KeyComparator>(new DBKeyComparator));
+  Client *dbClient = new Client("./metadataStorage_test_db", make_unique<KeyComparator>(new DBKeyComparator));
   dbClient->init();
-  metadataStorage =
-      new DBMetadataStorage(dbClient, std::make_unique<concord::storage::v1DirectKeyValue::MetadataKeyManipulator>());
-  bftEngine::MetadataStorage::ObjectDesc objectDesc[objectsNum];
-  for (uint32_t id = initialObjectId; id < objectsNum; ++id) {
-    objectDesc[id].id = id;
-    objectDesc[id].maxSize = maxObjDataSize;
-  }
-  metadataStorage->initMaxSizeOfObjects(objectDesc, objectsNum);
+  metadataStorage = initiateMetadataStorage(dbClient, "./metadataStorage_test_db", true);
   int res = RUN_ALL_TESTS();
   return res;
 }
