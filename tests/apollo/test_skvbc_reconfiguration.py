3f8fbe024014971a9cb26c7e509e13713b668cf9
teoparvanov <tparvanov@vmware.com>
2020-09-23 15:32:58 +0300

    [Apollo] Skip unstable reconfiguration tests (#898)
    Issue tracked under BC-4663.

--- tests/apollo/test_skvbc_reconfiguration.py
+++ tests/apollo/test_skvbc_reconfiguration.py
@@ -46,6 +46,8 @@ def start_replica_cmd(builddir, replica_id):
 
 
 class SkvbcReconfigurationTest(unittest.TestCase):
+
+    @unittest.skip("Unstable due to BC-4663")
     @with_trio
     @with_bft_network(start_replica_cmd, selected_configs=lambda n, f, c: n == 7)
     async def test_wedge_command(self, bft_network):
@@ -108,6 +110,7 @@ class SkvbcReconfigurationTest(unittest.TestCase):
 
         await self.validate_stop_on_super_stable_checkpoint(bft_network, skvbc)
 
+    @unittest.skip("Unstable due to BC-4663")
     @with_trio
     @with_bft_network(start_replica_cmd, selected_configs=lambda n, f, c: n == 7)
     async def test_wedge_command_and_specific_replica_info(self, bft_network):
@@ -187,6 +190,7 @@ class SkvbcReconfigurationTest(unittest.TestCase):
         await self.validate_state_consistency(skvbc, key, val)
         await skvbc.write_known_kv()
 
+    @unittest.skip("Unstable due to BC-4663")
     @with_trio
     @with_bft_network(start_replica_cmd, selected_configs=lambda n, f, c: n == 7)
     async def test_remove_nodes(self, bft_network):
