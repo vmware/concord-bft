5fe9906d028ab7e5ce379ee6dad84e6754c013dd
Yehoantan <yontyon@gmail.com>
2020-11-25 12:55:46 +0200

    add a comment

--- storage/include/storage/storage_metrics.h
+++ storage/include/storage/storage_metrics.h
@@ -29,6 +29,8 @@ class InMemoryStorageMetrics {
   concordMetrics::AtomicCounterHandle total_written_bytes_;
 
  private:
+  // update_metrics_ has to be the last declared member due to some synchronization issue. If you have to put something
+  // after it, then explicit release it on the destructor
   std::unique_ptr<concord::util::PeriodicCall> update_metrics_ = nullptr;
 
  public:
@@ -63,6 +65,8 @@ class RocksDbStorageMetrics {
 
   std::shared_ptr<::rocksdb::SstFileManager> sstFm;
   std::shared_ptr<::rocksdb::Statistics> statistics;
+  // update_metrics_ has to be the last declared member due to some synchronization issue. If you have to put something
+  // after it, then explicit release it on the destructor
   std::unique_ptr<concord::util::PeriodicCall> update_metrics_ = nullptr;
 
  public:
@@ -92,7 +96,6 @@ class RocksDbStorageMetrics {
                                ::rocksdb::Tickers::COMPACT_WRITE_BYTES,
                                ::rocksdb::Tickers::FLUSH_WRITE_BYTES,
                                ::rocksdb::Tickers::STALL_MICROS}) {}
-
   void setAggregator(std::shared_ptr<concordMetrics::Aggregator> aggregator) {
     rocksdb_comp_.SetAggregator(aggregator);
   }
