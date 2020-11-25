09cb55c4e27a0e90408e93c386e658c5b88aa8ec
Yehoantan <yontyon@gmail.com>
2020-11-25 19:58:35 +0200

    foramt

--- storage/include/storage/storage_metrics.h
+++ storage/include/storage/storage_metrics.h
@@ -93,7 +93,7 @@ class RocksDbStorageMetrics {
                                ::rocksdb::Tickers::COMPACT_WRITE_BYTES,
                                ::rocksdb::Tickers::FLUSH_WRITE_BYTES,
                                ::rocksdb::Tickers::STALL_MICROS}) {}
-  ~RocksDbStorageMetrics() { update_metrics_.reset();}
+  ~RocksDbStorageMetrics() { update_metrics_.reset(); }
   void setAggregator(std::shared_ptr<concordMetrics::Aggregator> aggregator) {
     rocksdb_comp_.SetAggregator(aggregator);
   }
