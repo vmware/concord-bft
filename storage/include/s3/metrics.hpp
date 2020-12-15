7e9a3d2e7ff5935106c28ff9b8a6416112cfe9f1
Tsvetomir Dimitrov <tsv.dimitrov@gmail.com>
2020-12-15 13:13:44 +0000

    Use gauge for last_saved_block_id S3 metric
    Gauge is more suitable type for the metric as it represents a numeric
    value.

--- storage/include/s3/metrics.hpp
+++ storage/include/s3/metrics.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
-#include <Metrics.hpp>
-#include <sliver.hpp>
-
+#include "Metrics.hpp"
+#include "sliver.hpp"
+#include "Logger.hpp"
 namespace concord::storage::s3 {
 
-struct Metrics {
+class Metrics {
+  public:
   Metrics()
       : metrics_component_{concordMetrics::Component("s3", std::make_shared<concordMetrics::Aggregator>())},
         numKeysTransferred{metrics_component_.RegisterCounter("keys_transferred")},
@@ -13,7 +14,7 @@ struct Metrics {
             metrics_component_.RegisterCounter("bytes_transferred"),
         },
         lastSavedBlockId_{
-            metrics_component_.RegisterStatus("last_saved_block_id", "0"),
+            metrics_component_.RegisterGauge("last_saved_block_id", 0),
         }
 
   {
@@ -35,10 +36,23 @@ struct Metrics {
     // the format is: "PREFIX/BLOCK_ID/KEY", where PREFIX is optional
     if (elems.size() < 2) return;
 
-    lastSavedBlockId_.Get().Set(elems[elems.size() - 2]);
+    uint64_t lastSavedBlockVal = 0;
+    try {
+      lastSavedBlockVal = stoull(elems[elems.size() - 2]);
+    } catch(std::invalid_argument& e) {
+      LOG_ERROR(logger_, "Can't convert lastSavedBlockId (" << elems[elems.size() - 2] << ") to numeric value.");
+      return;
+    } catch(std::out_of_range& e) {
+      LOG_ERROR(logger_, "lastSavedBlockId value (" << elems[elems.size() - 2] << ") doesn't fit in unsigned 64bit integer.");
+      return;
+    } catch(std::exception& e) {
+      LOG_ERROR(logger_, "Unexpected error occured while converting lastSavedBlockId (" << elems[elems.size() - 2] << ") to numeric value.");
+      return;
+    }
+    lastSavedBlockId_.Get().Set(lastSavedBlockVal);
   }
 
-  std::string getLastSavedBlockId() { return lastSavedBlockId_.Get().Get(); }
+  uint64_t getLastSavedBlockId() { return lastSavedBlockId_.Get().Get(); }
 
   concordMetrics::Component metrics_component_;
 
@@ -50,6 +64,8 @@ struct Metrics {
   // In the latter case it updates the metric
   bool isBlockKey(std::string_view key) { return key.find("metadata") == std::string_view::npos; }
 
-  concordMetrics::StatusHandle lastSavedBlockId_;
+  concordMetrics::GaugeHandle lastSavedBlockId_;
+
+  logging::Logger logger_ = logging::getLogger("concord.storage.s3.metrics");
 };
 }  // namespace concord::storage::s3
