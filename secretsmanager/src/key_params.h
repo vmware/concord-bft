d82b29e100bb8c77d7b17537e081658cf8550bfa
Kostas Teofanidis <kteofanidis@vmware.com>
2021-02-25 08:34:21 +0000

    SecretsManager implementation
    SecretsManagerEnc handles encrypted files reading and writing. It is
    initialised with a symmetric key derived from a password. Secrets API is
    used to obtain encryption and digest algorithm, key size and password
    (used for secret key derivation).
    
    SecretsManagerPlain is a convenience class. It implements the same
    interface as SecretsManagerEnc, but doesn't do any
    encryption/decryption. It's purpose is to act as transparent
    SecretsManagerEnc replacement for the cases when encryption is not used.

---
+++ secretsmanager/src/key_params.h
@@ -0,0 +1,30 @@
+// Concord
+//
+// Copyright (c) 2020 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the
+// "License").  You may not use this product except in compliance with the
+// Apache 2.0 License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+//
+// This convenience header combines different block implementations.
+
+#pragma once
+
+#include <vector>
+#include <stdint.h>
+
+namespace concord::secretsmanager {
+
+struct KeyParams {
+  KeyParams(const size_t key_size, const size_t iv_size) : key(key_size), iv(iv_size) {}
+
+  std::vector<uint8_t> key;
+  std::vector<uint8_t> iv;
+};
+
+}  // namespace concord::secretsmanager
