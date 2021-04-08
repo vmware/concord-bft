fecbb1cddd84e832ad3f1c8bc45f7a57d2dde42f
Lior <eliorone@gmail.com>
2021-04-08 09:25:36 -0700

    Fix issues with new client batching

--- bftclient/src/bft_client.cpp
+++ bftclient/src/bft_client.cpp
@@ -244,6 +244,7 @@ SeqNumToReplyMap Client::sendBatch(std::deque<WriteRequest>& write_requests, con
   if (replies.size() == pending_requests_.size()) {
     expected_commit_time_ms_.add(
         std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count());
+    pending_requests_.clear();
     return replies;
   }
 
@@ -280,6 +281,7 @@ void Client::wait(SeqNumToReplyMap& replies) {
     for (auto&& reply : unmatched_requests) {
       auto request = reply_certificates_.find(reply.metadata.seq_num);
       if (request == reply_certificates_.end()) continue;
+      if (replies.size() == pending_requests_.size()) return;
       if (auto match = request->second.onReply(std::move(reply))) {
         primary_ = match->primary;
         replies.insert(std::make_pair(request->first, match->reply));
@@ -287,7 +289,12 @@ void Client::wait(SeqNumToReplyMap& replies) {
       }
     }
   }
-  if (!reply_certificates_.empty()) primary_ = std::nullopt;
+  if (!reply_certificates_.empty()) {
+    primary_ = std::nullopt;
+    for (auto& request : reply_certificates_) {
+      request.second.clearReplies();
+    }
+  }
 }
 
 MatchConfig Client::writeConfigToMatchConfig(const WriteConfig& write_config) {
