f77edb81b4f76b7d16c7b2fabe82fb6fa0ecace2
Gil Levkovich <glevkovich@vmware.com>
2021-05-04 14:31:38 +0300

    SigManager: add Metrics counters and replicasInfo
    Here we add 5 metrics of 3 types of outcomes when verifying messages:
    1) Verification success/failure for replicas messages (2 counters)
    2) Verification success/failure for external clients messages
        (2 counters)
    3) A counter for unrecognized principal id (failure to verify)
    
    We need replicaInfo to be able to distinguish between principal ids
    of different types, so it is passed in ctor.

--- bftengine/src/bftengine/SigManager.cpp
+++ bftengine/src/bftengine/SigManager.cpp
@@ -12,6 +12,7 @@
 #include "SigManager.hpp"
 #include "Crypto.hpp"
 #include "assertUtils.hpp"
+#include "ReplicasInfo.hpp"
 
 #include <vector>
 #include <algorithm>
@@ -27,13 +28,14 @@ SigManager* SigManager::initImpl(ReplicaId myId,
                                  KeyFormat replicasKeysFormat,
                                  const std::set<std::pair<const std::string, std::set<uint16_t>>>* publicKeysOfClients,
                                  KeyFormat clientsKeysFormat,
-                                 uint16_t numReplicas,
-                                 uint16_t numRoReplicas,
-                                 uint16_t numOfClientProxies,
-                                 uint16_t numOfExternalClients) {
+                                 ReplicasInfo& replicasInfo) {
   vector<pair<Key, KeyFormat>> publickeys;
   map<PrincipalId, SigManager::KeyIndex> publicKeysMapping;
   size_t lowBound, highBound;
+  auto numReplicas = replicasInfo.getNumberOfReplicas();
+  auto numRoReplicas = replicasInfo.getNumberOfRoReplicas();
+  auto numOfClientProxies = replicasInfo.getNumOfClientProxies();
+  auto numOfExternalClients = replicasInfo.getNumberOfExternalClients();
 
   LOG_INFO(
       GL,
@@ -54,6 +56,8 @@ SigManager* SigManager::initImpl(ReplicaId myId,
 
   if (publicKeysOfClients) {
     // Multiple clients might be signing with the same private key (1 to many relation)
+    // Also, we do not enforce to have all range between [lowBound, highBound] construcred. We might want to have less
+    // principal ids mapped to keys than what is stated in the range.
     lowBound = numRoReplicas + numReplicas + numOfClientProxies;
     highBound = lowBound + numOfExternalClients - 1;
     for (const auto& p : (*publicKeysOfClients)) {
@@ -73,7 +77,8 @@ SigManager* SigManager::initImpl(ReplicaId myId,
                         make_pair(mySigPrivateKey, replicasKeysFormat),
                         publickeys,
                         publicKeysMapping,
-                        (publicKeysOfClients != nullptr));
+                        (publicKeysOfClients != nullptr),
+                        replicasInfo);
 }
 
 SigManager* SigManager::init(ReplicaId myId,
@@ -82,20 +87,14 @@ SigManager* SigManager::init(ReplicaId myId,
                              KeyFormat replicasKeysFormat,
                              const std::set<std::pair<const std::string, std::set<uint16_t>>>* publicKeysOfClients,
                              KeyFormat clientsKeysFormat,
-                             uint16_t numReplicas,
-                             uint16_t numRoReplicas,
-                             uint16_t numOfClientProxies,
-                             uint16_t numOfExternalClients) {
+                             ReplicasInfo& replicasInfo) {
   SigManager* sm = initImpl(myId,
                             mySigPrivateKey,
                             publicKeysOfReplicas,
                             replicasKeysFormat,
                             publicKeysOfClients,
                             clientsKeysFormat,
-                            numReplicas,
-                            numRoReplicas,
-                            numOfClientProxies,
-                            numOfExternalClients);
+                            replicasInfo);
   return SigManager::getInstance(sm);
 }
 
@@ -104,12 +103,26 @@ SigManager::SigManager(PrincipalId myId,
                        const pair<Key, KeyFormat>& mySigPrivateKey,
                        const vector<pair<Key, KeyFormat>>& publickeys,
                        const map<PrincipalId, KeyIndex>& publicKeysMapping,
-                       bool clientTransactionSigningEnabled)
-    : myId_(myId), clientTransactionSigningEnabled_(clientTransactionSigningEnabled) {
+                       bool clientTransactionSigningEnabled,
+                       ReplicasInfo& replicasInfo)
+    : myId_(myId),
+      clientTransactionSigningEnabled_(clientTransactionSigningEnabled),
+      replicasInfo_(replicasInfo),
+
+      metrics_component_{
+          concordMetrics::Component("signature_manager", std::make_shared<concordMetrics::Aggregator>())},
+
+      metrics_{
+          metrics_component_.RegisterAtomicCounter("external_client_request_signature_verification_failed"),
+          metrics_component_.RegisterAtomicCounter("external_client_request_signatures_verified"),
+          metrics_component_.RegisterAtomicCounter("peer_replicas_signature_verification_failed"),
+          metrics_component_.RegisterAtomicCounter("peer_replicas_signatures_verified"),
+          metrics_component_.RegisterAtomicCounter("signature_verification_failed_on_unrecognized_participant_id")},
+      updateAggregatorCounter(0) {
   map<KeyIndex, RSAVerifier*> publicKeyIndexToVerifier;
   size_t numPublickeys = publickeys.size();
+
   ConcordAssert(publicKeysMapping.size() >= numPublickeys);
-  ConcordAssert(numPublickeys + 1 >= numReplicas);
   mySigner_ = new RSASigner(mySigPrivateKey.first.c_str(), mySigPrivateKey.second);
   for (const auto& p : publicKeysMapping) {
     ConcordAssert(verifiers_.count(p.first) == 0);
@@ -125,6 +138,8 @@ SigManager::SigManager(PrincipalId myId,
       verifiers_[p.first] = iter->second;
   }
 
+  metrics_component_.Register();
+
   // This is done mainly for debugging and sanity check:
   // compute a vector which counts how many participants and which are per each key:
   vector<set<PrincipalId>> keyIndexToPrincipalIds(publickeys.size());
@@ -139,6 +154,7 @@ SigManager::SigManager(PrincipalId myId,
              "Key index " << i << " is used by " << ids.size() << " principal IDs"
                           << " from " << (*std::min_element(ids.begin(), ids.end())) << " to "
                           << (*std::max_element(ids.begin(), ids.end())));
+    ++i;
   }
 
   LOG_INFO(GL,
@@ -176,14 +192,41 @@ uint16_t SigManager::getSigLength(PrincipalId pid) const {
 bool SigManager::verifySig(
     PrincipalId pid, const char* data, size_t dataLength, const char* sig, uint16_t sigLength) const {
   auto pos = verifiers_.find(pid);
+  bool idOfPeerReplica = false, idOfExternalClient = false, result;
+
   if (pos == verifiers_.end()) {
     LOG_ERROR(GL, "Unrecognized pid " << pid);
+    metrics_.sigVerificationFailedOnUnrecognizedParticipantId_.Get().Inc();
+    metrics_component_.UpdateAggregator();
+    updateAggregatorCounter = 0;
     return false;
   }
 
   RSAVerifier* verifier = pos->second;
-  bool res = verifier->verify(data, dataLength, sig, sigLength);
-  return res;
+  result = verifier->verify(data, dataLength, sig, sigLength);
+  idOfExternalClient = replicasInfo_.isIdOfExternalClient(pid);
+  if (!idOfExternalClient) {
+    idOfPeerReplica = replicasInfo_.isIdOfPeerReplica(pid);
+  }
+  ConcordAssert(idOfPeerReplica || idOfExternalClient);
+  if (!result) {  // failure
+    metrics_component_.UpdateAggregator();
+    updateAggregatorCounter = 0;
+    if (idOfExternalClient)
+      metrics_.externalClientReqSigVerificationFailed_.Get().Inc();
+    else
+      metrics_.replicaSigVerificationFailed_.Get().Inc();
+  } else {  // success
+    if (idOfExternalClient)
+      metrics_.externalClientReqSigVerified_.Get().Inc();
+    else
+      metrics_.replicaSigVerified_.Get().Inc();
+    ++updateAggregatorCounter;
+    if ((updateAggregatorCounter % updateMetricsAggregatorThresh) == 0) {
+      metrics_component_.UpdateAggregator();
+    }
+  }
+  return result;
 }
 
 void SigManager::sign(const char* data, size_t dataLength, char* outSig, uint16_t outSigLength) const {
