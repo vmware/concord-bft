581925d59ac513f7083c511bd7bae183d7aadce3
robem <robem@posteo.de>
2021-05-12 18:48:35 +0000

    Clientservice: gRPC scaffolding
    Introduce gRPC server with the two services defined in concord_client.proto.
    Both services' methods are empty and will be implemented in a future change.
    However, with this change we got the gRPC scaffolding out of the way.

---
+++ client/clientservice/src/request_service.cpp
@@ -0,0 +1,29 @@
+// Concord
+//
+// Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License"). You may not use this product except in
+// compliance with the Apache 2.0 License.
+//
+// This product may include a number of subcomponents with separate copyright notices and license terms. Your use of
+// these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+
+#include <iostream>
+
+#include "request_service.hpp"
+
+using grpc::Status;
+using grpc::ServerContext;
+
+using vmware::concord::client::v1::Request;
+using vmware::concord::client::v1::Response;
+
+namespace concord::client {
+
+Status RequestServiceImpl::Send(ServerContext* context, const Request* request, Response* response) {
+  std::cout << "RequestService::Send called" << std::endl;
+  return grpc::Status::OK;
+}
+
+}  // namespace concord::client
