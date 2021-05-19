87b9777fddb0eb9ddf5761406d54a61284ceb333
Yehonatan Buchnik <yon_b@campus.technion.ac.il>
2021-05-19 13:41:18 +0300

    Add configuration for pruning metrics dump (#1480)
    In this PR we add an option to configure the timeout for dumping the deletes metrics in concordbft

--- bftengine/include/bftengine/ReplicaConfig.hpp
+++ bftengine/include/bftengine/ReplicaConfig.hpp
@@ -130,6 +130,7 @@ class ReplicaConfig : public concord::serialize::SerializableFactory<ReplicaConf
   CONFIG_PARAM(numBlocksToKeep_, uint64_t, 0, "how much blocks to keep while pruning");
 
   CONFIG_PARAM(debugPersistentStorageEnabled, bool, false, "whether persistent storage debugging is enabled");
+  CONFIG_PARAM(deleteMetricsDumpInterval, uint64_t, 300, "delete metrics dump interval (s)");
 
   // Messages
   CONFIG_PARAM(maxExternalMessageSize, uint32_t, 131072, "maximum size of external message");
@@ -216,6 +217,7 @@ class ReplicaConfig : public concord::serialize::SerializableFactory<ReplicaConf
     serialize(outStream, numBlocksToKeep_);
 
     serialize(outStream, debugPersistentStorageEnabled);
+    serialize(outStream, deleteMetricsDumpInterval);
     serialize(outStream, maxExternalMessageSize);
     serialize(outStream, maxReplyMessageSize);
     serialize(outStream, maxNumOfReservedPages);
@@ -277,6 +279,7 @@ class ReplicaConfig : public concord::serialize::SerializableFactory<ReplicaConf
     deserialize(inStream, numBlocksToKeep_);
 
     deserialize(inStream, debugPersistentStorageEnabled);
+    deserialize(inStream, deleteMetricsDumpInterval);
     deserialize(inStream, maxExternalMessageSize);
     deserialize(inStream, maxReplyMessageSize);
     deserialize(inStream, maxNumOfReservedPages);
