817ae83baea397082a9de4ccab581afd4518f8ac
Nikhil Kumar <nkumar4@vmware.com>
2021-05-26 08:09:23 -0700

    Return valid error message for addRemoveStatus
    AddRemove status command should return a valid error message if key is not found in the block chain

--- tests/apollo/test_skvbc_reconfiguration.py
+++ tests/apollo/test_skvbc_reconfiguration.py
@@ -555,6 +555,30 @@ class SkvbcReconfigurationTest(unittest.TestCase):
             status = cmf_msgs.ReconfigurationResponse.deserialize(r)[0]
             assert status.response.reconfiguration == test_config
 
+    @with_trio
+    @with_bft_network(start_replica_cmd, selected_configs=lambda n, f, c: n == 7)
+    async def test_addRemoveStatusError(self, bft_network):
+        """
+             Sends a addRemoveStatus command without adding new configuration 
+             and checks that replicas respond with valid error message.
+             Note that in this test we assume no failures and synchronized network.
+             The test does the following:
+             1. A client sends a addRemoveStatus command
+             2. The client verifies status returns a valid error message
+         """
+        bft_network.start_all_replicas()
+        skvbc = kvbc.SimpleKVBCProtocol(bft_network)
+        for i in range(100):
+            await skvbc.write_known_kv()
+        client = bft_network.random_client()
+        checkpoint_before = await bft_network.wait_for_checkpoint(replica_id=0)
+        op = operator.Operator(bft_network.config, client,  bft_network.builddir)
+        await op.add_remove_status()
+        rsi_rep = client.get_rsi_replies()
+        for r in rsi_rep.values():
+            status = cmf_msgs.ReconfigurationResponse.deserialize(r)[0]
+            assert status.response.error_msg == 'key_not_found'
+
 
     async def validate_stop_on_super_stable_checkpoint(self, bft_network, skvbc):
           with log.start_action(action_type="validate_stop_on_super_stable_checkpoint") as action:
