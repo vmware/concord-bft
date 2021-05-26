e0933b94c49933b0731fce70f808fe60f377ae9b
f-squirrel <dmitry.b.danilov@gmail.com>
2021-05-26 14:37:50 +0300

    TS: Add Reserved Pages Client for Time Service
    This commit introduces a reserved pages client for time service which is used to store the last used timestamp.
    
    Changes:
    * TimeServiceResPageClient - the client
    * Unit tests

---
+++ bftengine/src/bftengine/TimeServiceResPageClient.hpp
@@ -0,0 +1,36 @@
+// Concord
+//
+// Copyright (c) 2018-2021 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").  You may not use this product except in
+// compliance with the Apache 2.0 License.
+//
+// This product may include a number of subcomponents with separate copyright notices and license terms. Your use of
+// these subcomponents is subject to the terms and conditions of the sub-component's license, as noted in the LICENSE
+// file.
+
+#pragma once
+
+#include "ReservedPagesClient.hpp"
+#include <chrono>
+
+namespace bftEngine {
+using ConsensusTime = std::chrono::milliseconds;
+class TimeServiceResPageClient : private ResPagesClient<TimeServiceResPageClient, 3, 1> {
+ public:
+  TimeServiceResPageClient();
+  ~TimeServiceResPageClient() = default;
+  TimeServiceResPageClient(const TimeServiceResPageClient&) = delete;
+
+  ConsensusTime getLastTimestamp() const { return last_timestamp_; }
+
+  // saves the timestamp in reserved pages
+  void setLastTimestamp(ConsensusTime timestamp);
+
+  // loads the timestamp from reserved pages, to be called on ST completed
+  void load();
+
+ private:
+  ConsensusTime last_timestamp_ = ConsensusTime::min();
+};
+}  // namespace bftEngine
