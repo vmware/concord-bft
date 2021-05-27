aa4473122b97e124ccf6db15f59fdfbf122ba7eb
Yulia Sherman <47499870+yuliasherman@users.noreply.github.com>
2021-05-27 13:45:58 +0300

    Add PreProcessor metrics (#1484)

--- util/include/RollingAvgAndVar.hpp
+++ util/include/RollingAvgAndVar.hpp
@@ -1,6 +1,6 @@
 // Concord
 //
-// Copyright (c) 2018 VMware, Inc. All Rights Reserved.
+// Copyright (c) 2018-2021 VMware, Inc. All Rights Reserved.
 //
 // This product is licensed to you under the Apache 2.0 license (the "License").  You may not use this product except in
 // compliance with the Apache 2.0 License.
@@ -12,6 +12,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <mutex>
 
 namespace bftEngine {
 namespace impl {
@@ -19,40 +20,73 @@ namespace impl {
 // Based on Knuth TAOCP vol 2, 2nd edition, page 216 (see also https://www.johndcook.com/blog/standard_deviation/)
 class RollingAvgAndVar {
  public:
-  void reset() {
-    k = 0;
-    prevM = 0;
-    prevS = 0;
-    currM = 0;
-    currS = 0;
+  RollingAvgAndVar(bool thread_safe = false) : thread_safe_(thread_safe) {}
+
+  void resetUnsafe() {
+    k_ = 0;
+    prevM_ = 0;
+    prevS_ = 0;
+    currM_ = 0;
+    currS_ = 0;
   }
 
-  void add(double x) {
-    k++;
+  void reset() {
+    if (!thread_safe_) return resetUnsafe();
+    std::lock_guard<std::mutex> lock(mutex_);
+    resetUnsafe();
+  }
 
-    if (k == 1) {
-      prevM = currM = x;
-      prevS = 0.0;
+  void addUnsafe(double x) {
+    k_++;
+    if (k_ == 1) {
+      prevM_ = currM_ = x;
+      prevS_ = 0.0;
     } else {
-      currM = prevM + (x - prevM) / k;
-      currS = prevS + (x - prevM) * (x - currM);
-      prevM = currM;
-      prevS = currS;
+      currM_ = prevM_ + (x - prevM_) / k_;
+      currS_ = prevS_ + (x - prevM_) * (x - currM_);
+      prevM_ = currM_;
+      prevS_ = currS_;
     }
   }
 
-  double avg() const { return (k > 0) ? currM : 0.0; }
+  void add(double x) {
+    if (!thread_safe_) return addUnsafe(x);
+    std::lock_guard<std::mutex> lock(mutex_);
+    addUnsafe(x);
+  }
 
-  double var() const { return ((k > 1) ? currS / (k - 1) : 0.0); }
+  double avgUnsafe() const { return (k_ > 0) ? currM_ : 0.0; }
 
-  int numOfElements() const { return k; }
+  double avg() {
+    if (!thread_safe_) return avgUnsafe();
+    std::lock_guard<std::mutex> lock(mutex_);
+    return avgUnsafe();
+  }
+
+  double varUnsafe() const { return ((k_ > 1) ? currS_ / (k_ - 1) : 0.0); }
+
+  double var() {
+    if (!thread_safe_) return varUnsafe();
+    std::lock_guard<std::mutex> lock(mutex_);
+    return varUnsafe();
+  }
+
+  int numOfElementsUnsafe() const { return k_; }
+
+  int numOfElements() {
+    if (!thread_safe_) return numOfElementsUnsafe();
+    std::lock_guard<std::mutex> lock(mutex_);
+    return numOfElementsUnsafe();
+  }
 
  private:
-  uint32_t k{0};
-  double prevM{0};
-  double prevS{0};
-  double currM{0};
-  double currS{0};
+  std::mutex mutex_;
+  bool thread_safe_;
+  int k_{0};
+  double prevM_{0};
+  double prevS_{0};
+  double currM_{0};
+  double currS_{0};
 };
 
 }  // namespace impl
