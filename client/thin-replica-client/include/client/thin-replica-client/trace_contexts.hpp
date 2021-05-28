c69352b87726555f89499269af5ce9ec65150e24
gshruti <gshruti@vmware.com>
2021-05-28 15:24:35 -0700

    Update licenses in TRC files to make them uniform
    This commit corrects the date in the licenses and adds the license
    in trace_context files.

--- client/thin-replica-client/include/client/thin-replica-client/trace_contexts.hpp
+++ client/thin-replica-client/include/client/thin-replica-client/trace_contexts.hpp
@@ -1,4 +1,15 @@
-// Copyright (c) 2021 VMware, Inc. All rights reserved. VMware Confidential
+// Concord
+//
+// Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0
+// License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
 
 #ifndef THIN_REPLICA_CLIENT_TRACE_CONTEXTS_HPP_
 #define THIN_REPLICA_CLIENT_TRACE_CONTEXTS_HPP_
