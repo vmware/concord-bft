fb11fc631e6bb81e2fe95d70ea3be5208a51d184
Yehoantan <yontyon@gmail.com>
2021-06-16 18:32:38 +0300

    Consider the epoch number on state transfer handler

--- kvbc/include/st_reconfiguraion_sm.hpp
+++ kvbc/include/st_reconfiguraion_sm.hpp
@@ -45,23 +45,17 @@ class StReconfigurationHandler {
   template <typename T>
   bool handlerStoredCommand(const std::string& key, uint64_t current_cp_num);
   uint64_t getStoredBftSeqNum(BlockId bid);
-  /*
-   * For wedge, we need to do nothing. The wedge point is being cleared only on termination.
-   * Thus, as long no one did unwedge (currently, restarting the replicas) the late replica will get the data
-   * In the reserved pages and behave accordingly.
-   * TODO: Notice that until we have the unwedge command, we cannot distinguish between restart for unwedge and restart
-   * out of a crash
-   */
-  bool handle(const concord::messages::WedgeCommand&, uint64_t, uint64_t) { return true; }
-  bool handle(const concord::messages::DownloadCommand&, uint64_t, uint64_t) { return true; }
+  uint64_t getEpochNumber(uint64_t bid);
 
-  bool handle(const concord::messages::InstallCommand& cmd, uint64_t, uint64_t) { return true; }
+  bool handle(const concord::messages::WedgeCommand&, uint64_t, uint64_t, uint64_t) { return true; }
+  bool handle(const concord::messages::DownloadCommand&, uint64_t, uint64_t, uint64_t) { return true; }
+  bool handle(const concord::messages::InstallCommand& cmd, uint64_t, uint64_t, uint64_t) { return true; }
+  bool handle(const concord::messages::KeyExchangeCommand&, uint64_t, uint64_t, uint64_t) { return true; }
+  bool handle(const concord::messages::AddRemoveCommand&, uint64_t, uint64_t, uint64_t) { return true; }
+  bool handle(const concord::messages::AddRemoveWithWedgeCommand&, uint64_t, uint64_t, uint64_t);
+  bool handle(const concord::messages::PruneRequest&, uint64_t, uint64_t, uint64_t);
 
-  bool handle(const concord::messages::KeyExchangeCommand&, uint64_t, uint64_t) { return true; }
-  bool handle(const concord::messages::AddRemoveCommand&, uint64_t, uint64_t) { return true; }
-  bool handle(const concord::messages::AddRemoveWithWedgeCommand&, uint64_t, uint64_t);
-  bool handle(const concord::messages::PruneRequest&, uint64_t, uint64_t);
-
+  >>>>>>> Consider the epoch number on state transfer handler
   kvbc::IReader& ro_storage_;
   std::vector<std::shared_ptr<concord::reconfiguration::IReconfigurationHandler>> orig_reconf_handlers_;
 };
