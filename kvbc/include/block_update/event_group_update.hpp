d6eca4ce64a4d074c0b9609f02fb302ea33ce93e
shgandhi <gshruti@vmware.com>
2021-07-27 16:47:22 -0700

    Add EventGroupUpdate struct
    This is to be used for EventGroup request, instead of BlockUpdate struct
    that is used currently.

---
+++ kvbc/include/block_update/event_group_update.hpp
@@ -0,0 +1,35 @@
+// Concord
+//
+// Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0
+// License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+//
+// Update added to spsc subscription queue for consumption by subscribers.
+// Note: EventGroupUpdate is NOT stored on the blockchain. Consequently it does NOT update the blockchain state.
+
+#ifndef CONCORD_KVBC_EVENTGROUP_UPDATE_H_
+#define CONCORD_KVBC_EVENTGROUP_UPDATE_H_
+
+#include <optional>
+
+#include "categorization/updates.h"
+#include "kv_types.hpp"
+
+namespace concord::kvbc {
+
+struct EventGroupUpdate {
+  kvbc::EventGroupId event_group_id;
+  kvbc::categorization::EventGroup event_group;
+  std::optional<std::string> parent_span;
+};
+
+}  // namespace concord::kvbc
+
+#endif  // CONCORD_KVBC_BLOCK_UPDATE_H_
