8ad3be58327c167b0a92071ed0d32035e60eea71
Hristo Staykov <hstaykov@vmware.com>
2021-07-27 14:02:41 +0300

    Add tests for Working Window that contains only Active part. Refactor old tests to cover more cases.

--- bftengine/tests/testSequenceWithActiveWindow/testSequenceWithActiveWindow.cpp
+++ bftengine/tests/testSequenceWithActiveWindow/testSequenceWithActiveWindow.cpp
@@ -35,14 +35,150 @@ class MockItemFuncs {
   }
 };
 
-TEST(testSequenceWithActiveWindow_test, basic_tests) {
+void basic_tests_no_inactive_window(unsigned int initialAdvance) {
+  const int windowSize = 12;
+  const int resolution = 2;
+  uint64_t beginning = initialAdvance * (windowSize / 2);
+  SequenceWithActiveWindow<windowSize, resolution, uint64_t, uint64_t, MockItemFuncs> windowOfInts(0, nullptr);
+
+  auto initial_start = beginning;
+  windowOfInts.advanceActiveWindow(beginning);
+
+  // Set consecutive values in Active Window
+  for (uint64_t i = beginning; i < beginning + windowSize; i += resolution) {
+    ConcordAssert(MockItemFuncs::restarted == windowOfInts.get(i));
+    ConcordAssert(!windowOfInts.isPressentInHistory(i));
+    windowOfInts.get(i) = i;
+  }
+  // Verify consecutive values in Active Window
+  for (uint64_t i = beginning; i < beginning + windowSize; i += resolution) {
+    ConcordAssert(i == windowOfInts.get(i));
+  }
+
+  // MockItemFuncs::printActiveWindow(windowOfInts);
+
+  // Advance Active Window by half
+  beginning += windowSize / 2;
+  windowOfInts.advanceActiveWindow(beginning);
+
+  // MockItemFuncs::printActiveWindow(windowOfInts);
+
+  // Verify half of the values remain in the Active Window
+  for (uint64_t i = beginning; i < beginning + windowSize / 2; i += resolution) {
+    ConcordAssert(i == windowOfInts.get(i));
+  }
+
+  // Verify other half of the Active Window is freshly reset
+  for (uint64_t i = beginning + windowSize / 2; i < beginning + windowSize; i += resolution) {
+    ConcordAssert(MockItemFuncs::restarted == windowOfInts.get(i));
+  }
+
+  // Verify history is empty
+  for (uint64_t i = initial_start; i < initial_start + windowSize; i += resolution) {
+    ConcordAssert(!windowOfInts.isPressentInHistory(i));
+  }
+}
+
+TEST(testSequenceWithActiveWindow_test, basic_tests_no_inactive_window_no_initial_offset) {
+  basic_tests_no_inactive_window(0);
+}
+
+TEST(testSequenceWithActiveWindow_test, basic_tests_no_inactive_window_with_half_window_initial_offset) {
+  basic_tests_no_inactive_window(1);
+}
+
+TEST(testSequenceWithActiveWindow_test, basic_tests_no_inactive_window_with_window_and_half_initial_offset) {
+  basic_tests_no_inactive_window(3);
+}
+
+void basic_tests_no_inactive_window_scroll_full_window_size(unsigned int numToScroll) {
+  const int windowSize = 30;
+  const int resolution = 5;
+  uint64_t beginning = windowSize / 2;
+  SequenceWithActiveWindow<windowSize, resolution, uint64_t, uint64_t, MockItemFuncs> windowOfInts(0, nullptr);
+
+  auto advance = windowSize * numToScroll;
+  auto initial_start = beginning;
+  windowOfInts.advanceActiveWindow(beginning);
+
+  // Set consecutive values in Active Window
+  for (uint64_t i = beginning; i < beginning + windowSize; i += resolution) {
+    ConcordAssert(MockItemFuncs::restarted == windowOfInts.get(i));
+    ConcordAssert(!windowOfInts.isPressentInHistory(i));
+    windowOfInts.get(i) = i;
+  }
+  // Verify consecutive values in Active Window
+  for (uint64_t i = beginning; i < beginning + windowSize; i += resolution) {
+    ConcordAssert(i == windowOfInts.get(i));
+  }
+
+  // MockItemFuncs::printActiveWindow(windowOfInts);
+
+  // Advance Active Window
+  beginning += advance;
+  windowOfInts.advanceActiveWindow(beginning);
+
+  // MockItemFuncs::printActiveWindow(windowOfInts);
+
+  // Verify the Working Window is freshly reset
+  for (uint64_t i = beginning; i < beginning + windowSize; i += resolution) {
+    ConcordAssert(MockItemFuncs::restarted == windowOfInts.get(i));
+  }
+
+  // Verify history is empty
+  for (uint64_t i = initial_start; i < initial_start + advance; i += resolution) {
+    ConcordAssert(!windowOfInts.isPressentInHistory(i));
+  }
+
+  // Set consecutive values in Active Window
+  for (uint64_t i = beginning; i < beginning + windowSize; i += resolution) {
+    ConcordAssert(MockItemFuncs::restarted == windowOfInts.get(i));
+    ConcordAssert(!windowOfInts.isPressentInHistory(i));
+    windowOfInts.get(i) = i;
+  }
+  // Verify consecutive values in Active Window
+  for (uint64_t i = beginning; i < beginning + windowSize; i += resolution) {
+    ConcordAssert(i == windowOfInts.get(i));
+  }
+
+  // Advance Active Window by half
+  beginning += windowSize / 2;
+  windowOfInts.advanceActiveWindow(beginning);
+
+  // Verify half of the values remain in the Active Window
+  for (uint64_t i = beginning; i < beginning + windowSize / 2; i += resolution) {
+    ConcordAssert(i == windowOfInts.get(i));
+  }
+
+  // Verify other half of the Active Window is freshly reset and
+  // then set consecutive values in it
+  for (uint64_t i = beginning + windowSize / 2; i < beginning + windowSize; i += resolution) {
+    ConcordAssert(MockItemFuncs::restarted == windowOfInts.get(i));
+    ConcordAssert(!windowOfInts.isPressentInHistory(i));
+    windowOfInts.get(i) = i;
+  }
+
+  // Verify consecutive values in Active Window
+  for (uint64_t i = beginning; i < beginning + windowSize; i += resolution) {
+    ConcordAssert(i == windowOfInts.get(i));
+  }
+}
+
+TEST(testSequenceWithActiveWindow_test, basic_tests_no_inactive_window_scrow_full_window_size) {
+  basic_tests_no_inactive_window_scroll_full_window_size(1);
+}
+
+TEST(testSequenceWithActiveWindow_test, basic_tests_no_inactive_window_scrow_twice_window_size) {
+  basic_tests_no_inactive_window_scroll_full_window_size(2);
+}
+
+void basic_tests_with_inactive_window(bool initialAdvance) {
   const int windowSize = 10;
   const int resolution = 1;
-  uint64_t beginning = 0;
-  SequenceWithActiveWindow<windowSize, resolution, uint64_t, uint64_t, MockItemFuncs, 1> windowOfInts(beginning,
-                                                                                                      nullptr);
+  uint64_t beginning = initialAdvance ? 5 * windowSize : 0;
+  SequenceWithActiveWindow<windowSize, resolution, uint64_t, uint64_t, MockItemFuncs, 1> windowOfInts(0, nullptr);
 
-  auto initial_start = beginning = 5 * windowSize;
+  auto initial_start = beginning;
   windowOfInts.advanceActiveWindow(beginning);
 
   // Set consecutive values in Active Window
@@ -88,6 +224,14 @@ TEST(testSequenceWithActiveWindow_test, basic_tests) {
     ConcordAssert(windowOfInts.isPressentInHistory(i));
     ConcordAssert(i == windowOfInts.getFromHistory(i));
   }
+}
+
+TEST(testSequenceWithActiveWindow_test, basic_tests_with_inactive_window_no_initial_offset) {
+  basic_tests_with_inactive_window(false);
+}
+
+TEST(testSequenceWithActiveWindow_test, basic_tests_with_inactive_window_with_initial_offset) {
+  basic_tests_with_inactive_window(true);
 }
 
 TEST(testSequenceWithActiveWindow_test, move_working_window_7_times) {
