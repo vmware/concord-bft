e316332fbed95cd83987eeac708266942c3663b5
Toly Kournik <tkournik@vmware.com>
2021-08-04 05:03:58 -0700

    Add an option to build third party libraries as an external cmake project. Use -DBUILD_THIRDPARTY=ON to enable the option. For starters relic and and its dependency gmp libraries were added as an external project.

---
+++ thirdparty/gmp.cmake
@@ -0,0 +1,16 @@
+#The GNU Multiple Precision Arithmetic Library
+
+ExternalProject_Add(gmp
+                    PREFIX gmp
+                    URL "https://gmplib.org/download/gmp/gmp-6.1.2.tar.lz"
+                    LOG_DOWNLOAD 1
+                    LOG_BUILD 1
+                    BUILD_IN_SOURCE 1
+                    CONFIGURE_COMMAND ./configure --with-pic --enable-cxx --disable-fat --build x86_64-linux-gnu 
+                    INSTALL_COMMAND ""
+                    COMMAND make check
+)
+
+ExternalProject_Get_Property(gmp BINARY_DIR)
+set(GMP_STATIC_LIBRARY ${BINARY_DIR}/.libs/libgmp.a PARENT_SCOPE)
+message(STATUS "GMP_STATIC_LIBRARY ${BINARY_DIR}/.libs/libgmp.a")
