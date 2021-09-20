d4977d8dcd82e8792696223fdbe2f1304b95e735
Lior <eliorone@gmail.com>
2021-09-20 01:54:15 -0700

    WIP: Increase timeout too much

--- tests/apollo/util/bft.py
+++ tests/apollo/util/bft.py
@@ -918,7 +918,7 @@ class BftTestNetwork:
                 replica_views.append(replica_view)
 
             try:
-                with trio.fail_after(seconds=60):
+                with trio.fail_after(seconds=120):
                     while True:
                         replica_views = []
                         async with trio.open_nursery() as nursery:
