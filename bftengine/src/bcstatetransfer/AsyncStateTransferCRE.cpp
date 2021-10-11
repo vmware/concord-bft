a50646f7db0e66a5db329d5eca4855fa41b21ebf
Yehoantan <yontyon@gmail.com>
2021-10-11 13:59:58 +0300

    Introducing cre for state transfer

---
+++ bftengine/src/bcstatetransfer/AsyncStateTransferCRE.cpp
@@ -0,0 +1,76 @@
+// Concord
+//
+// Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0
+// License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+#include "AsyncStateTransferCRE.hpp"
+#include "bftengine/ReplicaConfig.hpp"
+
+namespace bftEngine::bcst::asyncCRE {
+bftEngine::bcst::asyncCRE::Communication::Communication(std::shared_ptr<MsgsCommunicator> msgsCommunicator,
+                                                        std::shared_ptr<MsgHandlersRegistrator> msgHandlers)
+    : msgsCommunicator_{msgsCommunicator} {
+  msgHandlers->registerMsgHandler(MsgCode::ClientReply, [&](bftEngine::impl::MessageBase* message) {
+    if (receiver_) receiver_->onNewMessage(message->senderId(), message->body(), message->size());
+  });
+}
+int Communication::start() {
+  is_running_ = true;
+  return 0;
+}
+int Communication::stop() {
+  is_running_ = false;
+  return 0;
+}
+bool Communication::isRunning() const { return is_running_; }
+bft::communication::ConnectionStatus Communication::getCurrentConnectionStatus(bft::communication::NodeNum) {
+  if (!is_running_) return bft::communication::ConnectionStatus::Disconnected;
+  return bft::communication::ConnectionStatus::Connected;
+}
+int Communication::send(bft::communication::NodeNum destNode, std::vector<uint8_t>&& msg) {
+  return msgsCommunicator_->sendAsyncMessage(destNode, reinterpret_cast<char*>(msg.data()), msg.size());
+}
+std::set<bft::communication::NodeNum> Communication::send(std::set<bft::communication::NodeNum> dests,
+                                                          std::vector<uint8_t>&& msg) {
+  auto ret = dests;
+  msgsCommunicator_->send(dests, reinterpret_cast<char*>(msg.data()), msg.size());
+  return ret;
+}
+void Communication::setReceiver(bft::communication::NodeNum receiverNum, bft::communication::IReceiver* receiver) {
+  receiver_ = receiver;
+}
+
+concord::client::reconfiguration::ClientReconfigurationEngine* CreFactory::create(
+    std::shared_ptr<MsgsCommunicator> msgsCommunicator, std::shared_ptr<MsgHandlersRegistrator> msgHandlers) {
+  bft::client::ClientConfig bftClientConf;
+  auto& repConfig = bftEngine::ReplicaConfig::instance();
+  bftClientConf.f_val = repConfig.fVal;
+  bftClientConf.c_val = repConfig.cVal;
+  bftClientConf.id = bft::client::ClientId{repConfig.replicaId};
+  for (uint16_t i = 0; i < repConfig.numReplicas; i++) {
+    bftClientConf.all_replicas.emplace(bft::client::ReplicaId{i});
+  }
+  for (uint16_t i = repConfig.numReplicas; i < repConfig.numReplicas + repConfig.numRoReplicas; i++) {
+    bftClientConf.ro_replicas.emplace(bft::client::ReplicaId{i});
+  }
+  std::unique_ptr<bft::communication::ICommunication> comm =
+      std::make_unique<Communication>(msgsCommunicator, msgHandlers);
+  bft::client::Client* bftClient = new bft::client::Client(std::move(comm), bftClientConf);
+  concord::client::reconfiguration::Config cre_config;
+  cre_config.id_ = repConfig.replicaId;
+  cre_config.interval_timeout_ms_ = 1000;
+  concord::client::reconfiguration::IStateClient* pbc = new concord::client::reconfiguration::PollBasedStateClient(
+      bftClient, cre_config.interval_timeout_ms_, 0, cre_config.interval_timeout_ms_);
+  concord::client::reconfiguration::ClientReconfigurationEngine* cre =
+      new concord::client::reconfiguration::ClientReconfigurationEngine(
+          cre_config, pbc, std::make_shared<concordMetrics::Aggregator>());
+  return cre;
+}
+}  // namespace bftEngine::bcst::asyncCRE
---
+++ bftengine/src/bcstatetransfer/AsyncStateTransferCRE.hpp
@@ -0,0 +1,52 @@
+// Concord
+//
+// Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0
+// License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+
+#pragma once
+
+#include "bftengine/MsgsCommunicator.hpp"
+#include "communication/ICommunication.hpp"
+#include "MsgHandlersRegistrator.hpp"
+#include "MsgsCommunicator.hpp"
+#include "client/reconfiguration/poll_based_state_client.hpp"
+#include "client/reconfiguration/client_reconfiguration_engine.hpp"
+#include "bftclient/bft_client.h"
+
+namespace bftEngine::bcst::asyncCRE {
+
+class Communication : public bft::communication::ICommunication {
+ public:
+  Communication(std::shared_ptr<MsgsCommunicator> msgsCommunicator,
+                std::shared_ptr<MsgHandlersRegistrator> msgHandlers);
+
+  int getMaxMessageSize() override { return 128 * 1024; }  // 128KB
+  int start() override;
+  int stop() override;
+  bool isRunning() const override;
+  bft::communication::ConnectionStatus getCurrentConnectionStatus(bft::communication::NodeNum node) override;
+  int send(bft::communication::NodeNum destNode, std::vector<uint8_t>&& msg) override;
+  std::set<bft::communication::NodeNum> send(std::set<bft::communication::NodeNum> dests,
+                                             std::vector<uint8_t>&& msg) override;
+  void setReceiver(bft::communication::NodeNum receiverNum, bft::communication::IReceiver* receiver) override;
+  void dispose(bft::communication::NodeNum i) override {}
+
+ private:
+  std::shared_ptr<MsgsCommunicator> msgsCommunicator_;
+  bool is_running_ = false;
+  bft::communication::IReceiver* receiver_ = nullptr;
+};
+class CreFactory {
+ public:
+  static concord::client::reconfiguration::ClientReconfigurationEngine* create(
+      std::shared_ptr<MsgsCommunicator> msgsCommunicator, std::shared_ptr<MsgHandlersRegistrator> msgHandlers);
+};
+}  // namespace bftEngine::bcst::asyncCRE
