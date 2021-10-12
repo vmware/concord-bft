cabdc74c93e038e8f5fb5fb3aee711a270b25dfd
Yehoantan <yontyon@gmail.com>
2021-10-12 12:04:30 +0300

    Don't add new epoch in backward compatibility cases

--- storage/src/rocksdb_client.cpp
+++ storage/src/rocksdb_client.cpp
@@ -250,21 +250,7 @@ void Client::initDB(bool readOnly, const std::optional<Options> &userOptions, bo
   // Reference: https://github.com/facebook/rocksdb/blob/v6.8.1/db/db_impl/db_impl.cc#L2187
   auto incompletelyCreatedColumnFamilies = std::vector<std::string>{};
 
-  // Try to read the stored options configuration file
-  // Note that if we recover, then rocksdb should have its option configuration file stored in the rocksdb directory.
-  // Thus, we don't need to persist our custom configuration file.
   auto s_opt = ::rocksdb::LoadLatestOptions(m_dbPath, ::rocksdb::Env::Default(), &options.db_options, &cf_descs);
-  if (!s_opt.ok()) {
-    const char kPathSeparator =
-#ifdef _WIN32
-        '\\';
-#else
-        '/';
-#endif
-    // If we couldn't read the stored configuration file, try to read the default configuration file.
-    s_opt = ::rocksdb::LoadOptionsFromFile(
-        m_dbPath + kPathSeparator + default_opt_config_name, ::rocksdb::Env::Default(), &options.db_options, &cf_descs);
-  }
   if (!s_opt.ok()) {
     // If we couldn't read the stored configuration and not the default configuration file, then create
     // one.
