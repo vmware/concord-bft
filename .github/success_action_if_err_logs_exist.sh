4f145c934dd7fa5fdddb5cad871612f865d2550c
Evgeniy Dzhurov <edzhurov@vmware.com>
2021-11-01 10:23:05 -0700

    Check fatal error logs only
    Modify the workflow so that only fatal error logs
    are exported to ReplicaErrorLog.txt and would
    fail the build.

--- .github/success_action_if_err_logs_exist.sh
+++ .github/success_action_if_err_logs_exist.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This script used to check if ERROR/FATAL logs are present in apollo replica logs.
+# This script used to check if FATAL logs are present in apollo replica logs.
 
 set -e
 
@@ -7,7 +7,7 @@ file_name=ReplicaErrorLogs.txt
 file_count=$(find ${1} -name $file_name | wc -l)
 
 if [[ $file_count -gt 0 ]]; then
-    echo -e "\033[31mERROR/FATAL logs found in below paths. Please check artifacts\033[m"
+    echo -e "\033[31mFATAL logs found in below paths. Please check artifacts\033[m"
     echo -e "$(find . -type f -name ReplicaErrorLogs.txt)"
 else 
     echo "File not found"
