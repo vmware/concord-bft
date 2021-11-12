c05c468e4f0c61163219fc92fb086c4079eba1b3
Situ Singh <ssitu@vmware.com>
2021-11-12 19:49:11 +0530

    Revert "TS: Time_serviceV2 apollo testcases addition" (#2027)

--- tests/apollo/test_skvbc_time_service.py
+++ tests/apollo/test_skvbc_time_service.py
@@ -21,7 +21,6 @@ from util.bft import with_trio, with_bft_network, KEY_FILE_PREFIX
 from util.skvbc_history_tracker import verify_linearizability
 from util import eliot_logging as log
 from util import skvbc as kvbc
-from util import bft_network_traffic_control as ntc
 
 FILE_PATH_PREFIX = "/tmp/fake_clock_"
 FILE_PATH_SUFFIX = ".config"
@@ -61,8 +60,10 @@ class SkvbcTimeServiceTest(unittest.TestCase):
         # the system should return to the fast path.
         self.evaluation_period_seq_num = 64
 
+    @unittest.skip("Disabled till Timeservice V2 is not enabled")
     @with_trio
-    @with_bft_network(start_replica_cmd, rotate_keys=True)
+    @with_bft_network(start_replica_cmd,
+                      selected_configs=lambda n, f, c: c == 0 and n >= 6, rotate_keys=True)
     @verify_linearizability()
     async def test_wrong_time_in_primary(self, bft_network, tracker):
         """
@@ -107,9 +108,10 @@ class SkvbcTimeServiceTest(unittest.TestCase):
         await bft_network.wait_for_fast_path_to_be_prevalent(
             run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
 
+    @unittest.skip("Disabled till Timeservice V2 is not enabled")
     @with_trio
     @with_bft_network(start_replica_cmd,
-            selected_configs=lambda n, f, c: c == 0 and n >= 6, rotate_keys=True)
+                      selected_configs=lambda n, f, c: c == 0 and n >= 6, rotate_keys=True)
     @verify_linearizability()
     async def test_wrong_time_in_non_primary(self, bft_network, tracker):
         """
@@ -142,20 +144,15 @@ class SkvbcTimeServiceTest(unittest.TestCase):
         await bft_network.wait_for_slow_path_to_be_prevalent(
             run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
         
-        with log.start_action(action_type="get_hard_limit_reached_counter") as action:
-            hard_limit_cnt = await bft_network.get_metric(non_primary_replica, bft_network, "Counters",
-                    "hard_limit_reached_counter", "time_service")
-            action.log(
-                    message_type=f'hard_limit_reached_counter #{hard_limit_cnt}')
-            self.assertGreater(hard_limit_cnt, 0)       
-        
         await self.manipulate_time_file_write(path, CLOCK_NO_DRIFT)
 
         await bft_network.wait_for_fast_path_to_be_prevalent(
             run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
 
+    @unittest.skip("Disabled till Timeservice V2 is not enabled")
     @with_trio
-    @with_bft_network(start_replica_cmd_without_time_service('0') , rotate_keys=True)
+    @with_bft_network(start_replica_cmd_without_time_service('0'),
+                      selected_configs=lambda n, f, c: c == 0 and n >= 6, rotate_keys=True)
     @verify_linearizability()
     async def test_wrong_time_in_primary_without_ts(self, bft_network, tracker):
         """
@@ -193,8 +190,10 @@ class SkvbcTimeServiceTest(unittest.TestCase):
         await bft_network.wait_for_fast_path_to_be_prevalent(
             run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
 
+    @unittest.skip("Disabled till Timeservice V2 is not enabled")
     @with_trio
-    @with_bft_network(start_replica_cmd_without_time_service('0'), rotate_keys=True)
+    @with_bft_network(start_replica_cmd_without_time_service('0'),
+                      selected_configs=lambda n, f, c: c == 0 and n >= 6, rotate_keys=True)
     @verify_linearizability()
     async def test_wrong_time_in_non_primary_without_ts(self, bft_network, tracker): 
         """
@@ -231,119 +230,6 @@ class SkvbcTimeServiceTest(unittest.TestCase):
 
         await bft_network.wait_for_fast_path_to_be_prevalent(
             run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20) 
-
-    @with_trio
-    @with_bft_network(start_replica_cmd,
-            selected_configs=lambda n, f, c: c == 0 and n >= 6, rotate_keys=True)
-    @verify_linearizability()
-    async def test_wrong_time_in_f_replicas(self, bft_network, tracker):
-        """
-        1. Launch a cluster
-        2. Make sure that the cluster works in the fast path mode
-        3. Change local time in f of the non-primary's container so that it would be behind or beyond other replica's local time
-        4. Expected result: The cluster should switch to the slow path
-        5. Change time in the f non-primary's container so that it is the same as in other replicas
-        6. Expected result: The cluster should switch to the fast path
-        """
-        
-        n = bft_network.config.n
-        f = bft_network.config.f
-        initial_view = 0
-    
-        for replica_id in range(n):
-            path = FILE_PATH_PREFIX + str(replica_id) + FILE_PATH_SUFFIX
-            await self.manipulate_time_file_write(path, CLOCK_NO_DRIFT)
-        
-        bft_network.start_all_replicas()
-        skvbc = kvbc.SimpleKVBCProtocol(bft_network,tracker)
-        await bft_network.wait_for_fast_path_to_be_prevalent(
-            run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
-
-        wrong_replicas = bft_network.random_set_of_replicas(f, without={initial_view})
-    
-        for replica_id in wrong_replicas:
-            path = FILE_PATH_PREFIX + str(replica_id) + FILE_PATH_SUFFIX
-            await self.manipulate_time_file_write(path, CLOCK_DRIFT) 
-        
-        await bft_network.wait_for_slow_path_to_be_prevalent(
-            run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
-
-        for replica_id in wrong_replicas:
-            path = FILE_PATH_PREFIX + str(replica_id) + FILE_PATH_SUFFIX
-            await self.manipulate_time_file_write(path, CLOCK_NO_DRIFT)
-
-        await bft_network.wait_for_fast_path_to_be_prevalent(
-            run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
-
-    @with_trio
-    @with_bft_network(start_replica_cmd, rotate_keys=True)
-    @verify_linearizability()
-    async def test_restart_non_primary_replica(self, bft_network, tracker):
-        """
-        1. Launch a cluster
-        2. Make sure that the cluster works in the fast path mode
-        3. Stop one non-primary replica
-        4. Expected result: The cluster should switch to the slow path
-        5. Restart the stopped replica
-        6. Expected result: The cluster should switch to the fast path
-        """
-
-        initial_primary = 0
-        skvbc = kvbc.SimpleKVBCProtocol(bft_network, tracker)
-        bft_network.start_all_replicas()
-
-        await bft_network.wait_for_fast_path_to_be_prevalent(
-            run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)        
-
-        non_primary_replica = random.choice(
-            bft_network.all_replicas(without={initial_primary}))
- 
-        bft_network.stop_replica(non_primary_replica)
-
-        await bft_network.wait_for_slow_path_to_be_prevalent(
-        run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
-
-        bft_network.start_replica(non_primary_replica)
-
-        await bft_network.wait_for_fast_path_to_be_prevalent(
-        run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
-
-    @with_trio
-    @with_bft_network(start_replica_cmd,
-            selected_configs=lambda n, f, c: c == 0 and n >= 6, rotate_keys=True)
-    @verify_linearizability()
-    async def test_delay_with_soft_limit_reached_counter(self, bft_network, tracker):
-        """
-        1. Start all replicas except a non-primary
-        2. Expected result: The cluster should work in slow path
-        3. Add a delay of 500ms in network
-        4. Start the stopped non-primary replica
-        5. Expected result: Increment in soft_limit_reached_counter
-        6. Remove the delay from network
-        """
-
-        initial_primary = 0
-        skvbc = kvbc.SimpleKVBCProtocol(bft_network, tracker)
-        non_primary_replica = random.choice(
-            bft_network.all_replicas(without={initial_primary}))
-
-        bft_network.start_replicas(bft_network.all_replicas(without={non_primary_replica})) 
-
-        await bft_network.wait_for_slow_path_to_be_prevalent(
-        run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
- 
-        with ntc.NetworkTrafficControl() as tc:
-            #delay added to loopback interface
-            tc.put_loop_back_interface_delay(500)
-            bft_network.start_replica(non_primary_replica)
-            await skvbc.run_concurrent_ops(100, 1)
-            
-            with log.start_action(action_type="get_soft_limit_reached_counter") as action:
-                soft_limit_cnt = await bft_network.get_metric(non_primary_replica, bft_network, "Counters",
-                        "soft_limit_reached_counter", "time_service")
-                action.log(
-                        message_type=f'soft_limit_reached_counter #{soft_limit_cnt}')
-                self.assertGreater(soft_limit_cnt, 0)
 
     @classmethod
     async def manipulate_time_file_write(self, path, data):
--- tests/simpleKVBC/TesterReplica/setup.cpp
+++ tests/simpleKVBC/TesterReplica/setup.cpp
@@ -68,7 +68,6 @@ std::unique_ptr<TestSetup> TestSetup::ParseArgs(int argc, char** argv) {
     replicaConfig.pruningEnabled_ = true;
     replicaConfig.numBlocksToKeep_ = 10;
     replicaConfig.batchedPreProcessEnabled = true;
-    replicaConfig.timeServiceEnabled = true;
     replicaConfig.set("sourceReplicaReplacementTimeoutMilli", 6000);
     replicaConfig.set("concord.bft.st.runInSeparateThread", true);
     replicaConfig.set("concord.bft.keyExchage.clientKeysEnabled", false);
