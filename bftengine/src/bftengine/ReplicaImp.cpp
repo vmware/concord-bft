201c8874bab9e6c27fc390bf0764007622b33a0b
Lior Gal <eliorone@gmail.com>
2021-12-19 10:08:59 +0200

    [1.5] Fix memory leak  (#2137)
    * Try to fix
    
    * Disable transaction signing unstable test case
    
    * Skip tls_key_exchange_with_ror UT since it unstable

--- bftengine/src/bftengine/ReplicaImp.cpp
+++ bftengine/src/bftengine/ReplicaImp.cpp
@@ -443,6 +443,8 @@ void ReplicaImp::onMessage<ClientRequestMsg>(ClientRequestMsg *m) {
         deferredRORequests_.push_back(
             m);  // We should handle span and deleting the message when we handle the deferred message
         deferredRORequestsMetric_++;
+      } else {
+        delete m;
       }
     } else {
       executeReadOnlyRequest(span, m);
@@ -2364,6 +2366,8 @@ void ReplicaImp::pushDeferredMessage(MessageBase *m) {
   if (deferredMessages_.size() < maxQueueSize) {
     deferredMessages_.push_back(m);
     deferredMessagesMetric_++;
+  } else {
+    delete m;
   }
 }
 
