28da8641696e9dbc0b1a700439ab0dc6c92c3371
shgandhi <gshruti@vmware.com>
2022-01-07 13:39:38 -0800

    Initialize state_snapshot_service in clientservice

--- client/clientservice/include/client/clientservice/client_service.hpp
+++ client/clientservice/include/client/clientservice/client_service.hpp
@@ -14,6 +14,7 @@
 
 #include "event_service.hpp"
 #include "request_service.hpp"
+#include "state_snapshot_service.hpp"
 #include "Logger.hpp"
 #include "client/concordclient/concord_client.hpp"
 
@@ -25,18 +26,21 @@ class ClientService {
       : logger_(logging::getLogger("concord.client.clientservice")),
         client_(std::move(client)),
         event_service_(std::make_unique<EventServiceImpl>(client_)),
-        request_service_(std::make_unique<RequestServiceImpl>(client_)){};
+        request_service_(std::make_unique<RequestServiceImpl>(client_)),
+        state_snapshot_service_(std::make_unique<StateSnapshotServiceImpl>(client_)){};
 
   void start(const std::string& addr, uint64_t max_receive_msg_size);
 
   const std::string kRequestService{"vmware.concord.client.request.v1.RequestService"};
   const std::string kEventService{"vmware.concord.client.event.v1.EventService"};
+  const std::string kStateSnapshotService{"vmware.concord.client.statesnapshot.v1.StateSnapshotService"};
 
  private:
   logging::Logger logger_;
   std::shared_ptr<concord::client::concordclient::ConcordClient> client_;
   std::unique_ptr<EventServiceImpl> event_service_;
   std::unique_ptr<RequestServiceImpl> request_service_;
+  std::unique_ptr<StateSnapshotServiceImpl> state_snapshot_service_;
 };
 
 }  // namespace concord::client::clientservice
