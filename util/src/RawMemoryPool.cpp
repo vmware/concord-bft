fbe877be407c0dc2884f29daf738d10bf96520d7
yuliasherman <ysherman@vmware.com>
2022-01-10 04:41:21 -0800

    Introduce RawMemoryPool for PreProcessor

---
+++ util/src/RawMemoryPool.cpp
@@ -0,0 +1,148 @@
+// Concord
+//
+// Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").  You may not use this product except in
+// compliance with the Apache 2.0 License.
+//
+// This product may include a number of subcomponents with separate copyright notices and license terms. Your use of
+// these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+
+#include "RawMemoryPool.hpp"
+
+#include <thread>
+#include "assertUtils.hpp"
+#include "kvstream.h"
+
+namespace concordUtil {
+
+using namespace std;
+using namespace std::chrono;
+
+RawMemoryPool::RawMemoryPool(uint32_t chunkSize, Timers& timers) : chunkSize_(chunkSize), metrics_(timers) {}
+
+RawMemoryPool::~RawMemoryPool() {
+  stopWorking_ = true;
+  unique_lock<mutex> lock(waitForAvailChunkLock_);
+  this_thread::sleep_for(std::chrono::milliseconds(WAIT_TIMEOUT_MILLI * 2));
+  char* chunk = nullptr;
+  while (pool_->pop(chunk)) delete[] chunk;
+}
+
+void RawMemoryPool::allocatePool(int32_t initialChunksNum, int32_t maxChunksNum) {
+  if (initialChunksNum > maxChunksNum || initialChunksNum < 0 || maxChunksNum < 0) {
+    stringstream err;
+    err << "Wrong parameters specified: initialChunksNum=" << initialChunksNum << ", maxChunksNum=" << maxChunksNum;
+    LOG_ERROR(logger(), KVLOG(err.str()));
+    throw std::invalid_argument(__PRETTY_FUNCTION__ + err.str());
+  }
+  initialChunksNum_ = initialChunksNum;
+  maxChunksNum_ = maxChunksNum;
+  pool_ = make_shared<boost::lockfree::queue<char*, boost::lockfree::fixed_sized<true>>>(maxChunksNum_);
+  for (int32_t i = 0; i < initialChunksNum_; ++i) {
+    pool_->push(new char[chunkSize_]);
+    metrics_.increaseAvailableChunksNum();
+    metrics_.increaseAllocatedChunksNum();
+  }
+  numOfAvailableChunks_ = initialChunksNum;
+  numOfAllocatedChunks_ = initialChunksNum;
+  LOG_INFO(logger(),
+           "Memory pool allocated" << KVLOG(chunkSize_,
+                                            initialChunksNum_,
+                                            maxChunksNum_,
+                                            numOfAllocatedChunks_,
+                                            numOfAvailableChunks_,
+                                            maxAvailableChunksPercentage_));
+}
+
+void RawMemoryPool::increaseNumOfAllocatedChunks() {
+  numOfAllocatedChunks_++;
+  metrics_.increaseAllocatedChunksNum();
+}
+
+void RawMemoryPool::increaseNumOfAvailableChunks() {
+  numOfAvailableChunks_++;
+  metrics_.increaseAvailableChunksNum();
+}
+
+void RawMemoryPool::decreaseNumOfAllocatedChunks() {
+  numOfAllocatedChunks_--;
+  metrics_.decreaseAllocatedChunksNum();
+}
+
+void RawMemoryPool::decreaseNumOfAvailableChunks() {
+  numOfAvailableChunks_--;
+  metrics_.decreaseAvailableChunksNum();
+}
+
+char* RawMemoryPool::allocateChunk() {
+  char* chunk = nullptr;
+  if (numOfAllocatedChunks_ < maxChunksNum_) {
+    chunk = new char[chunkSize_];
+    increaseNumOfAllocatedChunks();
+    increaseNumOfAvailableChunks();
+  }
+  if (chunk)
+    LOG_DEBUG(logger(), "A chunk has been allocated" << KVLOG(numOfAllocatedChunks_, numOfAvailableChunks_));
+  else
+    LOG_WARN(logger(),
+             "The pool size has reached the maximum, wait for a chunk to become available" << KVLOG(maxChunksNum_));
+  return chunk;
+}
+
+void RawMemoryPool::deleteChunk(char*& chunk) {
+  delete[] chunk;
+  decreaseNumOfAllocatedChunks();
+  LOG_DEBUG(logger(), "A chunk has been deleted" << KVLOG(numOfAllocatedChunks_, numOfAvailableChunks_));
+}
+
+void RawMemoryPool::returnChunkToThePool(char* chunk) {
+  pool_->push(chunk);
+  increaseNumOfAvailableChunks();
+  LOG_DEBUG(logger(), "A chunk has been returned to the pool" << KVLOG(numOfAllocatedChunks_, numOfAvailableChunks_));
+  waitForAvailChunkCond_.notify_one();
+}
+
+bool RawMemoryPool::isPoolPruningRequired() {
+  return (((numOfAllocatedChunks_ > initialChunksNum_) &&
+           (numOfAvailableChunks_ >= numOfAllocatedChunks_ * maxAvailableChunksPercentage_)));
+}
+
+char* RawMemoryPool::getChunk() {
+  ConcordAssert(pool_.get() != nullptr);
+  char* chunk = nullptr;
+  if (!pool_->pop(chunk)) {
+    // No available chunks => allocate a new one, if permitted
+    chunk = allocateChunk();
+    if (!chunk) {
+      // Pool size limit has been reached; wait until some chunk gets released
+      unique_lock<mutex> lock(waitForAvailChunkLock_);
+      while (!stopWorking_ && !chunk) {
+        waitForAvailChunkCond_.wait_until(lock, steady_clock::now() + milliseconds(WAIT_TIMEOUT_MILLI));
+        pool_->pop(chunk);
+      }
+    }
+  }
+  if (chunk) {
+    decreaseNumOfAvailableChunks();
+    LOG_DEBUG(logger(), "A chunk has been taken" << KVLOG(numOfAllocatedChunks_, numOfAvailableChunks_));
+  }
+  return chunk;
+}
+
+void RawMemoryPool::returnChunk(char* chunk) {
+  if (numOfAvailableChunks_ >= maxChunksNum_) {
+    stringstream err;
+    err << "Returned chunk overflows the pool size limit: numOfAvailableChunks_=" << numOfAvailableChunks_
+        << ", maxChunksNum_=" << maxChunksNum_;
+    LOG_ERROR(logger(), KVLOG(err.str()));
+    throw std::runtime_error(__PRETTY_FUNCTION__ + err.str());
+  }
+  if (!isPoolPruningRequired())
+    returnChunkToThePool(chunk);
+  else  // Too many chunks are not in use => delete
+    deleteChunk(chunk);
+}
+
+}  // namespace concordUtil
---
+++ util/test/RawMemoryPool_test.cpp
@@ -0,0 +1,88 @@
+// Concord
+//
+// Copyright (c) 2022 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0
+// License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the
+// LICENSE file.
+
+#include <memory>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "RawMemoryPool.hpp"
+
+namespace {
+using namespace std;
+using namespace concordUtil;
+
+Timers timers;
+const uint32_t chunkSize = 1034;
+const uint32_t initialChunks = 3;
+const uint32_t maxChunks = 20;
+
+TEST(RawMemoryPoolTest, checkInput) {
+  RawMemoryPool pool(chunkSize, timers);
+  EXPECT_THROW(pool.allocatePool(maxChunks + 1, maxChunks), std::invalid_argument);
+  EXPECT_THROW(pool.allocatePool(initialChunks, -1), std::invalid_argument);
+  EXPECT_THROW(pool.allocatePool(-1, maxChunks), std::invalid_argument);
+  EXPECT_NO_THROW(pool.allocatePool(initialChunks, initialChunks));
+}
+
+TEST(RawMemoryPoolTest, testAllocFree) {
+  RawMemoryPool pool(chunkSize, timers);
+  pool.allocatePool(initialChunks, maxChunks);
+
+  ASSERT_EQ(pool.getNumOfAvailableChunks(), initialChunks);
+  ASSERT_EQ(pool.getNumOfAllocatedChunks(), initialChunks);
+  ASSERT_FALSE(pool.isPoolFull());
+
+  // One chunk is in use
+  char* chunk = pool.getChunk();
+  ASSERT_TRUE(chunk);
+  ASSERT_EQ(pool.getNumOfAvailableChunks(), initialChunks - 1);
+  ASSERT_EQ(pool.getNumOfAllocatedChunks(), initialChunks);
+  ASSERT_FALSE(pool.isPoolFull());
+
+  pool.returnChunk(chunk);  // No pruning expected
+  ASSERT_EQ(pool.getNumOfAvailableChunks(), initialChunks);
+  ASSERT_EQ(pool.getNumOfAllocatedChunks(), initialChunks);
+  ASSERT_FALSE(pool.isPoolFull());
+
+  // Allocate all chunks up to maxChunks number through getChunk() function + fill chunks[] array
+  char* chunks[maxChunks];
+  for (uint32_t i = 0; i < maxChunks; i++) {
+    chunks[i] = pool.getChunk();
+    ASSERT_TRUE(chunks[i]);
+  }
+  ASSERT_EQ(pool.getNumOfAvailableChunks(), 0);
+  ASSERT_EQ(pool.getNumOfAllocatedChunks(), maxChunks);
+  ASSERT_TRUE(pool.isPoolFull());
+
+  // Return all chunks - pruning gets triggered
+  for (uint32_t i = 0; i < maxChunks; i++) {
+    ASSERT_TRUE(chunks[i]);
+    pool.returnChunk(chunks[i]);
+  }
+  ASSERT_EQ(pool.getNumOfAvailableChunks(), pool.getNumOfAvailableChunks());
+  ASSERT_EQ(pool.getNumOfAllocatedChunks(), pool.getNumOfAvailableChunks());
+  ASSERT_FALSE(pool.isPoolFull());
+}
+
+TEST(RawMemoryPoolTest, testAllocFreeErrors) {
+  RawMemoryPool pool(chunkSize, timers);
+  pool.allocatePool(maxChunks, maxChunks);
+  ASSERT_TRUE(pool.isPoolFull());
+
+  // Return more chunks than were allocated
+  char chunk[chunkSize];
+  EXPECT_THROW(pool.returnChunk(chunk), std::runtime_error);
+}
+
+}  // namespace
