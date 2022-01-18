a74cace01a4107b4dfa44dff44e827371445f94d
Gil Levkovich <glevkovich@vmware.com>
2022-01-18 20:26:50 +0200

    BCStateTran: Add new configuration parameters, reconfigure
    Here we add 2 new parameters that will be used on the next commits and
    some are already used by RVBM/RVT:
    1) RVT_K - maximal number of children in the TREE. This defines the K-ary
    tree size.
    2) enableSourceSelectorPrimaryAwareness - This feature has been already
    introduced but without an option to disable it. enabled by default.
    3) enableStoreRvbDataDuringCheckpointing - When disabled, no RVB data is
    persisted during checkpointing. Pruning data is still persisted.
    This helps us for testing purposes.

--- bftengine/include/bcstatetransfer/SimpleBCStateTransfer.hpp
+++ bftengine/include/bcstatetransfer/SimpleBCStateTransfer.hpp
@@ -164,6 +164,7 @@ struct Config {
   uint32_t gettingMissingBlocksSummaryWindowSize = 0;
   uint16_t minPrePrepareMsgsForPrimaryAwarness = 0;
   uint32_t fetchRangeSize = 0;
+  uint32_t RVT_K = 0;
 
   // timeouts
   uint32_t refreshTimerMs = 0;
@@ -178,6 +179,7 @@ struct Config {
   bool enableReservedPages = true;
   bool enableSourceBlocksPreFetch = true;
   bool enableSourceSelectorPrimaryAwareness = true;
+  bool enableStoreRvbDataDuringCheckpointing = true;
 };
 
 inline std::ostream &operator<<(std::ostream &os, const Config &c) {
@@ -198,7 +200,8 @@ inline std::ostream &operator<<(std::ostream &os, const Config &c) {
               c.minPrePrepareMsgsForPrimaryAwarness,
               c.fetchRangeSize);
   os << ",";
-  os << KVLOG(c.refreshTimerMs,
+  os << KVLOG(c.RVT_K,
+              c.refreshTimerMs,
               c.checkpointSummariesRetransmissionTimeoutMs,
               c.maxAcceptableMsgDelayMs,
               c.sourceReplicaReplacementTimeoutMs,
@@ -207,7 +210,8 @@ inline std::ostream &operator<<(std::ostream &os, const Config &c) {
               c.metricsDumpIntervalSec,
               c.enableReservedPages,
               c.enableSourceBlocksPreFetch,
-              c.enableSourceSelectorPrimaryAwareness);
+              c.enableSourceSelectorPrimaryAwareness,
+              c.enableStoreRvbDataDuringCheckpointing);
   return os;
 }
 // creates an instance of the state transfer module.
