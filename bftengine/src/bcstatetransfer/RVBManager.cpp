4561ffb8fb651d7cc5599b6702053724a529ab6f
Gil Levkovich <glevkovich@vmware.com>
2022-01-25 00:38:12 +0200

    RangeValidationTree: refactor NodeInfo,RVTNode, fix bugs
    1) Rewrite getRvbGroupId().
    2) Add LogPrintVerbosity, to print tree in 2 modes.
    3) Refactor NodeInfo, add helper functions.
    4) Fix the way we calculate rvb_index, also support case in which not
    the 1st node in span is rhe one which opens a new parent.
    5) Each EVT node holds now a vector of childs, and last_insertion_index.
    6) Add getRVTNodeByType() to support all cases where a node is needed
    (parent, siblings etc.)
    7) Update open rightmost/leftmost arrays in a dedicated function
    updateOpenRvtNodeArrays.
    8) Other bug fixes.
    9) Apply needed API changes in ST tests and RVB Manager.

--- bftengine/src/bcstatetransfer/RVBManager.cpp
+++ bftengine/src/bcstatetransfer/RVBManager.cpp
@@ -100,7 +100,7 @@ void RVBManager::init(bool fetching) {
 
   LOG_INFO(logger_, std::boolalpha << KVLOG(pruned_blocks_digests_.size(), desc.checkpointNum, loaded_from_data_store));
   if (print_rvt && (debug_prints_log_level.find(getLogLevel()) != debug_prints_log_level.end())) {
-    in_mem_rvt_->printToLog(false);
+    in_mem_rvt_->printToLog(LogPrintVerbosity::DETAILED);
   }
 }
 
@@ -221,7 +221,7 @@ void RVBManager::updateRvbDataDuringCheckpoint(CheckpointDesc& new_checkpoint_de
     const std::string s = rvb_data.str();
     ConcordAssert(!s.empty());
     std::copy(s.c_str(), s.c_str() + s.length(), back_inserter(new_checkpoint_desc.rvbData));
-    in_mem_rvt_->printToLog(false);
+    in_mem_rvt_->printToLog(LogPrintVerbosity::DETAILED);
   }
   last_checkpoint_desc_ = new_checkpoint_desc;
 }
@@ -357,7 +357,7 @@ bool RVBManager::setSerializedDigestsOfRvbGroup(char* data,
       LOG_ERROR(logger_, error_prefix << KVLOG(block_id, next_expected_rvb_id));
       return false;
     }
-    rvb_group_ids = in_mem_rvt_->getRvbGroupIds(block_id, block_id);
+    rvb_group_ids = in_mem_rvt_->getRvbGroupIds(block_id, block_id, true);
     ConcordAssertEQ(rvb_group_ids.size(), 1);
     if (rvb_group_ids.empty()) {
       LOG_ERROR(logger_, "Bad Digests of RVB group: rvb_group_ids is empty!" << KVLOG(block_id));
@@ -427,7 +427,7 @@ bool RVBManager::setSerializedDigestsOfRvbGroup(char* data,
     stored_rvb_digests_group_ids_.erase(stored_rvb_digests_group_ids_.begin());
     auto it = stored_rvb_digests_.begin();
     while (it != stored_rvb_digests_.end()) {
-      rvb_group_ids = in_mem_rvt_->getRvbGroupIds(it->first, it->first);
+      rvb_group_ids = in_mem_rvt_->getRvbGroupIds(it->first, it->first, true);
       ConcordAssertEQ(rvb_group_ids.size(), 1);
       if (rvb_group_ids[0] == rvb_group_id_removed) {
         it = stored_rvb_digests_.erase(it);
@@ -492,7 +492,7 @@ RVBGroupId RVBManager::getFetchBlocksRvbGroupId(BlockId from_block_id, BlockId t
 RVBGroupId RVBManager::getNextRequiredRvbGroupid(RVBId from_rvb_id, RVBId to_rvb_id) const {
   LOG_TRACE(logger_, KVLOG(from_rvb_id, to_rvb_id));
   if (from_rvb_id > to_rvb_id) return 0;
-  const auto rvb_group_ids = in_mem_rvt_->getRvbGroupIds(from_rvb_id, to_rvb_id);
+  const auto rvb_group_ids = in_mem_rvt_->getRvbGroupIds(from_rvb_id, to_rvb_id, true);
   for (const auto& id : rvb_group_ids) {
     if (std::find(stored_rvb_digests_group_ids_.begin(), stored_rvb_digests_group_ids_.end(), id) ==
         stored_rvb_digests_group_ids_.end()) {
@@ -512,7 +512,7 @@ BlockId RVBManager::getRvbGroupMaxBlockIdOfNonStoredRvbGroup(BlockId from_block_
     return to_block_id;
   }
 
-  const auto rvb_group_ids = in_mem_rvt_->getRvbGroupIds(min_rvb_id, max_rvb_id);
+  const auto rvb_group_ids = in_mem_rvt_->getRvbGroupIds(min_rvb_id, max_rvb_id, true);
   if (rvb_group_ids.size() < 2) {
     return to_block_id;
   }
