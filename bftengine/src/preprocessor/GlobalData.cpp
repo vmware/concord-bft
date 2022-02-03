11e8294d28ee0e45632884172de59df08e42bd07
Ynon Flum <ynon08@gmail.com>
2022-02-03 17:59:43 +0200

    Remove unused global data

--- bftengine/src/preprocessor/GlobalData.cpp
+++ bftengine/src/preprocessor/GlobalData.cpp
@@ -4,7 +4,4 @@ namespace preprocessor {
 
 std::atomic_uint64_t GlobalData::current_block_id = 0;
 std::atomic_uint64_t GlobalData::block_delta = 0;
-std::atomic_uint64_t GlobalData::delta_factor = 60;
-std::atomic_bool GlobalData::increment_step = false;
-std::atomic_bool GlobalData::decrement_step = false;
 }  // namespace preprocessor
--- bftengine/src/preprocessor/GlobalData.hpp
+++ bftengine/src/preprocessor/GlobalData.hpp
@@ -6,9 +6,6 @@ struct GlobalData {
   // Used for the conflict detection optimization.
   static std::atomic_uint64_t current_block_id;
   static std::atomic_uint64_t block_delta;
-  static std::atomic_uint64_t delta_factor;
-  static std::atomic_bool increment_step;
-  static std::atomic_bool decrement_step;
   const static uint8_t step = 10;
 };
 }  // namespace preprocessor
