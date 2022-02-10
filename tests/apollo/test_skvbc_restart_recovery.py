2f4c2572c5a2b0270766aff506b58df0c53f71f2
Hristo Staykov <hstaykov@vmware.com>
2022-02-10 16:54:22 +0200

    Added description of the steps in the current restart/recovery scenarios.

--- tests/apollo/test_skvbc_restart_recovery.py
+++ tests/apollo/test_skvbc_restart_recovery.py
@@ -66,6 +66,8 @@ class SkvbcRestartRecoveryTest(unittest.TestCase):
         skvbc = kvbc.SimpleKVBCProtocol(bft_network)
 
         primary_replica = 0
+
+        # Pick one replica to restart multiple times while the system is processing client requests
         replica_to_restart = random.choice(
             bft_network.all_replicas(without={primary_replica}))
 
@@ -78,18 +80,25 @@ class SkvbcRestartRecoveryTest(unittest.TestCase):
 
         with trio.move_on_after(seconds=60):
             async with trio.open_nursery() as nursery:
+                # Start the sending of client operations in the background.
                 nursery.start_soon(skvbc.send_indefinite_ops)
                 while True:
+                    # Get the total amount of Fast Paths the replicas have committed.
                     total_nb_fast_paths = await bft_network.num_of_fast_path_requests(primary_replica)
+                    # Since we restart the selected replica multiple times in this loop, wait for it to initialise.
                     with trio.fail_after(seconds=5):
                         while replica_to_restart not in bft_network.get_live_replicas():
                             await trio.sleep(seconds=0.1)
                     if replica_to_restart in bft_network.get_live_replicas():
-                        await trio.sleep(seconds=1)  # wait for some client operations to be processed
+                        await trio.sleep(seconds=1)  # wait for some client operations to be processed.
                     log.log_message(f"Restarting replica {replica_to_restart}")
+                    # Restart the selected replica.
                     bft_network.stop_replica(replica_to_restart, True)
                     bft_network.start_replica(replica_to_restart)
+                    # Get the latest value for the fast paths processed in the system.
                     latest_fast_paths = await bft_network.num_of_fast_path_requests(primary_replica)
+                    # Wait for the system to start processing requests on the fast path
+                    # once again since all replicas are up.
                     with trio.fail_after(seconds=15):
                         while latest_fast_paths - total_nb_fast_paths == 0:
                             await trio.sleep(seconds=0.1)
@@ -99,14 +108,19 @@ class SkvbcRestartRecoveryTest(unittest.TestCase):
                     slow_paths = await bft_network.num_of_slow_path_requests(primary_replica)
                     log.log_message(f"slow_paths: {slow_paths}")
 
+        # Before the test ends we verify the Fast Path is prevalent,
+        # no matter the restarts we performed on the selected replica.
+        log.log_message("wait for fast path to be prevalent")
         await bft_network.wait_for_fast_path_to_be_prevalent(
             run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
+        log.log_message("fast path prevailed")
+
 
     @with_trio
     @with_bft_network(start_replica_cmd, selected_configs=lambda n, f, c: f >= 2, rotate_keys=False)
     async def test_restarting_replica_during_system_is_in_view_change(self, bft_network):
         """
-        The goal of this test is trigger multiple View Changes and restart 1 non-next Priamry replica
+        The goal of this test is trigger multiple View Changes and restart 1 non-next Primary replica
         in order to test restart recovery during the replicas performing a View Change.
         """
 
@@ -130,21 +144,32 @@ class SkvbcRestartRecoveryTest(unittest.TestCase):
             _next_primary = (current_primary + 1) % num_replicas
             return _current_primary, _next_primary
 
+        # Perform multiple view changes and restart 1 replica while the replicas are agreeing the new View
         while view < 100:
+            # Pick one replica to restart while the others are agreeing the next View.
+            # We want a replica other than the current primary, which will be restarted to trigger the view change
+            # and we want also the restarted replica to be different from the next primary, since this will cause
+            # additional view change.
             replica_to_restart = random.choice(
                 bft_network.all_replicas(without={current_primary, next_primary}))
             log.log_message(f"Initiating View Change by restarting replica {current_primary} in view {view}")
+            # Stop the current primary.
             bft_network.stop_replica(current_primary, True)
+            # Run client operations to trigger view Change
             await skvbc.run_concurrent_ops(5)
+            # Wait for replicas to start initiating ReplicaAsksToLeaveView msgs followed by ViewChange msgs.
             await trio.sleep(seconds=viewChangeTimeoutSec)
             log.log_message(f"Restarting replica during View Change {replica_to_restart}")
+            # Restart the previously selected replica while the others are performing the view change
             bft_network.stop_replica(replica_to_restart, True)
             bft_network.start_replica(replica_to_restart)
             await trio.sleep(seconds=1)
+            # Start the primary of the previous view
             bft_network.start_replica(current_primary)
 
             old_view = view
 
+            # Wait for quorum of replicas to move to a higher view
             with trio.fail_after(seconds=40):
                 while view == old_view:
                     log.log_message(f"waiting for vc view={view}")
@@ -153,11 +178,14 @@ class SkvbcRestartRecoveryTest(unittest.TestCase):
                         view = await bft_network.get_current_view()
                     await trio.sleep(seconds=1)
 
+            # Update the values of the current_primary and next_primary according to the new view the system is in
             current_primary, next_primary = advance_current_next_primary(view, bft_network.config.n)
 
             log.log_message(f"view is {view}")
 
             log.log_message("Checking for replicas in state transfer:")
+            # Check for replicas in state transfer and wait for them to catch
+            # up before moving on to checking the system returns on Fast Path
             for r in bft_network.get_live_replicas():
                 fetching = await bft_network.is_fetching(r)
                 if fetching:
@@ -174,17 +202,23 @@ class SkvbcRestartRecoveryTest(unittest.TestCase):
                             await skvbc.run_concurrent_ops(50)
                             await trio.sleep(seconds=5)
 
+            # After all replicas have caught up, check if there has been
+            # additional view change while state transfer was completed
             view_after_st = await bft_network.get_current_view()
             log.log_message(f"view_after_st={view_after_st}")
             if view != view_after_st:
                 log.log_message(f"during ST we got a View Change from view {view} to {view_after_st}")
                 view = view_after_st
+                # Update the values of the current_primary and next_primary according to the new view the system is in
                 current_primary, next_primary = advance_current_next_primary(view, bft_network.config.n)
 
             log.log_message("wait for fast path to be prevalent")
+            # Make sure fast path is prevalent before moving to another round ot the test
             await bft_network.wait_for_fast_path_to_be_prevalent(
                 run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=1)
             log.log_message("fast path prevailed")
 
+        # Before the test ends we verify the Fast Path is prevalent, no matter
+        # the restarts we performed while the system was in view change.
         await bft_network.wait_for_fast_path_to_be_prevalent(
             run_ops=lambda: skvbc.run_concurrent_ops(num_ops=20, write_weight=1), threshold=20)
