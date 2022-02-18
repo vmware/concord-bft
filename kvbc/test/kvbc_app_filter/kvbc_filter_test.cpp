5343ef11e5fd256b71bc1f5cfd5b9372af72a9a7
shgandhi <gshruti@vmware.com>
2022-02-18 10:19:05 -0800

    Update KvbAppFilter tests to reflect the change

--- kvbc/test/kvbc_app_filter/kvbc_filter_test.cpp
+++ kvbc/test/kvbc_app_filter/kvbc_filter_test.cpp
@@ -336,12 +336,13 @@ TEST(kvbc_filter_test, kvbfilter_update_success_eg) {
   event2.tags = {"0"};
   event_group.events.emplace_back(event1);
   event_group.events.emplace_back(event2);
+  const auto next_ext_eg_id = EventGroupId{1};
+  const auto &filtered = kvb_filter.filterEventGroupUpdate({eg_id, event_group}, next_ext_eg_id);
 
-  const auto &filtered = kvb_filter.filterEventGroupUpdate({eg_id, event_group});
-
-  EXPECT_EQ(filtered.event_group_id, eg_id);
-  EXPECT_EQ(filtered.event_group.events.size(), 1);
-  EXPECT_EQ(filtered.event_group.events[0].data, "TridVal1");
+  EXPECT_TRUE(filtered.has_value());
+  EXPECT_EQ(filtered.value().event_group_id, eg_id);
+  EXPECT_EQ(filtered.value().event_group.events.size(), 1);
+  EXPECT_EQ(filtered.value().event_group.events[0].data, "TridVal1");
 }
 
 TEST(kvbc_filter_test, kvbfilter_update_message_empty_prefix) {
@@ -819,13 +820,14 @@ TEST(kvbc_filter_test, kvbfilter_success_hash_of_event_group) {
   EventGroupId eg_id_start = 1;
 
   auto hash_value = kvb_filter.readEventGroupHash(eg_id_start);
-  const auto &[eg_id, filtered] = kvb_filter.filterEventGroupUpdate(storage.eg_data_[0]);
-
-  EXPECT_EQ(eg_id, 1);
+  const auto next_ext_eg_id = EventGroupId{1};
+  const auto &filtered = kvb_filter.filterEventGroupUpdate(storage.eg_data_[0], next_ext_eg_id);
+  EXPECT_TRUE(filtered.has_value());
+  EXPECT_EQ(filtered.value().event_group_id, 1);
   std::string concatenated_entry_hashes;
-  concatenated_entry_hashes += blockIdToByteStringLittleEndian(eg_id);
+  concatenated_entry_hashes += blockIdToByteStringLittleEndian(filtered.value().event_group_id);
   std::set<std::string> entry_hashes;
-  for (const auto &event : filtered.events) {
+  for (const auto &event : filtered.value().event_group.events) {
     entry_hashes.emplace(computeSHA256Hash(event.data));
   }
   for (const auto &event_hash : entry_hashes) {
