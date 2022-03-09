526a3dddae079469c12ba62fc986a903df4be036
Evgeniy Dzhurov <edzhurov@vmware.com>
2022-03-09 13:28:01 +0200

    Split application state classes and transaction classes into separate source files. Cleanup includes and unnecessary files.

---
+++ tests/uttDemo/app_state.cpp
@@ -0,0 +1,60 @@
+// Concord
+//
+// Copyright (c) 2018-2020 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0
+// License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+// This module creates an instance of ClientImp class using input
+// parameters and launches a bunch of tests created by TestsBuilder towards
+// concord::consensus::ReplicaImp objects.
+
+#include "app_state.hpp"
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+void Account::depositPublic(int val) { publicBalance_ += val; }
+
+int Account::withdrawPublic(int val) {
+  val = std::min<int>(publicBalance_, val);
+  publicBalance_ -= val;
+  return val;
+}
+
+std::ostream& operator<<(std::ostream& os, const Block& b) {
+  os << "Block " << b.id_ << "\n";
+  os << "---------------------------\n";
+  for (const auto& kvp : b.tx_) os << kvp.first << " : " << kvp.second << '\n';
+  // To-Do: print nullifiers?
+  return os;
+}
+
+AppState::AppState() {
+  accounts_.emplace_back("A1");
+  accounts_.emplace_back("A2");
+  accounts_.emplace_back("A3");
+  blocks_.emplace_back();  // Genesis block
+}
+
+bool AppState::validateTx(const Tx& tx) const { return true; }
+
+void AppState::executeTx(const Tx& tx) {}
+
+void AppState::printLedger(std::optional<int> from, std::optional<int> to) const {
+  int first = from ? *from : 0;
+  int last = to ? *to + 1 : blocks_.size();
+
+  first = std::clamp<int>(first, 0, blocks_.size());
+  last = std::clamp<int>(last, 0, blocks_.size());
+
+  if (first > last) std::swap(first, last);
+
+  for (int i = first; i < last; ++i) std::cout << blocks_[i] << '\n';
+}
---
+++ tests/uttDemo/transactions.hpp
@@ -0,0 +1,55 @@
+// Concord
+//
+// Copyright (c) 2018-2020 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0
+// License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+
+#pragma once
+
+#include <set>
+#include <string>
+#include <variant>
+#include <optional>
+
+struct TxPublicDeposit {
+  TxPublicDeposit(std::string accId, int amount) : amount_{amount}, toAccountId_{std::move(accId)} {}
+
+  int amount_ = 0;
+  std::string toAccountId_;
+};
+std::ostream& operator<<(std::ostream& os, const TxPublicDeposit& tx);
+
+struct TxPublicWithdraw {
+  TxPublicWithdraw(std::string accId, int amount) : amount_{amount}, toAccountId_{std::move(accId)} {}
+
+  int amount_ = 0;
+  std::string toAccountId_;
+};
+std::ostream& operator<<(std::ostream& os, const TxPublicWithdraw& tx);
+
+struct TxPublicTransfer {
+  TxPublicTransfer(std::string fromAccId, std::string toAccId, int amount)
+      : amount_{amount}, fromAccountId_{std::move(fromAccId)}, toAccountId_{std::move(toAccId)} {}
+
+  int amount_ = 0;
+  std::string fromAccountId_;
+  std::string toAccountId_;
+};
+std::ostream& operator<<(std::ostream& os, const TxPublicTransfer& tx);
+
+struct TxUttTransfer {
+  std::string data_;  // some opaque data
+};
+
+using Tx = std::variant<TxPublicDeposit, TxPublicWithdraw, TxPublicTransfer, TxUttTransfer>;
+
+std::ostream& operator<<(std::ostream& os, const Tx& tx);
+
+std::optional<Tx> parseTx(const std::string& str);
