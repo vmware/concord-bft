c1fa1702400920f71f45aec21818b04ca573cbad
Evgeniy Dzhurov <edzhurov@vmware.com>
2022-04-13 18:27:59 +0300

    Rework the scripts for building the utt demo for use with docker compose and put them into steps. Update the README-UTT

---
+++ 7_utt-demo-reset.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker-compose stop
+sudo rm -rf utt-demo-run/logs/* utt-demo-run/rocksdb/*
--- build-utt-demo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-make format
-make TARGET="utt_replica utt_client copy_utt_demo_scripts gen_utt_cfg"
