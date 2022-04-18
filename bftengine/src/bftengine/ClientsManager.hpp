00cd6576fa6bb07b7a7b63b2a9f5900f5dc6b95a
Yulia Sherman <47499870+yuliasherman@users.noreply.github.com>
2022-04-18 17:40:54 +0300

    Fix a race condition between PreProcessor and consensus threads. (#2571)

--- bftengine/src/bftengine/ClientsManager.hpp
+++ bftengine/src/bftengine/ClientsManager.hpp
@@ -19,6 +19,7 @@
 #include "bftengine/IKeyExchanger.hpp"
 #include "PersistentStorage.hpp"
 #include "ReplicaSpecificInfoManager.hpp"
+#include <mutex>
 #include <map>
 #include <set>
 #include <unordered_map>
@@ -83,7 +84,7 @@ class ClientsManager : public ResPagesClient<ClientsManager>, public IPendingReq
   // that client with ID reqSeqNum. Returns false otherwise.
   // TODO(GG): make sure that ReqId is based on time (and ignore requests with time that does
   // not make sense (too high) - this will prevent some potential attacks)
-  bool hasReply(NodeIdType clientId, ReqId reqSeqNum) const;
+  bool hasReply(NodeIdType clientId, ReqId reqSeqNum);
 
   bool isValidClient(NodeIdType clientId) const { return clientIds_.find(clientId) != clientIds_.end(); }
 
@@ -125,7 +126,7 @@ class ClientsManager : public ResPagesClient<ClientsManager>, public IPendingReq
 
   // Returns true if there is a valid client with ID clientId and this ClientsManager currently has a recorded request
   // with ID reqSeqNum from that client; otherwise returns false.
-  bool isClientRequestInProcess(NodeIdType clientId, ReqId reqSeqNum) const;
+  bool isClientRequestInProcess(NodeIdType clientId, ReqId reqSeqNum);
 
   // Returns true IFF there is no pending requests for clientId, and reqSeqNum can become the new pending request, that
   // is, if all of the following are true:
@@ -218,9 +219,45 @@ class ClientsManager : public ResPagesClient<ClientsManager>, public IPendingReq
     bool committed = false;
   };
 
+  class RequestsInfo {
+   public:
+    void emplaceSafe(NodeIdType clientId, ReqId reqSeqNum, const std::string& cid);
+    bool removeRequestsOutOfBatchBoundsSafe(NodeIdType clientId, ReqId reqSequenceNum);
+    bool findSafe(ReqId reqSeqNum);
+    void clearSafe();
+    void removeOldPendingReqsSafe(NodeIdType clientId, ReqId reqSeqNum);
+    void removePendingForExecutionRequestSafe(NodeIdType clientId, ReqId reqSeqNum);
+
+    size_t size() const { return requestsMap_.size(); }
+    bool find(ReqId reqSeqNum) const;
+    bool isPending(ReqId reqSeqNum) const;
+    void markRequestAsCommitted(NodeIdType clientId, ReqId reqSeqNum);
+    void infoOfEarliestPendingRequest(Time& earliestTime, RequestInfo& earliestPendingReqInfo) const;
+    void logAllPendingRequestsExceedingThreshold(const int64_t threshold,
+                                                 const Time& currTime,
+                                                 int& numExceeding) const;
+
+   public:
+    std::mutex requestsMapMutex_;
+    std::map<ReqId, RequestInfo> requestsMap_;
+  };
+
+  class RepliesInfo {
+   public:
+    void deleteOldestReplyIfNeededSafe(NodeIdType clientId, uint16_t maxNumOfReqsPerClient);
+    bool insertOrAssignSafe(ReqId reqSeqNum, Time time);
+    bool findSafe(ReqId reqSeqNum);
+
+    bool find(ReqId reqSeqNum) const;
+
+   public:
+    std::mutex repliesMapMutex_;
+    std::map<ReqId, Time> repliesMap_;
+  };
+
   struct ClientInfo {
-    std::map<ReqId, RequestInfo> requestsInfo;
-    std::map<ReqId, Time> repliesInfo;  // replyId to replyTime
+    std::shared_ptr<RequestsInfo> requestsInfo;
+    std::shared_ptr<RepliesInfo> repliesInfo;
     std::pair<std::string, concord::util::crypto::KeyFormat> pubKey;
   };
 
@@ -237,7 +274,7 @@ class ClientsManager : public ResPagesClient<ClientsManager>, public IPendingReq
   concordMetrics::CounterHandle metric_reply_inconsistency_detected_;
   concordMetrics::CounterHandle metric_removed_due_to_out_of_boundaries_;
   std::unique_ptr<RsiDataManager> rsiManager_;
-};
+};  // namespace impl
 
 }  // namespace impl
 }  // namespace bftEngine
