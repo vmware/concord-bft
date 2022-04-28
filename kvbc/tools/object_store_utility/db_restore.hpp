af7c77b6e9b9b202620acc8b971d456880f4f80c
Toly Kournik <tkournik@vmware.com>
2022-04-28 10:02:39 -0700

    Unify s3_integrity_check and db_restore into a single utility.

--- kvbc/tools/object_store_utility/db_restore.hpp
+++ kvbc/tools/object_store_utility/db_restore.hpp
@@ -10,29 +10,24 @@
 // notices and license terms. Your use of these subcomponents is subject to the
 // terms and conditions of the subcomponent's license, as noted in the LICENSE
 // file.
-
+#pragma once
+#include <memory>
 #include "Logger.hpp"
 #include "categorization/kv_blockchain.h"
-#include "tools/db_integrity_check/integrity_checker.hpp"
+#include "integrity_checker.hpp"
 
 namespace concord::kvbc::tools {
-namespace po = boost::program_options;
 class DBRestore {
  public:
-  DBRestore();
-  /** Parse CLI params */
-  void parseCLIArgs(int argc, char** argv);
+  DBRestore(std::shared_ptr<IntegrityChecker> checker, logging::Logger logger) : checker_(checker), logger_(logger) {}
   void restore();
+  void initRocksDB(const fs::path&);
 
  protected:
-  void initRocksDB(const fs::path&);
   std::unique_ptr<categorization::KeyValueBlockchain> kv_blockchain_;
-  tools::IntegrityChecker integrity_check_;
   IStorageFactory::DatabaseSet rocksdb_dataset_;
-  po::variables_map var_map_;
-  po::options_description cli_options_{"\nDB restore options"};
-  po::options_description cli_mandatory_options_{"\nDB restore mandatory options"};
-  logging::Logger logger_ = logging::getLogger("concord.kvbc.tools.restore");
+  std::shared_ptr<IntegrityChecker> checker_;
+  logging::Logger logger_;
 };
 
 }  // namespace concord::kvbc::tools
