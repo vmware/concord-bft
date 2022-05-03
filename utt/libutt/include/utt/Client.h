902fe48fa0203ea0bd4ba3399f205a7ff3a13d3a
Evgeniy Dzhurov <edzhurov@vmware.com>
2022-05-03 11:17:15 +0300

    Prefer to pass additional data from utt::Client interface methods as part of the result instead through an additional argument

--- utt/libutt/include/utt/Client.h
+++ utt/libutt/include/utt/Client.h
@@ -9,43 +9,46 @@
 
 namespace libutt::Client {
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 size_t calcBalance(const Wallet& w);
 size_t calcBudget(const Wallet& w);
 
-struct CreateTxEvent {
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct CreateTxResult {
   std::string txType_ = "undefined";
   std::vector<size_t> inputNormalCoinValues_;
   std::optional<size_t> inputBudgetCoinValue_;
   std::map<std::string, size_t> recipients_;  // [pid : coinValue]
+  Tx tx;
 };
 
-struct ClaimEvent {
-  bool isBudgetCoin_ = false;
-  size_t value_;
-};
+using CoinStrategy = std::function<CreateTxResult(const Wallet&, const std::string&, size_t)>;
+extern CoinStrategy k_CoinStrategyPreferExactChange;
 
+CreateTxResult createTxForPayment(const Wallet& w,
+                                  const std::string& pid,
+                                  size_t payment,
+                                  const CoinStrategy& strategy = k_CoinStrategyPreferExactChange);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 struct PruneCoinsResult {
   std::vector<size_t> spentCoins_;
   std::optional<size_t> spentBudgetCoin_;
 };
 
-using CoinStrategy = std::function<Tx(const Wallet&, const std::string&, size_t, CreateTxEvent&)>;
-extern CoinStrategy k_CoinStrategyPreferExactChange;
-
-Tx createTxForPayment(const Wallet& w,
-                      const std::string& pid,
-                      size_t payment,
-                      CreateTxEvent& outEvent,
-                      const CoinStrategy& strategy = k_CoinStrategyPreferExactChange);
-
 PruneCoinsResult pruneSpentCoins(Wallet& w, const std::set<std::string>& nullset);
 
-void tryClaimCoin(Wallet& w,
-                  const Tx& tx,
-                  size_t txoIdx,
-                  const std::vector<RandSigShare>& sigShares,
-                  const std::vector<size_t>& signerIds,
-                  size_t n,
-                  std::optional<ClaimEvent>& outEvent);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct ClaimCoinResult {
+  bool isBudgetCoin_ = false;
+  size_t value_;
+};
+
+std::optional<ClaimCoinResult> tryClaimCoin(Wallet& w,
+                                            const Tx& tx,
+                                            size_t txoIdx,
+                                            const std::vector<RandSigShare>& sigShares,
+                                            const std::vector<size_t>& signerIds,
+                                            size_t n);
 
 }  // namespace libutt::Client
