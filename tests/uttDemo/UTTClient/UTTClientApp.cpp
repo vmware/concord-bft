16e0ccb7c164fa85889ed8533cbdc14159890b0c
Evgeniy Dzhurov <edzhurov@vmware.com>
2022-05-16 15:26:34 +0300

    Move UTTClientApp to a separate source file

---
+++ tests/uttDemo/UTTClient/UTTClientApp.cpp
@@ -0,0 +1,114 @@
+// Concord
+//
+// Copyright (c) 2018-2022 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0
+// License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+
+#include "UTTClientApp.hpp"
+
+#include <fstream>
+
+#include <utt/Client.h>
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+UTTClientApp::UTTClientApp(logging::Logger& logger, uint16_t walletId) : logger_{logger} {
+  if (walletId == 0) throw std::runtime_error("wallet id must be a positive value!");
+
+  const std::string fileName = "config/utt_wallet_" + std::to_string(walletId);
+  std::ifstream ifs(fileName);
+  if (!ifs.is_open()) throw std::runtime_error("Missing config: " + fileName);
+
+  UTTClientConfig cfg;
+  ifs >> cfg;
+
+  myPid_ = cfg.wallet_.getUserPid();
+  if (myPid_.empty()) throw std::runtime_error("Empty wallet pid!");
+
+  for (auto& pid : cfg.pids_) {
+    if (pid == myPid_) continue;
+    otherPids_.emplace(std::move(pid));
+  }
+  if (otherPids_.empty()) throw std::runtime_error("Other pids are empty!");
+
+  LOG_INFO(logger_, "Successfully loaded UTT wallet with pid '" << myPid_);
+
+  addAccount(Account{myPid_, cfg.initPublicBalance_});
+  wallet_ = std::move(cfg.wallet_);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+const std::string& UTTClientApp::getMyPid() const { return myPid_; }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+const Account& UTTClientApp::getMyAccount() const { return *getAccountById(myPid_); }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+Account& UTTClientApp::getMyAccount() { return *getAccountById(myPid_); }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+const libutt::Wallet& UTTClientApp::getMyUttWallet() const { return wallet_; }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+const std::set<std::string>& UTTClientApp::getOtherPids() const { return otherPids_; }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+size_t UTTClientApp::getUttBalance() const {
+  size_t balance = 0;
+  for (const auto& c : wallet_.coins) balance += c.getValue();
+  return balance;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+size_t UTTClientApp::getUttBudget() const { return wallet_.budgetCoin ? wallet_.budgetCoin->getValue() : 0; }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+void UTTClientApp::executeTx(const Tx& tx) {
+  UTTBlockchainApp::executeTx(tx);  // Common logic for tx execution
+
+  // Client removes spent coins and attempts to claim output coins
+  if (const auto* txUtt = std::get_if<TxUtt>(&tx)) {
+    std::cout << "Applying UTT tx " << txUtt->utt_.getHashHex() << '\n';
+    pruneSpentCoins();
+    tryClaimCoins(*txUtt);
+    std::cout << '\n';
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+void UTTClientApp::pruneSpentCoins() {
+  auto result = libutt::Client::pruneSpentCoins(wallet_, nullset_);
+
+  for (const size_t value : result.spentCoins_)
+    std::cout << " - \'" << wallet_.getUserPid() << "' removes spent " << fmtCurrency(value) << " normal coin.\n";
+
+  if (result.spentBudgetCoin_)
+    std::cout << " - \'" << wallet_.getUserPid() << "' removes spent " << fmtCurrency(*result.spentBudgetCoin_)
+              << " budget coin.\n";
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+void UTTClientApp::tryClaimCoins(const TxUtt& tx) {
+  // Add any new coins
+  const size_t n = 4;  // [TODO-UTT] Get from config
+  if (!tx.sigShares_) throw std::runtime_error("Missing sigShares in utt tx!");
+  const auto& sigShares = *tx.sigShares_;
+
+  size_t numTxo = tx.utt_.outs.size();
+  if (numTxo != sigShares.sigShares_.size())
+    throw std::runtime_error("Number of output coins differs from provided sig shares!");
+
+  for (size_t i = 0; i < numTxo; ++i) {
+    auto result = libutt::Client::tryClaimCoin(wallet_, tx.utt_, i, sigShares.sigShares_[i], sigShares.signerIds_, n);
+    if (result) {
+      std::cout << " + \'" << myPid_ << "' claims " << fmtCurrency(result->value_)
+                << (result->isBudgetCoin_ ? " budget" : " normal") << " coin.\n";
+    }
+  }
+}
