9c9dadce4c1c17aedc19eea64721c0f7498fa1e5
Evgeniy Dzhurov <edzhurov@vmware.com>
2022-05-16 16:22:59 +0300

    Integrate the 'balance' and 'accounts' commands through the 'show' command

--- tests/uttDemo/UTTClient/UTTClientApp.hpp
+++ tests/uttDemo/UTTClient/UTTClientApp.hpp
@@ -22,9 +22,7 @@ class UTTClientApp : public UTTBlockchainApp {
   UTTClientApp(logging::Logger& logger, uint16_t walletId);
 
   const std::string& getMyPid() const;
-
   const Account& getMyAccount() const;
-  Account& getMyAccount();
 
   const libutt::Wallet& getMyUttWallet() const;
   const std::set<std::string>& getOtherPids() const;
@@ -32,8 +30,20 @@ class UTTClientApp : public UTTBlockchainApp {
   size_t getUttBalance() const;
   size_t getUttBudget() const;
 
+  // Prints the state of the client given a selector that
+  // represents a hierarchical path to some part of the state
+  // Examples:
+  // default: prints the account balance
+  // 'wallet' - prints the content of the utt wallet
+  // 'wallet/coins' - prints a list of the wallet coins
+  // 'wallet/coins/0' - prints details about the first coin
+  // 'ledger' - prints the contents of the ledger
+  // 'ledger/0' - prints the contents of the first block of the ledger
+
+  void printState(const std::string& selector = "") const;
+
   template <typename T>
-  std::string fmtCurrency(T val) {
+  std::string fmtCurrency(T val) const {
     return "$" + std::to_string(val);
   }
 
@@ -41,6 +51,8 @@ class UTTClientApp : public UTTBlockchainApp {
   void executeTx(const Tx& tx) override;
   void pruneSpentCoins();
   void tryClaimCoins(const TxUtt& tx);
+
+  void printOtherPids() const;
 
   logging::Logger& logger_;
   std::string myPid_;
