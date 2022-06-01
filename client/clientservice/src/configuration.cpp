vmware/concord-bft

commit 794ce040aff48960e9382c6473eac41db78d8ac2

Unify_certs: Changes for new certificate scheme for Replica/TRS and Client/TRC along with UT (#2613)

client/clientservice/src/configuration.cpp
@@ -130,6 +130,7 @@ void parseConfigFile(ConcordClientConfig& config, const YAML::Node& yaml) {
     comm_type = concord::client::concordclient::TransportConfig::TlsTcp;
     readYamlField(yaml, "tls_certificates_folder_path", config.transport.tls_cert_root_path);
     readYamlField(yaml, "tls_cipher_suite_list", config.transport.tls_cipher_suite);
+    readYamlField(yaml, "use_unified_certificates", config.transport.use_unified_certs);
   } else if (comm == "udp") {
     comm_type = concord::client::concordclient::TransportConfig::PlainUdp;
   } else {
@@ -145,6 +146,10 @@ void parseConfigFile(ConcordClientConfig& config, const YAML::Node& yaml) {
 
   readYamlField(node["participant_node"][0], "participant_node_host", config.client_service.host);
   readYamlField(node["participant_node"][0], "principal_id", config.client_service.id.val);
+
+  if (config.transport.use_unified_certs) {
+    readYamlField(node["participant_node"][0], "clientservice_host_uuid", config.client_service.host_uuid);
+  }
 
   for (const auto& item : node["participant_node"][0]["external_clients"]) {
     ConcordAssert(item.IsMap());
@@ -166,23 +171,40 @@ void configureSubscription(concord::client::concordclient::ConcordClientConfig& 
                            const std::string& tr_id,
                            bool is_insecure,
                            const std::string& tls_path) {
-  config.subscribe_config.id = tr_id;
   config.subscribe_config.use_tls = not is_insecure;
+  std::string cert_client_id;
+  std::string client_cert_path;
+  std::string base_path;
 
   if (config.subscribe_config.use_tls) {
-    LOG_INFO(logger, "TLS for thin replica client is enabled, certificate path: " << tls_path);
-    const std::string client_cert_path = tls_path + "/client.cert";
+    LOG_INFO(logger,
+             "TLS for thin replica client is enabled, use_unified_certs: " << config.transport.use_unified_certs);
+
+    if (config.transport.use_unified_certs) {
+      LOG_INFO(logger, " Certificates path" << config.transport.tls_cert_root_path);
+      base_path = config.transport.tls_cert_root_path + "/" + std::to_string(config.client_service.id.val);
+      client_cert_path = base_path + "/node.cert";
+
+      // With unified certificates, clientservice_host_uuid will be used instead of tr-id from the environment variable.
+      // Config file clientservice_host_uuid will be compared to the 'O' field of the client certificate for
+      // authentication.
+      config.subscribe_config.id = config.client_service.host_uuid;
+    } else {
+      LOG_INFO(logger, " Certificates path" << tls_path);
+      base_path = tls_path;
+      client_cert_path = tls_path + "/client.cert";
+      config.subscribe_config.id = tr_id;
+    }
 
     readCert(client_cert_path, config.subscribe_config.pem_cert_chain);
+    config.subscribe_config.pem_private_key = decryptPrivateKey(config.transport.secret_data, base_path);
+    cert_client_id = getClientIdFromClientCert(client_cert_path, config.transport.use_unified_certs);
 
-    config.subscribe_config.pem_private_key = decryptPrivateKey(config.transport.secret_data, tls_path);
-
-    std::string cert_client_id = getClientIdFromClientCert(client_cert_path);
     // The client cert must have the client ID in the OU field, because the TRS obtains
     // the client_id from the certificate of the connecting client.
     if (cert_client_id.empty()) {
       LOG_FATAL(logger, "Failed to construct concord client.");
-      throw std::runtime_error("The OU field in client certificate is empty. It must contain the client ID.");
+      throw std::runtime_error("The 'O' field in client certificate is empty. It must contain the client ID.");
     }
     // cert_client_id in client cert should match the client_id if TLS is
     // enabled for TRC-TRS connection. Since the TRS reads the client id from
@@ -192,8 +214,8 @@ void configureSubscription(concord::client::concordclient::ConcordClientConfig& 
     // key value pairs meant for the requesting client.
     if (cert_client_id.compare(config.subscribe_config.id) != 0) {
       LOG_FATAL(logger, "Failed to construct concord client.");
-      throw std::runtime_error("The client ID in the OU field of the client certificate (" + cert_client_id +
-                               ") does not match the client ID in the environment variable (" +
+      throw std::runtime_error("The client ID in the 'O' field of client certificate (" + cert_client_id +
+                               ") does not match the client ID in the input configuration file (" +
                                config.subscribe_config.id + ").");
     }
   } else {
@@ -207,11 +229,25 @@ void configureTransport(concord::client::concordclient::ConcordClientConfig& con
                         bool is_insecure,
                         const std::string& tls_path) {
   if (not is_insecure) {
-    const std::string server_cert_path = tls_path + "/server.cert";
-    // read server TLS certs for this TRC instance
-    // server_cert_path specifies the path to a composite cert file i.e., a
-    // concatenation of the certificates of all known servers
-    readCert(server_cert_path, config.transport.event_pem_certs);
+    std::string server_cert_path;
+    if (config.transport.use_unified_certs) {
+      LOG_INFO(logger, "TLS Certificates Path: " << config.transport.tls_cert_root_path);
+      // Read all server certificates for this TRC instance and concatenate them to one buffer
+      for (size_t i = 0; i < config.topology.replicas.size(); ++i) {
+        server_cert_path = config.transport.tls_cert_root_path + "/" + std::to_string(i) + "/node.cert";
+        std::string out_certs = "";
+        readCert(server_cert_path, out_certs);
+        config.transport.event_pem_certs += out_certs;
+      }
+    } else {
+      LOG_INFO(logger, "TLS Certificates Path: " << tls_path);
+      server_cert_path = tls_path + "/server.cert";
+      // read server TLS certs for this TRC instance
+      // server_cert_path specifies the path to a composite cert file i.e., a
+      // concatentation of the certificates of all known servers
+      readCert(server_cert_path, config.transport.event_pem_certs);
+    }
+    LOG_INFO(logger, "Certificate chain: " << config.transport.event_pem_certs);
   }
 }
 
@@ -257,9 +293,10 @@ void readCert(const std::string& input_filename, std::string& out_data) {
   }
 }
 
-std::string getClientIdFromClientCert(const std::string& client_cert_path) {
+std::string getClientIdFromClientCert(const std::string& client_cert_path, bool use_unified_certs) {
   std::array<char, 128> buffer;
   std::string client_id;
+  std::string delimiter;
 
   // check if client cert can be opened
   std::ifstream input_file(client_cert_path.c_str(), std::ios::in);
@@ -275,18 +312,20 @@ std::string getClientIdFromClientCert(const std::string& client_cert_path) {
   if (!pipe) {
     throw std::runtime_error("Failed to read subject fields from client cert - popen() failed!");
   }
+
+  // parse the O field i.e., the client id from the subject field when
+  // unified certificates are used, else parse OU field.
+  delimiter = (use_unified_certs) ? "O = " : "OU = ";
   if (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-    // parse the OU field i.e., the client id from the subject field
-    client_id = parseClientIdFromSubject(buffer.data());
+    client_id = parseClientIdFromSubject(buffer.data(), delimiter);
   }
   return client_id;
 }
 
 // Parses the value of the OU field i.e., the client id from the subject
 // string
-std::string parseClientIdFromSubject(const std::string& subject_str) {
-  std::string delim = "OU = ";
-  size_t start = subject_str.find(delim) + delim.length();
+std::string parseClientIdFromSubject(const std::string& subject_str, const std::string& delimiter) {
+  size_t start = subject_str.find(delimiter) + delimiter.length();
   size_t end = subject_str.find(',', start);
   std::string raw_str = subject_str.substr(start, end - start);
   size_t fstart = 0;

bftengine/src/bftengine/KeyExchangeManager.cpp
@@ -145,10 +145,12 @@ void KeyExchangeManager::loadPublicKeys() {
 void KeyExchangeManager::exchangeTlsKeys(const std::string& type, const SeqNum& bft_sn) {
   auto keys = concord::util::crypto::Crypto::instance().generateECDSAKeyPair(
       concord::util::crypto::KeyFormat::PemFormat, concord::util::crypto::CurveType::secp384r1);
-  std::string root_path =
-      bftEngine::ReplicaConfig::instance().certificatesRootPath + "/" + std::to_string(repID_) + "/" + type;
+  bool use_unified_certs = bftEngine::ReplicaConfig::instance().useUnifiedCertificates;
+  const std::string base_path =
+      bftEngine::ReplicaConfig::instance().certificatesRootPath + "/" + std::to_string(repID_);
+  std::string root_path = (use_unified_certs) ? base_path : base_path + "/" + type;
 
-  std::string cert_path = root_path + "/" + type + ".cert";
+  std::string cert_path = (use_unified_certs) ? root_path + "/node.cert" : root_path + "/" + type + ".cert";
   std::string prev_key_pem = concord::util::crypto::Crypto::instance()
                                  .RsaHexToPem(std::make_pair(SigManager::instance()->getSelfPrivKey(), ""))
                                  .first;

client/clientservice/include/client/clientservice/configuration.hpp
@@ -40,10 +40,10 @@ const std::string decryptPrivateKey(const std::optional<secretsmanager::SecretDa
 void readCert(const std::string& input_filename, std::string& out_data);
 
 // This method gets the client_id from the OU field in the client certificate
-std::string getClientIdFromClientCert(const std::string& client_cert_path);
+std::string getClientIdFromClientCert(const std::string& client_cert_path, bool use_unified_certs);
 
 // This method is used by getClientIdFromClientCert to get the client_id from
 // the subject in the client certificate
-std::string parseClientIdFromSubject(const std::string& subject_str);
+std::string parseClientIdFromSubject(const std::string& subject_str, const std::string& delimiter);
 
 }  // namespace concord::client::clientservice

client/clientservice/test/example_config.hpp
@@ -48,6 +48,7 @@ tls_cipher_suite_list: ECDHE-ECDSA-AES256-GCM-SHA384
 tls_1_3_cipher_suite_list: TLS_AES_256_GCM_SHA384
 transaction_signing_enabled: true
 enable_multiplex_channel: false
+use_unified_certificates: false
 with_cre: false
 node:
   - replica:

client/concordclient/include/client/concordclient/concord_client.hpp
@@ -72,6 +72,7 @@ struct BftTopology {
 struct ClientServiceInfo {
   bft::client::ClientId id;
   std::string host;
+  std::string host_uuid;
 };
 
 struct BftClientInfo {
@@ -91,6 +92,7 @@ struct TransportConfig {
   // TLS settings ignored if comm_type is not TlsTcp
   std::string tls_cert_root_path;
   std::string tls_cipher_suite;
+  bool use_unified_certs;
   std::optional<concord::secretsmanager::SecretData> secret_data;
   // Buffer with the servers' PEM encoded certificates for the event port
   std::string event_pem_certs;

client/reconfiguration/CMakeLists.txt
@@ -7,7 +7,9 @@ add_library(cre
         src/default_handlers.cpp
         )
 
-target_include_directories(cre PUBLIC include)
+target_include_directories(cre PUBLIC 
+        include
+        ${bftengine_SOURCE_DIR}/include/bftengine)
 
 target_link_libraries(cre PUBLIC
         corebft

client/reconfiguration/src/default_handlers.cpp
@@ -13,6 +13,7 @@
 #include "bftclient/StateControl.hpp"
 #include "concord.cmf.hpp"
 #include "crypto_utils.hpp"
+#include "ReplicaConfig.hpp"
 
 #include <variant>
 #include <experimental/filesystem>
@@ -71,8 +72,11 @@ bool ClientTlsKeyExchangeHandler::execute(const State& state, WriteState&) {
   if (master_key.empty()) LOG_FATAL(getLogger(), "unable to read the node master key");
   auto root = fs::path(cert_folder_);
   for (const auto& c : bft_clients_) {
-    fs::path pkey_path = root / std::to_string(c) / "client" / ("pk.pem" + suffix);
-    fs::path cert_path = root / std::to_string(c) / "client" / "client.cert";
+    fs::path root_path = root / std::to_string(c);
+    bool use_unified_certs = bftEngine::ReplicaConfig::instance().useUnifiedCertificates;
+    fs::path pkey_path =
+        (use_unified_certs) ? root_path / ("pk.pem" + suffix) : root_path / "client" / ("pk.pem" + suffix);
+    fs::path cert_path = (use_unified_certs) ? root_path / "node.cert" : root_path / "client" / "client.cert";
 
     auto cert =
         concord::util::crypto::CertificateUtils::generateSelfSignedCert(cert_path, new_cert_keys.second, master_key);

communication/include/communication/CommDefs.hpp
@@ -16,6 +16,7 @@
 #include <cstdint>
 #include <memory>
 #include <unordered_map>
+#include <unordered_set>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <sys/socket.h>

commit e35c6460004817dc1c7d99674b1d855d801a7759

TRC ID needs to be populated irrespective of in(secure) TRC-TRS conn.
The bug was introduced in https://github.com/vmware/concord-bft/pull/2613.

client/clientservice/src/configuration.cpp
@@ -171,6 +171,7 @@ void configureSubscription(concord::client::concordclient::ConcordClientConfig& 
                            const std::string& tr_id,
                            bool is_insecure,
                            const std::string& tls_path) {
+  config.subscribe_config.id = tr_id;
   config.subscribe_config.use_tls = not is_insecure;
   std::string cert_client_id;
   std::string client_cert_path;
@@ -193,7 +194,6 @@ void configureSubscription(concord::client::concordclient::ConcordClientConfig& 
       LOG_INFO(logger, " Certificates path" << tls_path);
       base_path = tls_path;
       client_cert_path = tls_path + "/client.cert";
-      config.subscribe_config.id = tr_id;
     }
 
     readCert(client_cert_path, config.subscribe_config.pem_cert_chain);