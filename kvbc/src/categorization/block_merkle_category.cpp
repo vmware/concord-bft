96ce95ab972d40566c142bf3cb11823202170ec2
Yehonatan Buchnik <yontyon@gmail.com>
2022-06-09 08:27:23 +0300

    Fix unused variable error

--- kvbc/src/categorization/block_merkle_category.cpp
+++ kvbc/src/categorization/block_merkle_category.cpp
@@ -561,6 +561,7 @@ std::vector<std::string> BlockMerkleCategory::getBlockStaleKeys(BlockId block_id
   std::vector<Hash> hash_stale_keys;
   auto [hashed_keys, _, latest_versions] = getLatestVersions(out);
   (void)_;
+  (void)latest_versions;
   for (auto i = 0u; i < hashed_keys.size(); i++) {
     auto& tagged_version = latest_versions[i];
     auto& hashed_key = hashed_keys[i];
