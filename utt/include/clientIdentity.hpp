9b103ce4611cd159dbbfad2f764d4de5b5a2b68c
Yehonatan Buchnik <yontyon@gmail.com>
2022-07-19 06:57:20 +0300

    Add client identity functionality to the new API

---
+++ utt/include/clientIdentity.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "commitment.hpp"
+#include "details.hpp"
+#include "common.hpp"
+#include <memory>
+namespace libutt {
+class AddrSK;
+class RandSigPK;
+}  // namespace libutt
+namespace libutt::api {
+class ClientIdentity {
+ public:
+  ClientIdentity(const std::string& pid, const std::string& bpk);
+  Commitment generatePartialRCM(Details& d);
+  std::string getPid() const;
+  std::vector<uint64_t> getPRFSecretKey() const;
+  std::vector<uint64_t> getPidHash() const;
+  void setIBEDetails(const std::vector<uint8_t>& sk, const std::vector<uint8_t>& mpk);
+  void setRCM(const Commitment& comm, const std::vector<uint8_t>& sig);
+  void randomizeRCM();
+  std::pair<Commitment, std::vector<uint8_t>> getRcm() const;
+  template <typename T>
+  bool validate(const T&);
+
+ private:
+  std::unique_ptr<libutt::AddrSK> ask_;
+  std::unique_ptr<libutt::RandSigPK> bpk_;
+  Commitment rcm_;
+  std::vector<uint8_t> rcm_sig_;
+};
+}  // namespace libutt::api
---
+++ utt/libutt/src/clientIdentity.cpp
@@ -0,0 +1,56 @@
+#include "clientIdentity.hpp"
+#include "coin.hpp"
+#include <utt/IBE.h>
+#include <utt/Address.h>
+#include <utt/RandSig.h>
+#include <utt/Params.h>
+#include <utt/Serialization.h>
+#include <utt/Coin.h>
+#include <vector>
+#include <sstream>
+namespace libutt::api {
+ClientIdentity::ClientIdentity(const std::string& pid, const std::string& bpk) {
+  ask_.reset(new libutt::AddrSK());
+  ask_->pid = pid;
+  ask_->s = Fr::random_element();
+  ask_->pid_hash = AddrSK::pidHash(pid);
+  bpk_.reset(new libutt::RandSigPK());
+  *bpk_ = libutt::deserialize<libutt::RandSigPK>(bpk);
+}
+Commitment ClientIdentity::generatePartialRCM(Details& d) {
+  std::vector<std::vector<uint64_t>> m = {getPidHash(), ask_->s.to_words(), Fr::zero().to_words()};
+  auto comm = Commitment(d, Commitment::Type::REGISTRATION, m, true);
+  return comm;
+}
+
+std::string ClientIdentity::getPid() const { return ask_->pid; }
+std::vector<uint64_t> ClientIdentity::getPidHash() const { return ask_->getPidHash().to_words(); }
+std::vector<uint64_t> ClientIdentity::getPRFSecretKey() const { return ask_->s.to_words(); }
+
+void ClientIdentity::setIBEDetails(const std::vector<uint8_t>& sk, const std::vector<uint8_t>& mpk) {
+  ask_->e = libutt::deserialize<libutt::IBE::EncSK>(std::string(sk.begin(), sk.end()));
+  ask_->mpk_ = libutt::deserialize<libutt::IBE::MPK>(std::string(mpk.begin(), mpk.end()));
+}
+
+void ClientIdentity::setRCM(const Commitment& comm, const std::vector<uint8_t>& sig) {
+  rcm_ = comm;
+  rcm_sig_ = sig;
+  ask_->rs = libutt::deserialize<libutt::RandSig>(sig);
+}
+
+std::pair<Commitment, std::vector<uint8_t>> ClientIdentity::getRcm() const { return {rcm_, rcm_sig_}; }
+template <>
+bool ClientIdentity::validate<Coin>(const Coin& c) {
+  return c.coin_->hasValidSig(*bpk_);
+}
+void ClientIdentity::randomizeRCM() {
+  auto r_delta = rcm_.randomize(Details::instance(), Commitment::Type::REGISTRATION);
+  Fr fr_r_delta;
+  fr_r_delta.from_words(r_delta);
+  auto rand_sig = libutt::deserialize<libutt::RandSig>(rcm_sig_);
+  rand_sig.rerandomize(fr_r_delta, Fr::random_element());
+  auto tmp_sig = libutt::serialize<libutt::RandSig>(rand_sig);
+  rcm_sig_ = std::vector<uint8_t>(tmp_sig.begin(), tmp_sig.end());
+}
+
+}  // namespace libutt::api
