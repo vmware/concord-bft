1c03fc99965eba5a8345cc2f864c5f8b5fbb13fd
Yehonatan Buchnik <yontyon@gmail.com>
2022-07-19 06:39:49 +0300

    Add functionality to generate shares registration keys

--- utt/libutt/include/utt/RegAuth.h
+++ utt/libutt/include/utt/RegAuth.h
@@ -8,11 +8,18 @@
 namespace libutt {
 class AddrSK;
 class RegAuthPK;
+class RegAuthSharePK;
+class RegAuthShareSK;
 }  // namespace libutt
 
 std::ostream& operator<<(std::ostream& out, const libutt::RegAuthPK& rpk);
 std::istream& operator>>(std::istream& in, libutt::RegAuthPK& rpk);
 
+std::ostream& operator<<(std::ostream& out, const libutt::RegAuthSharePK& rpk);
+std::istream& operator>>(std::istream& in, libutt::RegAuthSharePK& rpk);
+
+std::ostream& operator<<(std::ostream& out, const libutt::RegAuthShareSK& rsk);
+std::istream& operator>>(std::istream& in, libutt::RegAuthShareSK& rsk);
 namespace libutt {
 
 class RegAuthPK {
@@ -27,6 +34,18 @@ class RegAuthPK {
   bool operator!=(const RegAuthPK& o) const { return !operator==(o); }
 };
 
+class RegAuthSharePK {
+ public:
+  RandSigSharePK vk;  // re-randomizable signature PK, needed to verify registration commitments
+
+  IBE::MPK mpk;  // IBE MPK, need to derive encryption PKs for users by TXN creators
+
+ public:
+  bool operator==(const RegAuthSharePK& o) const { return vk == o.vk && mpk == o.mpk; }
+
+  bool operator!=(const RegAuthSharePK& o) const { return !operator==(o); }
+};
+
 class RegAuthSK {
  public:
   CommKey ck_reg;  // registration commitment key, needed to create registration commitments
@@ -34,12 +53,14 @@ class RegAuthSK {
 
   IBE::Params p;
   IBE::MSK msk;  // IBE MSK, needed to issue encryption SKs to users
+  std::vector<RegAuthShareSK> shares;
 
  public:
   RegAuthSK() {}
 
  public:
   static RegAuthSK random(const CommKey& ck_reg, const IBE::Params& p);
+  static RegAuthSK generateKeyAndShares(CommKey& baseComm, size_t t, size_t n, const IBE::Params& p);
 
  public:
   // AddrSK randomUser() const;
@@ -47,6 +68,25 @@ class RegAuthSK {
 
   RegAuthPK toPK() const {
     RegAuthPK pk;
+    pk.vk = sk.toPK();
+    pk.mpk = msk.toMPK(p);
+    return pk;
+  }
+};
+
+class RegAuthShareSK {
+ public:
+  CommKey ck_reg;     // registration commitment key, needed to create registration commitments
+  RandSigShareSK sk;  // re-randomizable signature shared SK, needed to sign registration commitments
+
+  IBE::Params p;
+  IBE::MSK msk;  // IBE MSK, needed to issue encryption SKs to users
+
+ public:
+  RegAuthShareSK() {}
+
+  RegAuthSharePK toPK() const {
+    RegAuthSharePK pk;
     pk.vk = sk.toPK();
     pk.mpk = msk.toMPK(p);
     return pk;
--- utt/libutt/src/RegAuth.cpp
+++ utt/libutt/src/RegAuth.cpp
@@ -6,6 +6,7 @@
 #include <utt/Comm.h>
 #include <utt/RandSig.h>
 #include <utt/RegAuth.h>
+#include <utt/PolyOps.h>
 
 std::ostream& operator<<(std::ostream& out, const libutt::RegAuthPK& rpk) {
   out << rpk.vk;
@@ -19,6 +20,27 @@ std::istream& operator>>(std::istream& in, libutt::RegAuthPK& rpk) {
   return in;
 }
 
+std::ostream& operator<<(std::ostream& out, const libutt::RegAuthSharePK& rpk) {
+  out << rpk.vk;
+  out << rpk.mpk;
+  return out;
+}
+std::istream& operator>>(std::istream& in, libutt::RegAuthSharePK& rpk) {
+  in >> rpk.vk;
+  in >> rpk.mpk;
+  return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const libutt::RegAuthShareSK& rsk) {
+  out << rsk.sk;
+  out << rsk.msk;
+  return out;
+}
+std::istream& operator>>(std::istream& in, libutt::RegAuthShareSK& rsk) {
+  in >> rsk.sk;
+  in >> rsk.msk;
+  return in;
+}
 namespace libutt {
 
 RegAuthSK RegAuthSK::random(const CommKey& ck_reg, const IBE::Params& p) {
@@ -36,6 +58,70 @@ RegAuthSK RegAuthSK::random(const CommKey& ck_reg, const IBE::Params& p) {
   return rsk;
 }
 
+RegAuthSK RegAuthSK::generateKeyAndShares(CommKey& ck_reg, size_t t, size_t n, const IBE::Params& p_) {
+  (void)(ck_reg);
+  RegAuthSK ras;
+  ras.p = p_;
+  ras.msk = IBE::MSK::random();
+  size_t ell = 2;  // Registration commit contains only 3 messages
+  // degree t-1 polynomial f(X) such that t players can reconstruct f(0) = x, which encodes the bank's secret key x
+  std::vector<Fr> poly_f = random_field_elems(t);
+  Fr x = poly_f[0];
+
+  // degree t-1 polynomials Y_k(X) for secret sharing the y_k = Y_k(0)
+  std::vector<std::vector<Fr>> polys_y;
+  // PS16 secrets y_1, y_2, \dots, y_k
+  std::vector<Fr> y;
+  for (size_t k = 0; k < ell; k++) {
+    auto poly = random_field_elems(t);
+    polys_y.push_back(poly);
+    y.push_back(poly[0]);
+  }
+
+  G1 g = G1::random_element();
+  G2 g_tilde = G2::random_element();
+  auto ck = CommKey::fromTrapdoor(g, g_tilde, y);
+
+  ras.ck_reg = ck;
+  // initialize the single-server PS16 SK (useful for testing)
+  ras.sk.ck = ck;
+  ras.sk.x = x;
+  ras.sk.y = y;
+  ras.sk.X = x * g;
+
+  /**
+   * Compute the SK shares [x]_i, [y_k]_i, \forall k\in \{1,\dots,\ell\}
+   * i.e., evaluate the secret-sharing polys at n pre-determined points (i.e., Nth roots of unity, with smallest N = 2^k
+   * >= n)
+   */
+  std::vector<Fr> evals_f = poly_fft(poly_f, n);
+
+  std::vector<std::vector<Fr>> evals_y;
+  for (size_t k = 0; k < ell; k++) {
+    evals_y.push_back(poly_fft(polys_y[k], n));
+  }
+
+  // [x]_i = f(\omega_N^i)
+  // [y_k]_i = Y_k(\omega_N^i), \forall k\in\{1,\dots,\ell\}
+  std::vector<RegAuthShareSK> skRegShares;
+  for (size_t i = 0; i < n; i++) {
+    RegAuthShareSK as;
+    RandSigShareSK ss;
+
+    ss.ck = ras.ck_reg;
+    ss.x_and_ys.push_back(evals_f[i]);
+    for (size_t k = 0; k < ell; k++) {
+      ss.x_and_ys.push_back(evals_y[k][i]);
+    }
+    as.sk = ss;
+    as.ck_reg = ras.ck_reg;
+    as.p = ras.p;
+    as.msk = ras.msk;
+    skRegShares.push_back(as);
+  }
+  ras.shares = skRegShares;
+  return ras;
+}
 // AddrSK RegAuthSK::randomUser() const {
 //    // WARNING: Not a secure PRNG, but okay for this.
 //    return registerUser(std::to_string(rand()) + "@rand.com");
