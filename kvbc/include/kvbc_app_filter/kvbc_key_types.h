d6d8c131a2c9685dc7900ca4fee49a2ac7ef3dab
Senthil Balakrishnan <balakrishnas@vmware.com>
2022-09-19 14:16:02 +0000

    Organize ethereum transaction logs onto its only key space for event/log filtering and pruning.

--- kvbc/include/kvbc_app_filter/kvbc_key_types.h
+++ kvbc/include/kvbc_app_filter/kvbc_key_types.h
@@ -25,7 +25,8 @@ const char kKvbKeyEthCode = 0x04;
 const char kKvbKeyEthStorage = 0x05;
 const char kKvbKeyEthNonce = 0x06;
 const char kKvbKeyEthBlockHash = 0x07;
-
+const char kKvbKeyEthEventLog = 0x08;
+ 
 // Unused 0x10 - 0x1f
 
 // Concord 0x20 - 0x2f
