9942bf984eb6f8d6699490b6542768436d34ee42
Gil Levkovich <glevkovich@vmware.com>
2022-09-29 14:19:01 +0300

    TesterReplica main.cpp: convert signal to sigaction
    sigaction is almost always preferable, and should be used in a
    multuthreaded enviorment.

--- tests/simpleKVBC/TesterReplica/main.cpp
+++ tests/simpleKVBC/TesterReplica/main.cpp
@@ -199,8 +199,13 @@ static void signal_handler(int signal_num) {
 }  // namespace
 
 int main(int argc, char** argv) {
-  signal(SIGINT, signal_handler);
-  signal(SIGTERM, signal_handler);
+  struct sigaction sa;
+  LOG_INFO(GL, "skvbc_replia (concord-bft tester replica) starting...");
+  memset(&sa, 0, sizeof(sa));
+  sa.sa_handler = signal_handler;
+  sigfillset(&sa.sa_mask);
+  sigaction(SIGINT, &sa, NULL);
+  sigaction(SIGTERM, &sa, NULL);
 
   try {
     concord::kvbc::test::run_replica(argc, argv);
@@ -208,6 +213,6 @@ int main(int argc, char** argv) {
     LOG_FATAL(GL, "exception: " << e.what());
   }
   __lsan_do_recoverable_leak_check();
-
+  LOG_INFO(GL, "skvbc_replia (concord-bft tester replica) shutting down...");
   return 0;
 }
