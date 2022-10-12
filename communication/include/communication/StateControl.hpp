1270b48bb6eaa3a023bb04fbb93b41ec03a4914a
anshu mishra <anshum2@vmware.com>
2022-10-12 10:12:45 +0000

    Added code to bundle events

--- communication/include/communication/StateControl.hpp
+++ communication/include/communication/StateControl.hpp
@@ -14,9 +14,23 @@
 #include <functional>
 #include <utility>
 #include "callback_registry.hpp"
+#include <unordered_map>
+#include <variant>
+#include <memory>
+
 namespace bft::communication {
+struct EnumHash {
+  template <typename T>
+  size_t operator()(T t) const {
+    return static_cast<size_t>(t);
+  }
+};
+
 class StateControl {
  public:
+ using CallbackRegistry = concord::util::CallbackRegistry<uint32_t>;
+  enum class EventType { TLS_COMM, THIN_REPLICA_SERVER };
+
   static StateControl& instance() {
     static StateControl instance_;
     return instance_;
@@ -26,24 +40,50 @@ class StateControl {
   bool tryLockComm() { return lock_comm_.try_lock(); }
   void unlockComm() { lock_comm_.unlock(); }
 
-  void setCommRestartCallBack(std::function<void(uint32_t)> cb) {
-    if (cb != nullptr) comm_restart_cb_registry_.add(cb);
+  // Added/retained these methods to act as facade
+  void setCommRestartCallBack(std::function<void(uint32_t)> cb, const EventType& et = EventType::TLS_COMM) {
+    registerCallback(et, cb);
   }
 
-  void restartComm(uint32_t id) { comm_restart_cb_registry_.invokeAll(id); }
+  void restartComm(uint32_t id, const EventType& et = EventType::TLS_COMM) { invokeCallback(et, id); }
 
-  void restartThinReplicaServer(uint32_t id) {restartComm(id);}
+  void setTlsRestartCallBack(std::function<void(uint32_t)> cb, const EventType& et = EventType::THIN_REPLICA_SERVER) {
+    registerCallback(et, cb);
+  }
+
+  void restartThinReplicaServer(uint32_t id, const EventType& et = EventType::THIN_REPLICA_SERVER) {
+    invokeCallback(et, id);
+  }
 
   void setGetPeerPubKeyMethod(std::function<std::string(uint32_t)> m) { get_peer_pub_key_ = std::move(m); }
+
   std::string getPeerPubKey(uint32_t id) {
     if (get_peer_pub_key_) return get_peer_pub_key_(id);
     return std::string();
   }
 
-
  private:
+  void registerCallback(const EventType& et, std::function<void(uint32_t)> cb) {
+    if (cb != nullptr) {
+      if (event_registry_.find(et) == event_registry_.end()) {
+        event_registry_.insert({et, std::make_unique<CallbackRegistry>()});
+      }
+      event_registry_[et]->add(cb);
+    }
+  }
+
+  void invokeCallback(const EventType& et, uint32_t id) {
+    if (event_registry_.find(et) != event_registry_.end()) {
+      event_registry_[et]->invokeAll(id);
+    } else {
+      throw std::invalid_argument{"invokeCallback called with an invalid eventType"};
+    }
+  }
+
   std::mutex lock_comm_;
-  concord::util::CallbackRegistry<uint32_t> comm_restart_cb_registry_;
+  //keeping function template to be void(uint32_t) for uniformity
+  std::unordered_map<const EventType, std::unique_ptr<CallbackRegistry>, EnumHash>
+      event_registry_;
   std::function<std::string(uint32_t)> get_peer_pub_key_;
 };
 }  // namespace bft::communication
