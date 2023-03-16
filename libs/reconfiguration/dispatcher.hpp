78b796dd3bfd71fd74f2adb76a8ca9447315f3fa
Yehonatan Buchnik <yontyon@gmail.com>
2023-03-16 12:10:18 +0000

    move reconfiguration lib to be under libs

---
+++ libs/reconfiguration/dispatcher.hpp
@@ -0,0 +1,85 @@
+// Concord
+//
+// Copyright (c) 2018-2021 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0 License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+
+#pragma once
+
+#include "log/logger.hpp"
+#include "ireconfiguration.hpp"
+#include "concord.cmf.hpp"
+#include "util/Metrics.hpp"
+
+namespace concord::reconfiguration {
+// The dispatcher forwards all messages to their appropriate handlers.
+// All handled messages are defined in the IReconfigurationHandler interface.
+class Dispatcher {
+ public:
+  Dispatcher();
+  // This method is the gate for all reconfiguration actions. It works as
+  // follows:
+  // 1. Validate the request against the reconfiguration system operator (RSO)
+  // public key
+  // 2. Direct the request to the relevant handler
+  // 3. Wrap the response in the concordResponse message
+  //
+  // Basically, we would like to write each reconfiguration write command to the
+  // blockchain and document it as part of the state. This will be under the
+  // responsibility of each handler to write its own commands to the blockchain.
+  concord::messages::ReconfigurationResponse dispatch(const concord::messages::ReconfigurationRequest&,
+                                                      uint64_t sequence_num,
+                                                      const std::optional<bftEngine::Timestamp>& timestamp);
+
+  void addReconfigurationHandler(std::shared_ptr<IReconfigurationHandler> h,
+                                 ReconfigurationHandlerType type = ReconfigurationHandlerType::REGULAR) {
+    if (h) {
+      switch (type) {
+        case PRE:
+          pre_reconfig_handlers_.push_back(h);
+          break;
+        case REGULAR:
+          reconfig_handlers_.push_back(h);
+          break;
+        case POST:
+          post_reconfig_handlers_.push_back(h);
+          break;
+      }
+    }
+  }
+
+  void setAggregator(std::shared_ptr<concordMetrics::Aggregator> aggregator) {
+    aggregator_ = aggregator;
+    component_.SetAggregator(aggregator_);
+  }
+
+ private:
+  logging::Logger getLogger() {
+    static logging::Logger logger_(logging::getLogger("concord.reconfiguration"));
+    return logger_;
+  }
+  template <typename T>
+  bool handleRequest(const T& msg,
+                     uint64_t bft_seq_num,
+                     uint32_t sender_id,
+                     const std::optional<bftEngine::Timestamp>& timestamp,
+                     concord::messages::ReconfigurationResponse& rres,
+                     std::shared_ptr<IReconfigurationHandler> handler) {
+    return handler->handle(msg, bft_seq_num, sender_id, timestamp, rres);
+  }
+  std::vector<std::shared_ptr<IReconfigurationHandler>> pre_reconfig_handlers_;
+  std::vector<std::shared_ptr<IReconfigurationHandler>> reconfig_handlers_;
+  std::vector<std::shared_ptr<IReconfigurationHandler>> post_reconfig_handlers_;
+  std::shared_ptr<concordMetrics::Aggregator> aggregator_;
+  concordMetrics::Component component_;
+  concordMetrics::CounterHandle executions_;
+  concordMetrics::CounterHandle failures_;
+};
+
+}  // namespace concord::reconfiguration
---
+++ libs/reconfiguration/src/dispatcher.cpp
@@ -0,0 +1,112 @@
+// Concord
+//
+// Copyright (c) 2018-2021 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0 License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the subcomponent's license, as noted in the LICENSE
+// file.
+
+#include "reconfiguration/dispatcher.hpp"
+#include "util/kvstream.h"
+
+using namespace concord::messages;
+
+namespace concord::reconfiguration {
+
+#define ADDITIONAL_DATA(resp, x)                                                     \
+  {                                                                                  \
+    std::ostringstream oss;                                                          \
+    oss << (x);                                                                      \
+    std::string str = oss.str();                                                     \
+    std::copy(str.cbegin(), str.cend(), std::back_inserter((resp).additional_data)); \
+  }
+
+ReconfigurationResponse Dispatcher::dispatch(const ReconfigurationRequest& request,
+                                             uint64_t sequence_num,
+                                             const std::optional<bftEngine::Timestamp>& timestamp) {
+  ReconfigurationResponse rresp;
+  concord::messages::ReconfigurationErrorMsg error_msg;
+  bool valid = false;
+  ReconfigurationRequest request_without_sig = request;
+  request_without_sig.signature = {};
+  std::vector<uint8_t> serialized_cmd;
+  concord::messages::serialize(serialized_cmd, request_without_sig);
+  auto ser_data = std::string(serialized_cmd.begin(), serialized_cmd.end());
+  auto ser_sig = std::string(request.signature.begin(), request.signature.end());
+  rresp.success = true;
+  auto sender_id = request.sender;
+  executions_++;
+  try {
+    // Run pre-reconfiguration handlers
+    for (auto& handler : pre_reconfig_handlers_) {
+      // Each reconfiguration handler handles only what it can validate
+      if (!handler->verifySignature(sender_id, ser_data, ser_sig)) {
+        error_msg.error_msg = "Invalid signature";
+        continue;
+      }
+      error_msg.error_msg.clear();
+      valid = true;
+      rresp.success &=
+          std::visit([&](auto&& arg) { return handleRequest(arg, sequence_num, sender_id, timestamp, rresp, handler); },
+                     request.command);
+    }
+
+    // Run regular reconfiguration handlers
+    for (auto& handler : reconfig_handlers_) {
+      // Each reconfiguration handler handles only what it can validate
+      if (!handler->verifySignature(sender_id, ser_data, ser_sig)) {
+        error_msg.error_msg = "Invalid signature";
+        continue;
+      }
+      error_msg.error_msg.clear();
+      valid = true;
+      rresp.success &=
+          std::visit([&](auto&& arg) { return handleRequest(arg, sequence_num, sender_id, timestamp, rresp, handler); },
+                     request.command);
+    }
+
+    // Run post-reconfiguration handlers
+    for (auto& handler : post_reconfig_handlers_) {
+      // Each reconfiguration handler handles only what it can validate
+      if (!handler->verifySignature(sender_id, ser_data, ser_sig)) {
+        error_msg.error_msg = "Invalid signature";
+        continue;
+      }
+      error_msg.error_msg.clear();
+      valid = true;
+      rresp.success &=
+          std::visit([&](auto&& arg) { return handleRequest(arg, sequence_num, sender_id, timestamp, rresp, handler); },
+                     request.command);
+    }
+
+    if (!valid) {
+      failures_++;
+      rresp.success = false;  // If no handler was able to verify the request, it is an invalid request
+      LOG_WARN(getLogger(), "Failed to verify the reconfiguration request. " << KVLOG(request.sender, sequence_num));
+    }
+  } catch (const std::exception& e) {
+    failures_++;
+    rresp.success = false;
+    LOG_ERROR(getLogger(),
+              "Reconfiguration request from sender: "
+                  << request.sender << " seqnum:" << std::to_string(sequence_num) + " failed, exception: " + e.what());
+    ADDITIONAL_DATA(rresp,
+                    "Reconfiguration request " + std::to_string(sequence_num) + " failed, exception: " + e.what());
+  }
+  component_.UpdateAggregator();
+  return rresp;
+}
+Dispatcher::Dispatcher()
+    : aggregator_{std::make_shared<concordMetrics::Aggregator>()},
+      component_{"reconfiguration_dispatcher", aggregator_},
+      executions_{component_.RegisterCounter("executions")},
+      failures_{component_.RegisterCounter("failures")} {
+  component_.Register();
+  component_.UpdateAggregator();
+}
+
+}  // namespace concord::reconfiguration
