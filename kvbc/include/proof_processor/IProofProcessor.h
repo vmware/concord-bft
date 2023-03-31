c7fe8a29bf2207c4a56a66f43c863122df56a6bc
HristoStaykov <61588673+HristoStaykov@users.noreply.github.com>
2023-03-31 12:25:08 +0300

    Merkle management layer (#2947)
    * Initial Merkle management layer.
    
    Processing infrastructure for the updates to the Merkle trees.
    Added IMerkleBuilder, EmptyMerkleBuilder,
    initial BaseMerkleBuilder and SyncMerkleBuilder.
    
    * Added abstraction layer.
    
    The introduced abstraction layer is meant to hide
    the details of the exact mechanism used to build and
    maintain the proofs for validity of the state.
    
    * Member functions const correctness fix + argument changed to const reference.
    
    * Change builder to processor.
    
    * Formatting changes.
    
    * Added description.
    
    * Move implementations to cpp file.
    
    * Return from getProof changed to std::vector<concord::Byte>.

---
+++ kvbc/include/proof_processor/IProofProcessor.h
@@ -0,0 +1,41 @@
+// Concord
+//
+// Copyright (c) 2023 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0 License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the sub-component's license, as noted in the
+// LICENSE file.
+
+#pragma once
+
+//#include <stdint.h>
+#include <string>
+#include <vector>
+#include "categorization/updates.h"
+#include "util/sliver.hpp"
+#include "util/types.hpp"
+
+namespace concord {
+namespace kvbc {
+
+// This is an interface of a general proof builder.
+class IProofProcessor {
+ public:
+  virtual ~IProofProcessor() = default;
+
+  // Initialize with proper parameters.
+  virtual void Init(uint numVersionsStored) = 0;
+  // Process all the updates that will be persisted.
+  virtual void ProcessUpdates(const categorization::Updates& updates) = 0;
+  // Get a serialized object serving as proof for correctness.
+  virtual std::vector<concord::Byte> GetProof(const std::string& key) = 0;
+  // In case of async processing we use this call to sync and wait for all async tasks to complete.
+  virtual void WaitForScheduledTasks() = 0;
+};
+
+}  // namespace kvbc
+}  // namespace concord
---
+++ kvbc/src/merkle_processor/SyncMerkleProcessor.cpp
@@ -0,0 +1,54 @@
+// Concord
+//
+// Copyright (c) 2023 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache 2.0 license (the "License").
+// You may not use this product except in compliance with the Apache 2.0 License.
+//
+// This product may include a number of subcomponents with separate copyright
+// notices and license terms. Your use of these subcomponents is subject to the
+// terms and conditions of the sub-component's license, as noted in the
+// LICENSE file.
+
+#include "merkle_processor/SyncMerkleProcessor.h"
+#include "kvbc_app_filter/kvbc_key_types.h"
+#include "util/hex_tools.hpp"
+
+namespace concord {
+namespace kvbc {
+namespace sparse_merkle {
+
+using concordUtils::bufferToHex;
+
+void SyncMerkleProcessor::Init(uint numVersionsStored) {}
+
+void SyncMerkleProcessor::BeginVersionUpdateBatch() {}
+
+void SyncMerkleProcessor::UpdateAccountTree(const address& addr, const std::string& key, const value& data) {
+  LOG_INFO(V4_BLOCK_LOG,
+           "MerkleProcessor: UpdateAccountTree(addr=" << bufferToHex(addr.data(), sizeof(addr))
+                                                      << ", key=" << bufferToHex(key.c_str(), key.size()));
+}
+
+void SyncMerkleProcessor::CommitVersionUpdateBatch() {
+  LOG_INFO(V4_BLOCK_LOG, "MerkleProcessor: CommitVersionUpdateBatch");
+}
+
+std::vector<std::string> SyncMerkleProcessor::GetAccountMerkleRootPath(const address& addr) { return {}; }
+
+std::vector<std::string> SyncMerkleProcessor::GetAccountStorageKeyMerklePath(const address& addr,
+                                                                             const std::string& key) {
+  return {};
+}
+
+std::vector<concord::Byte> SyncMerkleProcessor::GetProof(const std::string& key) { return {}; }
+
+bool SyncMerkleProcessor::VerifyMerkleTreePath(std::string root_hash, std::string key, std::vector<std::string> path) {
+  return true;
+}
+
+void SyncMerkleProcessor::WaitForScheduledTasks() {}
+
+}  // namespace sparse_merkle
+}  // namespace kvbc
+}  // namespace concord
